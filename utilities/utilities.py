c45f31ec76269f87bf8b0c02a841f8215a5d0959
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-06 20:53:40 +0000

    Migrated utilities.permIter( ) to listtools.permutations( ).

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1094,26 +1094,6 @@ def replace(l, indices, material, action = 'in place'):
       return result  
 
 
-def permIter(seq):
-   '''
-   Given some sequence 'seq', returns an iterator that gives
-   all permutations of that sequence.
-   '''
-
-   ## Base case
-   if len(seq) == 1:
-      yield(seq)
-      raise StopIteration
-
-   ## Inductive case
-   for i in range(len(seq)):
-      element_slice = seq[i:i+1]
-      rest_iter = permIter(seq[:i] + seq[i+1:])
-      for rest in rest_iter:
-         yield(element_slice + rest)
-   raise StopIteration
-
-
 def series(start = 0, step = 0, offsets = [0], max = sys.maxint):
    '''
    >>> s = series()
