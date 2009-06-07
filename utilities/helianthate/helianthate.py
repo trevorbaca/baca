bb22a3e1570d7a2cc05c9707a863a4baeadfb5f0
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-07 21:39:20 +0000

    utilities.helianthate( ) now works only with objects that are not Abjad components.

--- utilities/helianthate/helianthate.py
+++ utilities/helianthate/helianthate.py
@@ -1,5 +1,6 @@
-from abjad.leaf.leaf import _Leaf
-from abjad.note.note import Note
+#from abjad.leaf.leaf import _Leaf
+#from abjad.note.note import Note
+from abjad.component.component import _Component
 from abjad.tools import clone
 from abjad.tools import listtools
 from baca.utilities.rotate_nested import rotate_nested as \
@@ -20,42 +21,53 @@ def helianthate(l, outer, inner, flattened = True):
 
    >>> l = [[1, 2, 3], [4, 5], [6, 7, 8]]
    >>> utilities.helianthate(l, 'left', 'right')[:24]
-   [1, 2, 3, 4, 5, 6, 7, 8, 5, 4, 8, 6, 7, 3, 1, 2, 7, 8, 6, 2, 3, 1, 4, 5]
+   [1, 2, 3, 4, 5, 6, 7, 8, 5, 4, 8, 6, 7, 3, 1, 2, 7, 8, 6, 2, 3, 1, 4, 5]'''
 
-   >>> l = [note.Note(n, 1, 4) for n in range(1, 9)]
-   >>> l = listtools.partition_by_counts(l, [3, 2, 3])
-   >>> utilities.helianthate(l, 'left', 'right')'''
+   assert all([not isinstance(x, _Component) for x in l])
 
-   if isinstance(l[0][0], _Leaf):
-      start = [[n.pitch.pc for n in sublist] for sublist in l]
-      result = [ ]
-      for sublist in l:
-         result.append([clone.unspan([element])[0] for element in sublist])
-   else:
-      start = l[:]
-      result = l[:]
+#   if isinstance(l[0][0], _Leaf):
+#      start = [[n.pitch.pc for n in sublist] for sublist in l]
+#      result = [ ]
+#      for sublist in l:
+#         result.append([clone.unspan([element])[0] for element in sublist])
+#   else:
+#      start = l[:]
+#      result = l[:]
+
+   start = l[:]
+   result = l[:]
 
    while True:
       last = result[-len(start):]
 
-      if isinstance(last[0][0], (int, float, str, tuple)):
-         input = last
-      else:
-         input = [ ]
-         for sublist in last:
-            new = [ ]
-            for n in sublist:
-               new.append(clone.unspan([n])[0])
-            input.append(new)
+#      if isinstance(last[0][0], (int, float, str, tuple)):
+#         input = last
+#      else:
+#         input = [ ]
+#         for sublist in last:
+#            new = [ ]
+#            for n in sublist:
+#               new.append(clone.unspan([n])[0])
+#            input.append(new)
+
+      input = last
                
       next = utilities_rotate_nested(input, outer, inner)
 
-      if next == start or (isinstance(next[0][0], Note) and \
-         [[n.pitch.pc for n in sublist] for sublist in next] == start):
-         if flattened == True:
-            result = listtools.flatten(result)
+#      if next == start or (isinstance(next[0][0], Note) and \
+#         [[n.pitch.pc for n in sublist] for sublist in next] == start):
+#         if flattened == True:
+#            result = listtools.flatten(result)
+#         break
+#      else:
+#         result.extend(next)
+
+      if next == start:
          break
-      else:
-         result.extend(next)
+
+      result.extend(next) 
+
+   if flattened:
+      result = listtools.flatten(result)
 
    return result
