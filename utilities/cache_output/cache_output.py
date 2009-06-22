b8b8810edfd05862ebcdbeca8796dcf591beab2b
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-22 18:01:31 +0000

    Changed utilities.cache_output to look for output directory two levels up from starting location.

--- utilities/cache_output/cache_output.py
+++ utilities/cache_output/cache_output.py
@@ -2,17 +2,20 @@ import os
 
 
 def cache_output(output, name, location):
-   '''Cache string-based output to output directory relative to location.'''
+   '''Cache string-based output to location/../../output directory.'''
 
    assert isinstance(output, str)
    assert isinstance(name, str)
    assert not ' ' in name
 
-   dirname = os.path.abspath(location)
-   print dirname
-   raise Exception
-   dirname = os.path.dirname(dirname)
-   outfile = os.path.join(dirname, 'output', name + '.py')
+   filename = os.path.abspath(location)
+   dirname = os.path.dirname(filename)
+   dirpath = dirname.split(os.sep)
+   parpardir = dirpath[:-2]
+   parpardir.insert(0, '/')
+   parpardir.append('output')
+   output_directory = os.path.join(*parpardir)
+   outfile = os.path.join(output_directory, name + '.py')
    outfile = file(outfile, 'w')
    outfile.write(output)
    outfile.close( )
