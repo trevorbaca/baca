96fb57f0dbd2870b7f54932dd65208686c01e282
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-22 18:09:48 +0000

    Extended utilities.import_public_functions( ) to import public data instead of functions when the new functions keyword is set to False.

--- utilities/import_public_functions/import_public_functions.py
+++ utilities/import_public_functions/import_public_functions.py
@@ -2,9 +2,12 @@ import os
 import types
 
 
-def import_public_functions(directory, namespace):
+def import_public_functions(directory, namespace, functions = True):
    '''List the contents of directory. Import every public function
-   from every public module from directory into namespace.'''
+   from every public module from directory into namespace.
+
+   When functions is False, import public data descriptors like lists
+   but do not import public functions.'''
    
    module_names = os.listdir(os.path.dirname(directory))
    module_names = [x for x in module_names if x.endswith('.py')] 
@@ -15,5 +18,9 @@ def import_public_functions(directory, namespace):
       module = __import__(module_name, globals = namespace)
       for key, value in vars(module).items( ):
          if not key.startswith('_'):
-            if isinstance(value, types.FunctionType):
-               namespace[key] = value
+            if functions == True:
+               if isinstance(value, types.FunctionType):
+                  namespace[key] = value
+            else:
+               if not isinstance(value, types.ModuleType):
+                  namespace[key] = value
