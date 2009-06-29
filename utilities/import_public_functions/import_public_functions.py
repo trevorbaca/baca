50f2203ee57df50b4a1c0a54f9e41001c6fb5a07
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-29 15:16:05 +0000

    Extended import_public_functions(directory, namespace, functions = True, abjad = False) with optional 'abjad' keyword. When abjad is True, import Abjad functions and classes imported by directory modules. When abjad is False, import neither Abjad functions nor Abjad classes imported by directory modules.

--- utilities/import_public_functions/import_public_functions.py
+++ utilities/import_public_functions/import_public_functions.py
@@ -2,12 +2,19 @@ import os
 import types
 
 
-def import_public_functions(directory, namespace, functions = True):
-   '''List the contents of directory. Import every public function
-   from every public module from directory into namespace.
+def import_public_functions(directory, namespace, 
+   functions = True, abjad = False):
+   '''Import into `namespace` every public name in every public 
+   module resident in `directory.
 
-   When functions is False, import public data descriptors like lists
-   but do not import public functions.'''
+   When `functions` is True, do not import public functions.
+   When `functions` is False, do import public functions.
+
+   When `abjad` is True, import Abjad functions and classes imported by
+   `directory` functions and classes.
+   When `abjad` is False, import neither Abjad functions nor Abjad classes
+   imported by `directory` functions and classes.
+   '''
    
    module_names = os.listdir(os.path.dirname(directory))
    module_names = [x for x in module_names if x.endswith('.py')] 
@@ -16,11 +23,23 @@ def import_public_functions(directory, namespace, functions = True):
 
    for module_name in module_names:
       module = __import__(module_name, globals = namespace)
+#      for key, value in vars(module).items( ):
+#         if not key.startswith('_'):
+#            if functions == True:
+#               if isinstance(value, types.FunctionType):
+#                  namespace[key] = value
+#            else:
+#               if not isinstance(value, types.ModuleType):
+#                  namespace[key] = value
       for key, value in vars(module).items( ):
-         if not key.startswith('_'):
-            if functions == True:
-               if isinstance(value, types.FunctionType):
+         if functions == True or not isinstance(value, types.FunctionType):
+            if abjad == True or not _is_abjad(value):
+               if not key.startswith('_'):
                   namespace[key] = value
-            else:
-               if not isinstance(value, types.ModuleType):
-                  namespace[key] = value
+
+
+def _is_abjad(value):
+   path = getattr(value, '__module__', None)
+   if path is None:
+      path = getattr(value, '__name__', '')
+   return path.startswith('abjad.')
