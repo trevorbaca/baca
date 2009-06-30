076db3acbb9554a463443e7467ec6164735c26a2
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-06-30 13:24:06 +0000

    Taught utilities.cache_output( ) about promoted workspace directories.

--- utilities/cache_output/cache_output.py
+++ utilities/cache_output/cache_output.py
@@ -2,7 +2,7 @@ import os
 
 
 def cache_output(output, name, location):
-   '''Cache string-based output to location/../../output directory.'''
+   '''Cache string-based output to location/../_output directory.'''
 
    assert isinstance(output, str)
    assert isinstance(name, str)
@@ -11,9 +11,9 @@ def cache_output(output, name, location):
    filename = os.path.abspath(location)
    dirname = os.path.dirname(filename)
    dirpath = dirname.split(os.sep)
-   parpardir = dirpath[:-2]
+   parpardir = dirpath[:-1]
    parpardir.insert(0, '/')
-   parpardir.append('output')
+   parpardir.append('_output')
    output_directory = os.path.join(*parpardir)
    outfile = os.path.join(output_directory, name + '.py')
    outfile = file(outfile, 'w')
