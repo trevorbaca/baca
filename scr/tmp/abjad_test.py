0d6c3f315cdb28500b9d4f213b5f4fc692dcc49b
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-09-29 19:15:51 +0000

    Updated scores-grp.

--- scr/tmp/abjad_test.py
+++ scr/tmp/abjad_test.py
@@ -1,10 +1,10 @@
 from abjad import *
 
+t = Staff(RigidMeasure((2, 8), construct.run(2)) * 4)
+pitchtools.diatonicize(t)
+layout.line_break_every_prolated(t, Rational(4, 8))      
 
-t = Staff(construct.scale(4))
-spacing_spanner_1 = SpacingSpanner(t[:2])
-spacing_spanner_1.new_section = True
-spacing_spanner_1.proportional_notation_duration = Rational(1, 15)
-spacing_spanner_2 = SpacingSpanner(t[2:])
-spacing_spanner_2.new_section = True
-spacing_spanner_2.proportional_notation_duration = Rational(1, 30)
+systems = SystemYOffsets(40, 5)
+staves = StaffAlignmentOffsets(0, -15)
+positioning = FixedStaffPositioning(systems, staves)
+layout.apply_fixed_staff_positioning(t, positioning)
