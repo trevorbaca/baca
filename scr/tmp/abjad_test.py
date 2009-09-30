eeaebcd43d69bee9a787565660a4cf0af88eafba
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-09-30 15:00:18 +0000

    Removed scr/tmp.

--- scr/tmp/abjad_test.py
+++
@@ -1,8 +0,0 @@
-from abjad import *
-
-
-t = DynamicMeasure(construct.scale(2))
-Beam(t[:])
-label.measure_numbers(t)
-layout.insert_measure_padding_rest(
-   t, Rational(1, 32), Rational(1, 64), splice = True)
