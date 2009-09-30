3e9087ef3486ffaef2f5fbe09ebc27bea8fb9511
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2009-09-30 14:53:02 +0000

    Ignore tmp.

--- scr/tmp/abjad_test.py
+++ scr/tmp/abjad_test.py
@@ -1,10 +1,8 @@
 from abjad import *
 
-t = Staff(RigidMeasure((2, 8), construct.run(2)) * 4)
-pitchtools.diatonicize(t)
-layout.line_break_every_prolated(t, Rational(4, 8))      
 
-systems = SystemYOffsets(40, 5)
-staves = StaffAlignmentOffsets(0, -15)
-positioning = FixedStaffPositioning(systems, staves)
-layout.apply_fixed_staff_positioning(t, positioning)
+t = DynamicMeasure(construct.scale(2))
+Beam(t[:])
+label.measure_numbers(t)
+layout.insert_measure_padding_rest(
+   t, Rational(1, 32), Rational(1, 64), splice = True)
