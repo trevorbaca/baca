eb96f464d6fb30e951f30ad0528c4409bfd1f2b9
tbaca <tbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2010-01-31 16:08:55 +0000

    SCRIPTS (*)
        * Added all script update
        * Added all svn script
        * Added rmlx script
        * Added rmpycs script
        * Added vij2 script
        * Added render_tex() utility
        * Added script to find all executable files in a directory
        * Added script to recursively find file names with forbidden characters
        * Renamed abj-add-all to svn-add-all

--- __init__.py
+++ __init__.py
@@ -4,9 +4,12 @@
 
    * Cary (2004-06) for bass flute
    * Sekka (2006-07) for flute
-   * Lidercfeny (2007-08) for flute, violin & piano.'''
+   * Lidercfeny (2007-08) for flute, violin & piano.
+'''
 
-import music
+## temporarily commentd out music because of Abjad package location changes
+## TODO: uncomment music and integrate with new Abjad package locations.
+#import music
 import utilities
 
 ## import pitch manually to avoid the 2- to 3-second load time
---
+++ utilities/render_tex.py
@@ -0,0 +1,9 @@
+import os
+
+
+def render_tex(input_directory, input_file_name_stem, output_directory):
+   print 'Rendering %s.tex ...' % input_file_name_stem
+   command = 'pdflatex --jobname=%s -output-directory=%s %s/%s.tex'
+   command %= (input_file_name_stem, output_directory, input_directory, input_file_name_stem)
+   os.system(command)
+   os.system('rmlx %s' % output_directory)
