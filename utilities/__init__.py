c625f509d8c165710ddd11024acc87173653497d
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2010-09-18 15:03:42 +0000

    Removed segment( ) from utilities initializer.

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -14,4 +14,3 @@ from render_tex import render_tex
 from replace_nested_elements_with_unary_subruns import replace_nested_elements_with_unary_subruns
 from rotate_nested import rotate_nested
 from sectionalize import sectionalize
-from segment import segment
