7cf993ce292a5dff4674ad03c4b7688ccbe60ecb
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2010-09-18 14:58:00 +0000

    Reimplemented utilities.segment( ) with drop-in replacement from listtools.partition_by_weights_not_less_than( ). When the cycle = 'sheet' in utilities.segment( ) it is necessary to first repeated the input list using listtools.repeat_list_to_weight(l, sum(s), remainder = 'more').

--- utilities/segment.py
+++ utilities/segment.py
@@ -1,4 +1,5 @@
-from fractions import Fraction
+from abjad.tools import listtools
+#from fractions import Fraction
 
 
 ## TODO: Break out 'sheet' case into separate function. ##
@@ -21,61 +22,70 @@ def segment(l, s, cycle = 'knife'):
    >>> l = [1]
    >>> utilities.segment(l, [2, 3, 2, 3], cycle = 'sheet')
    >>> l
-   [[1, 1], [1, 1, 1], [1, 1], [1, 1, 1]]'''
+   [[1, 1], [1, 1, 1], [1, 1], [1, 1, 1]]
+   '''
 
-   assert isinstance(l, list)
-   assert isinstance(s, list)
-   assert all([isinstance(x, (int, float, long, Fraction)) for x in s])
-
-   result = [ ]
+#   assert isinstance(l, list)
+#   assert isinstance(s, list)
+#   assert all([isinstance(x, (int, float, long, Fraction)) for x in s])
+#
+#   result = [ ]
+#
+#   if cycle == False:
+#      j = 0
+#      k = 0
+#      while True:
+#         try:
+#            element = s[k]
+#            new = []
+#            while sum([abs(x) for x in new]) < element:
+#               try:
+#                  new.extend(eval(l[j]))
+#               except:
+#                  new.append(l[j])
+#               j += 1
+#               if j >= len(l):
+#                  raise IndexError
+#            result.append(new)
+#            k += 1
+#         except IndexError:
+#            break
+#
+#   elif cycle == 'knife':
+#      k = 0
+#      new = []
+#      for element in l:
+#         try:
+#            new.extend(eval(element))
+#         except:
+#            new.append(element)
+#         if sum([abs(x) for x in new]) >= s[k]:
+#            result.append(new)
+#            new = []
+#            k = (k + 1) % len(s)
+#
+#   elif cycle == 'sheet':
+#      j = 0
+#      for element in s:
+#         new = []
+#         while sum([abs(x) for x in new]) < element:
+#            try:
+#               new.extend(eval(l[j % len(l)]))
+#            except:
+#               new.append(l[j % len(l)])
+#            j += 1
+#         result.append(new)
+#
+#   else:
+#      print 'Unknown value %s for cycle.' % cycle
+#      raise ValueError
+#
+#   return result
 
    if cycle == False:
-      j = 0
-      k = 0
-      while True:
-         try:
-            element = s[k]
-            new = []
-            while sum([abs(x) for x in new]) < element:
-               try:
-                  new.extend(eval(l[j]))
-               except:
-                  new.append(l[j])
-               j += 1
-               if j >= len(l):
-                  raise IndexError
-            result.append(new)
-            k += 1
-         except IndexError:
-            break
-
+      return listtools.partition_by_weights_not_less_than(l, s, cyclic = False, overhang = False)
    elif cycle == 'knife':
-      k = 0
-      new = []
-      for element in l:
-         try:
-            new.extend(eval(element))
-         except:
-            new.append(element)
-         if sum([abs(x) for x in new]) >= s[k]:
-            result.append(new)
-            new = []
-            k = (k + 1) % len(s)
-
+      return listtools.partition_by_weights_not_less_than(l, s, cyclic = True, overhang = False)
    elif cycle == 'sheet':
-      j = 0
-      for element in s:
-         new = []
-         while sum([abs(x) for x in new]) < element:
-            try:
-               new.extend(eval(l[j % len(l)]))
-            except:
-               new.append(l[j % len(l)])
-            j += 1
-         result.append(new)
-
-   else:
-      print 'Unknown value %s for cycle.' % cycle
-      raise ValueError
-
-   return result
+      l = listtools.repeat_list_to_weight(l, sum(s), remainder = 'more')
+      return listtools.partition_by_weights_not_less_than(l, s, cyclic = False, overhang = False)
