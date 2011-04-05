25cb2f1e5368697c75fab7ebedfb3a1cd8364ea0
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-05 20:14:41 +0000

    Updated ConstellationCircuit to use new score override syntax. Updated ConstellationCircuit to use new Lily file settings syntax. Added ConstellationCircuit tests. As of this commit, all ConstellationCircuit attributes and methods perform as advertised with two exceptions. ConstellationCircuit.show_colored_generators( ) and ConstellationCircuit.show_colored_generators_and_pivots( ) have not yet ported forward.

--- pitch/ConstellationCircuit.py
+++ pitch/ConstellationCircuit.py
@@ -1,4 +1,6 @@
 from abjad.tools import iotools
+from abjad.tools import lilyfiletools
+from abjad.tools import schemetools
 from abjad.tools import scoretools
 from abjad.tools import seqtools
 from baca.pitch.Constellation import Constellation
@@ -25,6 +27,7 @@ class ConstellationCircuit(object):
 
    ## PRIVATE ATTRIBUTES ##
 
+   ## FIXME
    @property
    def _colored_generators(self):
       result = [ ]
@@ -56,14 +59,26 @@ class ConstellationCircuit(object):
          constellation = Constellation(self, partitioned_generator_pnl)
          self._constellations.append(constellation)
 
+   def _make_lily_file_and_score_from_chords(self, chords):
+      score, treble, bass = scoretools.make_piano_sketch_score_from_leaves(chords)
+      score.override.text_script.staff_padding = 10
+      score.set.proportional_notation_duration = schemetools.SchemeMoment(1, 30)
+      lily_file = lilyfiletools.make_basic_lily_file(score)
+      lily_file.default_paper_size = 'letter', 'landscape'
+      lily_file.global_staff_size = 18
+      lily_file.layout_block.indent = 0
+      lily_file.layout_block.ragged_right = True
+      lily_file.paper_block.system_system_spacing = schemetools.SchemeVector(
+         schemetools.SchemePair('basic_distance', 0),
+         schemetools.SchemePair('minimum_distance', 0),
+         schemetools.SchemePair('padding', 12),
+         schemetools.SchemePair('stretchability', 0))
+      lily_file.paper_block.top_margin = 24
+      return lily_file, score
+
    def _show_chords(self, chords):
-      score, treble, bass = \
-         scoretools.make_piano_sketch_score(chords)
-      score.spacing.proportional_notation_duration = Fraction(1, 24)
-      score.lily_file.default_paper_size = 'letter', 'landscape'
-      score.lily_file.global_staff_size = 18
-      score.text.staff_padding = 10
-      iotools.show(score.lily_file)
+      lily_file, score = self._make_lily_file_and_score_from_chords(chords)
+      iotools.show(lily_file)
 
    ## PUBLIC ATTRIBUTES ##
 
@@ -75,16 +90,16 @@ class ConstellationCircuit(object):
       return result
 
    @property
+   def pitch_range(self):
+      return self._pitch_range
+
+   @property
    def pivots(self):
       result = [ ]
       for constellation in self:
          result.append(constellation.pivot)
       return result
          
-   @property
-   def pitch_range(self):
-      return self._pitch_range
-
    ## PUBLIC METHODS ##
 
    def get(self, *args):
@@ -98,19 +113,19 @@ class ConstellationCircuit(object):
          constellation = self._constellations[constellation_index]
          return constellation.get(chord_number)
          
+   def show_colored_generators(self):
+      self._show_chords(self._colored_generators)
+
+   def show_colored_generators_and_pivots(self):
+      chords = zip(self._colored_generators, self.pivots)
+      chords = seqtools.flatten_sequence(chords, depth = 1)
+      self._show_chords(chords)
+
    def show_generators(self):
       self._show_chords(self.generators)
 
    def show_generators_and_pivots(self):
       chords = zip(self.generators, self.pivots)
-      chords = seqtools.flatten_sequence(chords, depth = 1)
-      self._show_chords(chords)
-
-   def show_generators_colored(self):
-      self._show_chords(self._colored_generators)
-
-   def show_generators_colored_and_pivots(self):
-      chords = zip(self._colored_generators, self.pivots)
       chords = seqtools.flatten_sequence(chords, depth = 1)
       self._show_chords(chords)
 
--- pitch/test/test_baca_pitch_CC.py
+++ pitch/test/test_baca_pitch_CC.py
@@ -1,21 +1,98 @@
+from abjad.components import Chord
 from abjad.tools import pitchtools
 import baca
 
 
+## TEST OVERRIDES ##
+
 def test_baca_pitch_CC_01( ):
-   assert len(baca.pitch.CC) == 8
+   '''Test __getitem__.'''
+
+   assert len(baca.pitch.CC[0]) == 180
+   assert len(baca.pitch.CC[1]) == 140
+   assert len(baca.pitch.CC[2]) == 80
+   assert len(baca.pitch.CC[3]) == 100
+   assert len(baca.pitch.CC[4]) == 180
+   assert len(baca.pitch.CC[5]) == 150
+   assert len(baca.pitch.CC[6]) == 120
+   assert len(baca.pitch.CC[7]) == 108
 
 
 def test_baca_pitch_CC_02( ):
-   pitch_range = pitchtools.PitchRange(-39, 48)
-   assert baca.pitch.CC.pitch_range == pitch_range
+   '''Test __len__.'''
 
+   assert len(baca.pitch.CC) == 8
+
+
+## TEST PRIVATE ATTRIBUTES ##
 
 def test_baca_pitch_CC_03( ):
-   generator_numbers = [80, 59, 56, 60, 83, 65, 79, 94]
-   assert baca.pitch.CC._generator_numbers == generator_numbers
+   '''Test generator numbers.'''
+
+   assert baca.pitch.CC._generator_numbers == [80, 59, 56, 60, 83, 65, 79, 94]
 
 
 def test_baca_pitch_CC_04( ):
-   pivot_numbers = [80, 75, 60, 73, 117, 69, 108, 99]
-   assert baca.pitch.CC._pivot_numbers == pivot_numbers
+   '''Test pivot numbers.'''
+
+   assert baca.pitch.CC._pivot_numbers == [80, 75, 60, 73, 117, 69, 108, 99]
+
+
+## TEST PUBLIC METHODS ## 
+
+def test_baca_pitch_CC_05( ):
+   '''Test get signature one.'''
+
+   assert baca.pitch.CC.get(1) is baca.pitch.CC[0]
+   assert baca.pitch.CC.get(2) is baca.pitch.CC[1]
+   assert baca.pitch.CC.get(3) is baca.pitch.CC[2]
+   assert baca.pitch.CC.get(4) is baca.pitch.CC[3]
+   assert baca.pitch.CC.get(5) is baca.pitch.CC[4]
+   assert baca.pitch.CC.get(6) is baca.pitch.CC[5]
+   assert baca.pitch.CC.get(7) is baca.pitch.CC[6]
+   assert baca.pitch.CC.get(8) is baca.pitch.CC[7]
+
+
+def test_baca_pitch_baca_CC_04aab( ):
+   '''Test get signature two.'''
+
+   assert baca.pitch.CC.get(1, 1) is baca.pitch.CC[0][0]
+   assert baca.pitch.CC.get(1, 2) is baca.pitch.CC[0][1]
+   assert baca.pitch.CC.get(1, 3) is baca.pitch.CC[0][2]
+   assert baca.pitch.CC.get(1, 4) is baca.pitch.CC[0][3]
+
+
+## TEST PUBLIC ATTRIBUTES ##
+
+def test_baca_pitch_CC_06( ):
+   '''Test generators.'''
+   
+   assert baca.pitch.CC.generators == [
+      Chord("<c d bf e' af' b' f'' g'' ef''' fs''' a''' cs''''>4"),
+      Chord("<c d bf e' af' b' f'' g'' ef''' fs''' a''' cs''''>4"),
+      Chord("<e b c' d' bf' ef'' af'' a'' cs''' f''' g''' fs''''>4"),
+      Chord("<e c' d' bf' b' ef'' a'' cs''' af''' f'''' fs'''' g''''>4"),
+      Chord("<c ef b cs' e' d'' fs'' g'' af'' bf'' f''' a'''>4"),
+      Chord("<d g bf c' ef' f' b' cs'' e'' fs''' af''' a''''>4"),
+      Chord("<d bf b c' f' g' ef'' fs'' af'' cs''' e''' a'''>4"),
+      Chord("<c b d' g' bf' e'' f'' fs'' af'' a'' ef''' cs''''>4")]
+
+
+def test_baca_pitch_CC_07( ):
+   '''Test pitch range.'''
+
+   assert baca.pitch.CC.pitch_range == pitchtools.PitchRange(-39, 48)
+
+
+def test_baca_pitch_CC_08( ):
+   '''Test pivots.'''
+
+   assert baca.pitch.CC.pivots == [
+      Chord("<c d bf e' af' b' f'' g'' ef''' fs''' a''' cs''''>4"),
+      Chord("<e b c' d' bf' ef'' af'' a'' cs''' f''' g''' fs''''>4"),
+      Chord("<e c' d' bf' b' ef'' a'' cs''' af''' f'''' fs'''' g''''>4"),
+      Chord("<c ef b cs' e' d'' fs'' g'' af'' bf'' f''' a'''>4"),
+      Chord("<d g bf c' ef' f' b' cs'' e'' fs''' af''' a''''>4"),
+      Chord("<d bf b c' f' g' ef'' fs'' af'' cs''' e''' a'''>4"),
+      Chord("<c b d' g' bf' e'' f'' fs'' af'' a'' ef''' cs''''>4"),
+      Chord("<c d bf e' af' b' f'' g'' ef''' fs''' a''' cs''''>4")]
