79148410ca99dfc857a64df0d8319f8c863e6ec6
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-19 14:06:46 +0000

    Moved cyclic list to Abjad public repository.

--- utilities/CyclicList/CyclicList.py
+++
@@ -1,9 +0,0 @@
-class CyclicList(list):
-
-   ## OVERLOADS ##
-
-   def __getitem__(self, expr):
-      if isinstance(expr, int):
-         return list.__getitem__(self, expr % len(self))
-      else:
-         raise NotImplementedError('TODO: implement slice-handling.')
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,3 @@
-from CyclicList import CyclicList
 from cache_output import cache_output
 from cache_text import cache_text
 from check_score_builder_commandline import check_score_builder_commandline
