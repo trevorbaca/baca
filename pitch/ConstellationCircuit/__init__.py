e3f597656977f0a1ec5a8eb8cdb1d9089707dea6
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-20 16:53:34 +0000

    Extended public class package initializers to load package contents dynamically.

--- pitch/ConstellationCircuit/__init__.py
+++ pitch/ConstellationCircuit/__init__.py
@@ -1,1 +1,4 @@
-from ConstellationCircuit import ConstellationCircuit
+#from ConstellationCircuit import ConstellationCircuit
+from abjad.tools.importtools._import_public_names_from_path_into_namespace import _import_public_names_from_path_into_namespace
+
+_import_public_names_from_path_into_namespace(__path__[0], globals( ), 'baca')
