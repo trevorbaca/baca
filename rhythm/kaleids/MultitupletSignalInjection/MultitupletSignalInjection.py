0322aca01fb9346dea8863427a0d1f1d76deb717
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-20 21:49:47 +0000

    Multituplet signal injection.

--- rhythm/kaleids/MultitupletSignalInjection/MultitupletSignalInjection.py
+++ rhythm/kaleids/MultitupletSignalInjection/MultitupletSignalInjection.py
@@ -8,14 +8,16 @@ class MultitupletSignalInjection(_RhythmicKaleid):
    '''Multituplet coruscation maker.
    '''
 
-   def __init__(self, tuplet_counts, prolation_addenda, note_values,
+   def __init__(self, minimum_tuplet_duration, tuplet_counts, prolation_addenda, note_values,
       tuplet_counts_inspector, prolation_addenda_inspector, note_values_inspector):
+      assert durtools.is_duration_token(minimum_tuplet_duration)
       assert seqtools.all_are_positive_integers(tuplet_counts)
       assert seqtools.all_are_integer_equivalent_numbers(prolation_addenda)
       assert seqtools.all_are_integer_equivalent_numbers(note_values)
       assert isinstance(tuplet_counts_inspector, types.FunctionType)
       assert isinstance(prolation_addenda_inspector, types.FunctionType)
       assert isinstance(note_values_inspector, types.FunctionType)
+      self._minimum_tuplet_duration = durtools.duration_token_to_rational(minimum_tuplet_duration)
       self._tuplet_counts = seqtools.CyclicTuple(tuplet_counts)
       self._prolation_addenda = seqtools.CyclicTuple(prolation_addenda)
       self._note_values = seqtools.CyclicTuple(note_values)
@@ -25,10 +27,11 @@ class MultitupletSignalInjection(_RhythmicKaleid):
 
    ## OVERLOADS ##
 
-   def __call__(self, duration_tokens, seeds = None):
+   def __call__(self, duration_tokens, seeds):
       '''Make rhythm from duration pairs.
       '''
-      duration_pairs = [durtools.duration_token_to_duration_pair(token) for token in duration_tokens]
-      if seeds is None:
-         seeds = [ ]
-      return [ ]
+      duration_pairs = self._duration_tokens_to_duration_pairs(duration_tokens)
+      tuplet_counts = self._tuplet_counts_inspector(duration_pairs, seeds)
+      prolation_addenda = self._prolation_addenda_inspector(duration_pairs, seeds)
+      note_values = self._note_values_inspector(duration_pairs, seeds)
+      return duration_pairs
--- rhythm/kaleids/_RhythmicKaleid/_RhythmicKaleid.py
+++ rhythm/kaleids/_RhythmicKaleid/_RhythmicKaleid.py
@@ -1,6 +1,14 @@
+from abjad.tools import durtools
+
+
 class _RhythmicKaleid(object):
 
    ## OVERLOADS ##
 
    def __repr__(self):
       return '%s( )' % (self.__class__.__name__)
+
+   ## PRIVATE METHODS ##
+
+   def _duration_tokens_to_duration_pairs(self, duration_tokens):
+      return [durtools.duration_token_to_duration_pair(token) for token in duration_tokens]
