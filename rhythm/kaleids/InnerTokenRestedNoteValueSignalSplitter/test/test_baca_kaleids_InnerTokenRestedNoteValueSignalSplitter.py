876b0195c9764e0417c90bfda65a06f2e298606c
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-22 20:55:28 +0000

    Inner token-rested note-value signal splitter.

---
+++ rhythm/kaleids/InnerTokenRestedNoteValueSignalSplitter/test/test_baca_kaleids_InnerTokenRestedNoteValueSignalSplitter.py
@@ -0,0 +1,71 @@
+from abjad import *
+from baca.rhythm.kaleids import *
+
+
+def test_baca_kaleids_InnerTokenRestedNoteValueSignalSplitter_01( ):
+   '''Fine note-value signal.
+   '''
+
+   nvs, denominator = [1], 16
+   rested_splitter = InnerTokenRestedNoteValueSignalSplitter(nvs, denominator)
+
+   duration_tokens = [(3, 16), (3, 8)]
+   leaf_lists = rested_splitter(duration_tokens)
+   leaves = seqtools.flatten_sequence(leaf_lists)
+
+   staff = Staff(measuretools.make_measures_with_full_measure_spacer_skips(duration_tokens))
+   measuretools.replace_contents_of_measures_in_expr(staff, leaves)
+
+   r'''
+   \new Staff {
+      {
+         \time 3/16
+         c'16
+         r16
+         c'16
+      }
+      {
+         \time 3/8
+         c'16
+         r16
+         r16
+         r16
+         r16
+         c'16
+      }
+   }
+   '''
+
+   assert staff.format == "\\new Staff {\n\t{\n\t\t\\time 3/16\n\t\tc'16\n\t\tr16\n\t\tc'16\n\t}\n\t{\n\t\t\\time 3/8\n\t\tc'16\n\t\tr16\n\t\tr16\n\t\tr16\n\t\tr16\n\t\tc'16\n\t}\n}"
+
+
+def test_baca_kaleids_InnerTokenRestedNoteValueSignalSplitter_02( ):
+   '''Coarse note-value signal.
+   '''
+
+   nvs, denominator = [1], 4
+   rested_splitter = InnerTokenRestedNoteValueSignalSplitter(nvs, denominator)
+
+   duration_tokens = [(3, 16), (3, 8)]
+   leaf_lists = rested_splitter(duration_tokens)
+   leaves = seqtools.flatten_sequence(leaf_lists)
+
+   staff = Staff(measuretools.make_measures_with_full_measure_spacer_skips(duration_tokens))
+   measuretools.replace_contents_of_measures_in_expr(staff, leaves)
+
+   r'''
+   \new Staff {
+      {
+         \time 3/16
+         c'8.
+      }
+      {
+         \time 3/8
+         c'16
+         r4
+         c'16
+      }
+   }
+   '''
+
+   assert staff.format == "\\new Staff {\n\t{\n\t\t\\time 3/16\n\t\tc'8.\n\t}\n\t{\n\t\t\\time 3/8\n\t\tc'16\n\t\tr4\n\t\tc'16\n\t}\n}"
