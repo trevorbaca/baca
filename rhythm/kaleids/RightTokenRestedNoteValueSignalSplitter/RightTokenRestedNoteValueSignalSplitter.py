cfa62a4fa21eaa54c518f950f948de3b8f4914e7
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-22 20:46:35 +0000

    Right token-rested note-value signal splitter.

---
+++ rhythm/kaleids/RightTokenRestedNoteValueSignalSplitter/RightTokenRestedNoteValueSignalSplitter.py
@@ -0,0 +1,25 @@
+from abjad.tools import mathtools
+from abjad.tools import seqtools
+from baca.rhythm.kaleids._TokenRestedNoteValueSignalSplitter import _TokenRestedNoteValueSignalSplitter
+import types
+
+
+class RightTokenRestedNoteValueSignalSplitter(_TokenRestedNoteValueSignalSplitter):
+   '''Right token-rested note-value signal splitter.
+
+   See the test file for examples.
+
+   Rest list of lists.
+
+   Sublists contain untied notes and / or rest.
+   '''
+
+   ## PRIVATE METHODS ##
+
+   def _token_rest_split_and_scaled_note_value_signal(self, split_and_scaled_note_value_signal):
+      split_and_rested_note_value_signal = [ ]
+      for part in split_and_scaled_note_value_signal:
+         rested_part = list(part)
+         rested_part[-1] = -abs(rested_part[-1])
+         split_and_rested_note_value_signal.append(rested_part)
+      return split_and_rested_note_value_signal
