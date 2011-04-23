b5fefe8b664985fbfc7ef85f581bf0c9d75b594d
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-23 03:06:16 +0000

    Little-endian note-maker.

---
+++ rhythm/kaleids/LittleEndianNoteMaker/LittleEndianNoteMaker.py
@@ -0,0 +1,20 @@
+from abjad.tools import notetools
+from abjad.tools import tietools
+from baca.rhythm.kaleids._NoteMaker import _NoteMaker
+
+
+class LittleEndianNoteMaker(_NoteMaker):
+   '''Little-endian note maker.
+
+   See the test file for examples.
+   '''
+
+   ## PRIVATE METHODS ##
+
+   def _make_note_lists(self, duration_tokens):
+      note_lists = [ ]
+      for duration_token in duration_tokens:
+         note_list = notetools.make_notes([0], [duration_token], direction = 'little-endian')
+         note_lists.append(note_list)
+      tietools.remove_tie_spanners_from_components_in_expr
+      return note_lists
