3ab84a4891ab47da4d157234165e2a47754c8fc0
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-23 03:01:56 +0000

    Big-endian note-maker.

---
+++ rhythm/kaleids/_NoteMaker/_NoteMaker.py
@@ -0,0 +1,13 @@
+from baca.rhythm.kaleids._RhythmicKaleid import _RhythmicKaleid
+
+
+class _NoteMaker(_RhythmicKaleid):
+   '''Big-endian note maker.
+   '''
+   
+   ## OVERLOADS ##
+
+   def __call__(self, duration_tokens, seeds = None):
+      seeds = self._none_seeds_to_list(seeds)
+      note_lists = self._make_note_lists(duration_tokens)
+      return note_lists
---
+++ rhythm/kaleids/_NoteMaker/__init__.py
@@ -0,0 +1,1 @@
+from _NoteMaker import _NoteMaker
