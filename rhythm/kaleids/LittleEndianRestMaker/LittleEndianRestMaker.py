8065e55b8059d8a42ae297b262e61221453c02b4
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-23 02:54:31 +0000

    Little-endian rest-maker.

---
+++ rhythm/kaleids/LittleEndianRestMaker/LittleEndianRestMaker.py
@@ -0,0 +1,18 @@
+from abjad.tools import resttools
+from baca.rhythm.kaleids._RestMaker import _RestMaker
+
+
+class LittleEndianRestMaker(_RestMaker):
+   '''Little-endian rest maker.
+
+   See the test file for examples.
+   '''
+
+   ## PRIVATE METHODS ##
+
+   def _make_rest_lists(self, duration_tokens):
+      rest_lists = [ ]
+      for duration_token in duration_tokens:
+         rest_list = resttools.make_rests([duration_token], direction = 'little-endian')
+         rest_lists.append(rest_list)
+      return rest_lists
