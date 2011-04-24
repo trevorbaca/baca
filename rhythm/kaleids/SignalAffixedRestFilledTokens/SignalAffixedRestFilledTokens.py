97c0876dd3eb341a7c99a02a54f11bd3b5105bfe
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-24 08:44:13 +0000

    Signal-affixed rest-filled tokens.

--- rhythm/kaleids/SignalAffixedRestFilledTokens/SignalAffixedRestFilledTokens.py
+++ rhythm/kaleids/SignalAffixedRestFilledTokens/SignalAffixedRestFilledTokens.py
@@ -1,4 +1,5 @@
 from abjad.tools import durtools
+from abjad.tools import leaftools
 from abjad.tools import mathtools
 from abjad.tools import seqtools
 from baca.rhythm.kaleids._RhythmicKaleid import _RhythmicKaleid
@@ -8,64 +9,106 @@ class SignalAffixedRestFilledTokens(_RhythmicKaleid):
    '''Signal-affixed rest-filled tokens.
    '''
 
-   def __init__(self, signal, denominator, prefix_lengths, suffix_lengths,
-      signal_helper = None, prefix_helper = None, suffix_helper = None):
+   def __init__(self, prefix_signal, prefix_denominator, prefix_lengths, 
+      suffix_signal, suffix_denominator, suffix_lengths,
+      prefix_signal_helper = None, prefix_lengths_helper = None,
+      suffix_signal_helper = None, suffix_lengths_helper = None):
       _RhythmicKaleid.__init__(self)
-      assert seqtools.all_are_integer_equivalent_numbers(signal)
-      assert mathtools.is_positive_integer(denominator)
+      assert seqtools.all_are_integer_equivalent_numbers(prefix_signal)
+      assert mathtools.is_positive_integer(prefix_denominator)
       assert seqtools.all_are_nonnegative_integer_equivalent_numbers(prefix_lengths)
+      assert seqtools.all_are_integer_equivalent_numbers(suffix_signal)
+      assert mathtools.is_positive_integer(suffix_denominator)
       assert seqtools.all_are_nonnegative_integer_equivalent_numbers(suffix_lengths)
-      self._signal = signal
-      self._denominator = denominator
+      self._prefix_signal = prefix_signal
+      self._prefix_denominator = prefix_denominator
       self._prefix_lengths = prefix_lengths
+      self._suffix_signal = suffix_signal
+      self._suffix_denominator = suffix_denominator
       self._suffix_lengths = suffix_lengths
-      self._repr_signals.append(signal)
+      self._repr_signals.append(prefix_signal)
       self._repr_signals.append(prefix_lengths)
+      self._repr_signals.append(suffix_signal)
       self._repr_signals.append(suffix_lengths)
-      self._signal_helper = self._handle_none_preprocessor(signal_helper)
-      self._prefix_helper = self._handle_none_preprocessor(prefix_helper)
-      self._suffix_helper = self._handle_none_preprocessor(suffix_helper)
+      self._prefix_signal_helper = self._handle_none_preprocessor(prefix_signal_helper)
+      self._prefix_lengths_helper = self._handle_none_preprocessor(prefix_lengths_helper)
+      self._suffix_signal_helper = self._handle_none_preprocessor(suffix_signal_helper)
+      self._suffix_lengths_helper = self._handle_none_preprocessor(suffix_lengths_helper)
 
    ## OVERLOADS ##
 
    def __call__(self, duration_tokens, seeds = None):
       duration_pairs, seeds = _RhythmicKaleid.__call__(self, duration_tokens, seeds)
       seeds = self._handle_none_seeds(seeds)
-      signal, denominator, duration_pairs = self._scale_signal_denominator_and_duration_pairs(
-         duration_pairs, seeds)
-      print signal, denominator, duration_pairs
-      prefix_lengths = self._prefix_helper(self._prefix_lengths, seeds)
-      suffix_lengths = self._suffix_helper(self._suffix_lengths, seeds)
-      numeric_map = self._make_numeric_map(duration_pairs, signal, prefix_lengths, suffix_lengths)
+      prefix_signal, suffix_signal, denominator, duration_pairs = \
+         self._scale_signal_denominator_and_duration_pairs(duration_pairs, seeds)
+      prefix_lengths = self._prefix_lengths_helper(self._prefix_lengths, seeds)
+      suffix_lengths = self._suffix_lengths_helper(self._suffix_lengths, seeds)
+      numeric_map = self._make_numeric_map(
+         duration_pairs, prefix_signal, prefix_lengths, suffix_signal, suffix_lengths)
       leaf_lists = self._numeric_map_and_denominator_to_leaf_lists(numeric_map, denominator)
       return leaf_lists
 
    ## PRIVATE METHODS ##
 
-   ## TODO: RESUME WORK HERE ON THIS METHOD
-   def _make_numeric_map(self, duration_pairs, signal, prefix_lengths, suffix_lengths):
-      numeric_map = [ ]
-      signal_index = 0
-      signal = seqtools.CyclicTuple(signal)
+   def _make_numeric_map(self, 
+      duration_pairs, prefix_signal, prefix_lengths, suffix_signal, suffix_lengths):
+      numeric_map, prefix_signal_index, suffix_signal_index = [ ], 0, 0
+      prefix_signal = seqtools.CyclicTuple(prefix_signal)
+      suffix_signal = seqtools.CyclicTuple(suffix_signal)
       prefix_lengths = seqtools.CyclicTuple(prefix_lengths)
       suffix_lengths = seqtools.CyclicTuple(suffix_lengths)
       for pair_index, duration_pair in enumerate(duration_pairs):
          numeric_map_part = [ ]
          prefix_length = prefix_lengths[pair_index]
          suffix_length = suffix_lengths[pair_index]
+         prefix = prefix_signal[prefix_signal_index:prefix_signal_index+prefix_length]
+         suffix = suffix_signal[suffix_signal_index:suffix_signal_index+suffix_length]
+         prefix_signal_index += prefix_length
+         suffix_signal_index += suffix_length
+         numerator = duration_pair[0]
+         prefix_weight = mathtools.weight(prefix)
+         suffix_weight = mathtools.weight(suffix)
+         middle = numerator - prefix_weight - suffix_weight
+         if numerator <prefix_weight:
+            weights = [numerator]
+            prefix = seqtools.split_sequence_once_by_weights_without_overhang(prefix, weights)[0]
+         if 0 < middle:
+            middle = (-abs(middle), )
+         else:
+            middle = ( )
+         suffix_space = numerator - prefix_weight
+         if suffix_space <= 0:
+            suffix = ( )
+         elif suffix_space < suffix_weight:
+            weights = [suffix_space]
+            suffix = seqtools.split_sequence_once_by_weights_without_overhang(suffix, weights)[0]
+         numeric_map_part = prefix + middle + suffix
+         numeric_map.append(numeric_map_part)
+      return numeric_map
       
    def _numeric_map_and_denominator_to_leaf_lists(self, numeric_map, denominator):
-      pass
+      leaf_lists = [ ]
+      for numeric_map_part in numeric_map:
+         leaf_list = leaftools.make_leaves_from_note_value_signal(numeric_map_part, denominator)
+         leaf_lists.append(leaf_list)
+      return leaf_lists
 
    def _scale_signal_denominator_and_duration_pairs(self, duration_pairs, seeds):
-      signal = self._signal_helper(self._signal, seeds)
+      prefix_signal = self._prefix_signal_helper(self._prefix_signal, seeds)
+      suffix_signal = self._suffix_signal_helper(self._suffix_signal, seeds)
       duration_pairs = duration_pairs[:]
-      dummy_duration_pair = (1, self._denominator)
-      duration_pairs.append(dummy_duration_pair)
+      dummy_prefix_duration_pair = (1, self._prefix_denominator)
+      dummy_suffix_duration_pair = (1, self._suffix_denominator)
+      duration_pairs.append(dummy_prefix_duration_pair)
+      duration_pairs.append(dummy_suffix_duration_pair)
       duration_pairs = durtools.duration_tokens_to_duration_pairs_with_least_common_denominator(
          duration_pairs)
-      dummy_duration_pair = duration_pairs.pop( )
-      denominator = dummy_duration_pair[1]
-      signal_multiplier = denominator / self._denominator
-      signal = [signal_multiplier * x for x in signal]
-      return signal, denominator, duration_pairs
+      dummy_suffix_duration_pair = duration_pairs.pop( )
+      dummy_prefix_duration_pair = duration_pairs.pop( )
+      denominator = duration_pairs[0][1]
+      prefix_signal_multiplier = denominator / self._prefix_denominator
+      prefix_signal = [prefix_signal_multiplier * x for x in prefix_signal]
+      suffix_signal_multiplier = denominator / self._suffix_denominator
+      suffix_signal = [suffix_signal_multiplier * x for x in suffix_signal]
+      return prefix_signal, suffix_signal, denominator, duration_pairs
