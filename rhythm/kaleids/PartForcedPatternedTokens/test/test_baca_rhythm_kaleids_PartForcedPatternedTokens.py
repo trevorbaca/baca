52a089e704e123a669e2c9b38b767fdf10140dec
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-04-26 21:45:01 +0000

    Part-forced patterned tokens with secondary divisions.

--- rhythm/kaleids/PartForcedPatternedTokens/test/test_baca_rhythm_kaleids_PartForcedPatternedTokens.py
+++ rhythm/kaleids/PartForcedPatternedTokens/test/test_baca_rhythm_kaleids_PartForcedPatternedTokens.py
@@ -182,3 +182,52 @@ def test_baca_rhythm_kaleids_PartForcedPatternedTokens_04( ):
    '''
 
    assert staff.format == "\\new Staff {\n\t{\n\t\t\\time 5/16\n\t\t{\n\t\t\tr32\n\t\t\tc'32\n\t\t\tc'16\n\t\t\tc'8\n\t\t\tc'32\n\t\t\tr32\n\t\t}\n\t}\n\t{\n\t\t\\time 6/16\n\t\t\\times 4/5 {\n\t\t\tr16\n\t\t\tc'8\n\t\t\tc'32\n\t\t\tc'32\n\t\t\tc'16\n\t\t\tc'8\n\t\t\tr32\n\t\t}\n\t}\n}"
+
+
+def test_baca_rhythm_kaleids_PartForcedPatternedTokens_05( ):
+
+   pattern, denominator, prolation_addenda = [1, 1, 2, 4], 32, [0, 3]
+   lefts, middles, rights = [-1], [0], [-1]
+   left_lengths, right_lengths = [1], [1]
+   secondary_divisions = [14]
+   kaleid = PartForcedPatternedTokens(pattern, denominator, prolation_addenda,
+      lefts, middles, rights, left_lengths, right_lengths, secondary_divisions)
+
+   duration_tokens = [(5, 16), (6, 16)]
+   music = kaleid(duration_tokens)
+
+   music = seqtools.flatten_sequence(music)
+   staff = Staff(measuretools.make_measures_with_full_measure_spacer_skips(duration_tokens))
+   measuretools.replace_contents_of_measures_in_expr(staff, music)
+
+   r'''
+   \new Staff {
+      {
+         \time 5/16
+         {
+            r32
+            c'32
+            c'16
+            c'8
+            c'32
+            r32
+         }
+      }
+      {
+         \time 6/16
+         \times 4/7 {
+            r16
+            c'8
+            r32
+         }
+         {
+            r32
+            c'16
+            c'8
+            r32
+         }
+      }
+   }
+   '''
+
+   assert staff.format == "\\new Staff {\n\t{\n\t\t\\time 5/16\n\t\t{\n\t\t\tr32\n\t\t\tc'32\n\t\t\tc'16\n\t\t\tc'8\n\t\t\tc'32\n\t\t\tr32\n\t\t}\n\t}\n\t{\n\t\t\\time 6/16\n\t\t\\times 4/7 {\n\t\t\tr16\n\t\t\tc'8\n\t\t\tr32\n\t\t}\n\t\t{\n\t\t\tr32\n\t\t\tc'16\n\t\t\tc'8\n\t\t\tr32\n\t\t}\n\t}\n}"
