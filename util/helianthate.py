561be932ca13ce9cdaacb12cce4b155916a4f2ff
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-06-19 18:56:31 +0000

    Updates to accommodate new location of Abjad components.

--- util/helianthate.py
+++ util/helianthate.py
@@ -1,4 +1,4 @@
-from abjad.components._Component import _Component
+from abjad.tools.componenttools._Component import _Component
 from abjad.tools import seqtools
 from baca.util.rotate_nested import rotate_nested
 
