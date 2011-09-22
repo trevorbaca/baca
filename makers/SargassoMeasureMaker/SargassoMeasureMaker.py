9894ca1e14ea2f94d7f5221e95f66b65417fc08b
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-09-22 16:57:11 +0000

    Sargasso measure maker.

--- makers/SargassoMeasureMaker/SargassoMeasureMaker.py
+++ makers/SargassoMeasureMaker/SargassoMeasureMaker.py
@@ -18,6 +18,13 @@ class SargassoMeasureMaker(_InteractiveMaterialMaker):
         print 'Making ended.'
         response = raw_input('Press return to continue.')
 
+    def get_possible_meter_multipliers(self, multiplied_measure_numerator):
+        possible_meter_multipliers = []
+        for denominator in range(multiplied_measure_numerator, 2 * multiplied_measure_numerator):
+            possible_meter_multiplier = fractions.Fraction(multiplied_measure_numerator, denominator)
+            possible_meter_multipliers.append(possible_meter_multiplier)
+        return possible_meter_multipliers
+
     def get_user_input(self):
 
         return (4, [2, 2, 2, 2, 1, 1, 4, 4], 
@@ -84,9 +91,38 @@ class SargassoMeasureMaker(_InteractiveMaterialMaker):
             print line
         print ''
 
+    def make_lilypond_file(self, measures):
+        from abjad.tools import leaftools
+        from abjad.tools import lilypondfiletools
+        from abjad.tools import layouttools
+        from abjad.tools import markuptools
+        from abjad.tools import measuretools
+        from abjad.tools import schemetools
+        from abjad.tools import scoretools
+        from abjad.tools import stafftools
+        staff = stafftools.RhythmicStaff(measures)
+        score = scoretools.Score([staff])
+        leaf = leaftools.get_leaf_in_expr_with_minimum_prolated_duration(score)
+        scheme_moment = schemetools.SchemeMoment(fractions.Fraction(2, 3) * leaf.prolated_duration)
+        score.override.bar_number.transparent = True
+        score.set.proportional_notation_duration = scheme_moment
+        measuretools.apply_beam_spanners_to_measures_in_expr(score)
+        scoretools.add_double_bar_to_end_of_score(score)
+        lilypond_file = lilypondfiletools.make_basic_lilypond_file(score) 
+        lilypond_file.default_paper_size = 'letter', 'portrait'
+        lilypond_file.global_staff_size = 14
+        lilypond_file.header_block.title = markuptools.Markup('Sargasso measures')
+        lilypond_file.layout_block.indent = 0
+        lilypond_file.layout_block.ragged_right = True
+        lilypond_file.paper_block.markup_system_spacing = layouttools.make_spacing_vector(0, 0, 12, 0)
+        lilypond_file.paper_block.system_system_spacing = layouttools.make_spacing_vector(0, 0, 10, 0)
+        return lilypond_file
+
     def make_material_interactively(self):
+        from abjad.tools import iotools
         measures = self.make_sargasso_measures(*self.get_user_input()) 
-        print measures
+        lilypond_file = self.make_lilypond_file(measures) 
+        iotools.show(lilypond_file)
         self.conclude()
 
     def make_sargasso_measures(self, measure_denominator, measure_numerator_talea, 
@@ -180,7 +216,6 @@ class SargassoMeasureMaker(_InteractiveMaterialMaker):
 
         return measures
 
-
     def permute_divided_measure_tokens(self, divided_measure_tokens):
         '''This can be extended later.'''
         modulus_of_permutation = 5
@@ -190,14 +225,8 @@ class SargassoMeasureMaker(_InteractiveMaterialMaker):
         divided_measure_tokens = sequencetools.permute_sequence(divided_measure_tokens, permutation)
         return divided_measure_tokens
 
-    def get_possible_meter_multipliers(self, multiplied_measure_numerator):
-        possible_meter_multipliers = []
-        for denominator in range(multiplied_measure_numerator, 2 * multiplied_measure_numerator):
-            possible_meter_multiplier = fractions.Fraction(multiplied_measure_numerator, denominator)
-            possible_meter_multipliers.append(possible_meter_multiplier)
-        return possible_meter_multipliers
-
     def select_meter_multiplier(self, possible_meter_multipliers, measure_index):
         possible_meter_multipliers = sequencetools.CyclicTuple(possible_meter_multipliers)
         meter_multiplier = possible_meter_multipliers[5 * measure_index]
         return meter_multiplier
+
