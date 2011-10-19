3728c24e9b690bdd5ba522c8ebd8246a0d492ea7
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-10-19 20:01:08 +0000

    SCF. Score template wizard.

--- scf/ScoreTemplateWizard/ScoreTemplateWizard.py
+++ scf/ScoreTemplateWizard/ScoreTemplateWizard.py
@@ -3,4 +3,13 @@ from baca.scf.menuing.Wizard import Wizard
 
 class ScoreTemplateWizard(Wizard):
 
-    pass
+    ### PUBLIC METHODS ###
+
+    def run(self):
+        self.clear_terminal()
+        print 'Score template wizard.\n'
+        print 'This wizard works in four largescale steps.\n'
+        print 'Part 1: number of players.\n'
+        response = raw_input("Enter the number of players. Or type 'skip'> ")
+        number_players = eval(response)
+        print number_players
