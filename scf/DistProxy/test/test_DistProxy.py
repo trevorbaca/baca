50ccbec9fdb362bbe21f99fb06628e3f9f4ceafe
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-10-28 20:05:25 +0000

    SCF. Dist, etc, exg proxy tests.

---
+++ scf/DistProxy/test/test_DistProxy.py
@@ -0,0 +1,12 @@
+import baca
+
+
+def test_DistProxy_01():
+
+    dist_proxy = baca.scf.DistProxy('manos')
+
+    assert dist_proxy.directory_name == '/Users/trevorbaca/Documents/scores/manos/dist'
+    assert dist_proxy.has_directory
+    assert dist_proxy.is_in_repository
+    assert dist_proxy.source_file_name == '/Users/trevorbaca/Documents/other/baca/scf/DistProxy/__init__.py'
+    assert dist_proxy.spaced_class_name == 'dist proxy'
