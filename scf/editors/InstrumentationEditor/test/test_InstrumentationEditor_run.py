0d55025787202703bf304278631ad8c7f889e5aa
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-12-06 21:04:02 +0000

    SCF. Instrumentation specifier.

--- scf/editors/InstrumentationEditor/test/test_InstrumentationEditor_run.py
+++ scf/editors/InstrumentationEditor/test/test_InstrumentationEditor_run.py
@@ -2,59 +2,32 @@ import baca
 
 
 def test_InstrumentationEditor_run_01():
-    '''Quit works.
+    '''Quit, back, studio & junk all work.
     '''
 
     studio = baca.scf.Studio()
-    studio.session.user_input = '9 perf q'
+    studio.session.user_input = '1 perf q'
     studio.run()
-
-    assert studio.session.transcript[-2] == [
-      'Sekka (2007) - performers & instrumentation',
-      '',
-      '     1: flutist (flute)',
-      '',
-      '     add: add performer',
-      '     del: delete performer',
-      '']
-
-
-def test_InstrumentationEditor_run_02():
-    '''Back works.
-    '''
+    transcript = studio.session.transcript
+    assert len(transcript) == 6
 
     studio = baca.scf.Studio()
     studio.session.user_input = '1 perf b q'
     studio.run()
     transcript = studio.session.transcript
-
     assert len(transcript) == 8
     assert transcript[2] == transcript[6]
-
-
-def test_InstrumentationEditor_run_03():
-    '''Studio works.
-    '''
 
     studio = baca.scf.Studio()
     studio.session.user_input = '1 perf studio q'
     studio.run()
     transcript = studio.session.transcript
-
     assert len(transcript) == 8
     assert transcript[0] == transcript[6]
-
-
-def test_InstrumentationEditor_run_04():
-    '''Junk works.
-    '''
 
     studio = baca.scf.Studio()
     studio.session.user_input = '1 perf foo q'
     studio.run()
     transcript = studio.session.transcript
-
     assert len(transcript) == 8
     assert transcript[-2] == transcript[-4]
-
-
