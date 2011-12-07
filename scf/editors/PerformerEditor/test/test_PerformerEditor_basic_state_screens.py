a0330cdffa2bf2379dccef1ff09cf9e5c87ca5a1
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-12-07 21:45:57 +0000

    SCF. Performer editor.

---
+++ scf/editors/PerformerEditor/test/test_PerformerEditor_basic_state_screens.py
@@ -0,0 +1,38 @@
+import baca
+
+
+def test_PerformerEditor_basic_state_screens_01():
+    '''Start-up, name, remove name.
+    '''
+
+    editor = baca.scf.editors.PerformerEditor()
+    editor.run(user_input='name foo ren bar rpn q')
+    assert editor.transcript == \
+    [['Performer',
+      '',
+      '     add: add instrument',
+      '     name: name performer',
+      ''],
+     ['SCF> name', ''],
+     ['Performer name> foo', ''],
+     ['Foo',
+      '',
+      '     add: add instrument',
+      '     ren: rename performer',
+      '     rpn: remove performer name',
+      ''],
+     ['SCF> ren', ''],
+     ['New performer name> bar', ''],
+     ['Bar',
+      '',
+      '     add: add instrument',
+      '     ren: rename performer',
+      '     rpn: remove performer name',
+      ''],
+     ['SCF> rpn', ''],
+     ['Performer',
+      '',
+      '     add: add instrument',
+      '     name: name performer',
+      ''],
+     ['SCF> q', '']]
