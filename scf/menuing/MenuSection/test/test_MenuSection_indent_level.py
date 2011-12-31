7c07d115b5e566b709bbeeb907acd9df6c1a09ad
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-12-31 20:27:57 +0000

    SCF. Menuing tests. Indent level.

---
+++ scf/menuing/MenuSection/test/test_MenuSection_indent_level.py
@@ -0,0 +1,12 @@
+import baca
+
+
+def test_MenuSection_indent_level_01():
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section = menu.make_new_section()
+    section.section_title = 'section'
+    section.menu_entry_tokens.extend(['apple', 'banana', 'cherry'])
+
+    assert section.indent_level == 1
