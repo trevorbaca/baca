114380b179b4d87b368f18b5e8a73c7cc0b46c36
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-12-31 20:16:38 +0000

    SCF. Menuing tests. Menu entry bodies, keys, return values.

--- scf/menuing/MenuSection/test/test_MenuSection_menu_entry_bodies.py
+++ scf/menuing/MenuSection/test/test_MenuSection_menu_entry_bodies.py
@@ -2,15 +2,44 @@ import baca
 
 
 def test_MenuSection_menu_entry_bodies_01():
-    '''Menu bodies work regardless menu entry token type.
+    '''Menu entry bodies equal menu entry tokens when menu entry tokens are strings.
+    True whether section is numbered or not.
     '''
 
     menu = baca.scf.menuing.Menu()
     menu.append_breadcrumb('location')
     section = menu.make_new_section()
-    section.title = 'section title'
+    section.section_title = 'section'
+    section.menu_entry_tokens.extend(['apple', 'banana', 'cherry'])
+
+    assert not section.number_menu_entries
+    assert section.menu_entry_bodies == ['apple', 'banana', 'cherry']
+    assert section.menu_entry_bodies == section.menu_entry_tokens
+
+    section.number_menu_entries = True
+    assert section.number_menu_entries
+    assert section.menu_entry_bodies == ['apple', 'banana', 'cherry']
+    assert section.menu_entry_bodies == section.menu_entry_tokens
+
+
+def test_MenuSection_menu_entry_bodies_02():
+    '''Menu entry bodies equal index 1 of menu entry tokens when menu entry tokens are tuples.
+    True whether section is numbered or not.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section = menu.make_new_section()
+    section.section_title = 'section title'
     section.menu_entry_tokens.append(('add', 'add something'))
     section.menu_entry_tokens.append(('del', 'delete something'))
     section.menu_entry_tokens.append(('mod', 'modify something'))
-
+    
+    assert not section.number_menu_entries
     assert section.menu_entry_bodies == ['add something', 'delete something', 'modify something']
+    assert section.menu_entry_bodies == [x[1] for x in section.menu_entry_tokens]
+
+    section.number_menu_entries = True
+    assert section.number_menu_entries
+    assert section.menu_entry_bodies == ['add something', 'delete something', 'modify something']
+    assert section.menu_entry_bodies == [x[1] for x in section.menu_entry_tokens]
--- scf/menuing/MenuSection/test/test_MenuSection_menu_entry_keys.py
+++ scf/menuing/MenuSection/test/test_MenuSection_menu_entry_keys.py
@@ -1,16 +1,44 @@
 import baca
 
 
+# TODO: change this behavior so that menu entry keys are none instead of emtpy strings
 def test_MenuSection_menu_entry_keys_01():
-    '''Menu keys work regardless menu entry token type.
+    '''Menu entry keys equal empty strings when menu entry tokens are strings.
+    True whether section is numbered or not.
     '''
 
     menu = baca.scf.menuing.Menu()
     menu.append_breadcrumb('location')
     section = menu.make_new_section()
-    section.title = 'section title'
+    section.section_title = 'section'
+    section.menu_entry_tokens.extend(['apple', 'banana', 'cherry'])
+
+    assert not section.number_menu_entries
+    assert section.menu_entry_keys == ['', '', '']
+
+    section.number_menu_entries = True
+    assert section.number_menu_entries
+    assert section.menu_entry_keys == ['', '', '']
+
+
+def test_MenuSection_menu_entry_keys_02():
+    '''Menu entry keys equal index 0 of menu entry tokens when menu entry tokens are tuples.
+    True whether section is numbered or not.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section = menu.make_new_section()
+    section.section_title = 'section title'
     section.menu_entry_tokens.append(('add', 'add something'))
     section.menu_entry_tokens.append(('del', 'delete something'))
     section.menu_entry_tokens.append(('mod', 'modify something'))
 
+    assert not section.number_menu_entries
     assert section.menu_entry_keys == ['add', 'del', 'mod']
+    assert section.menu_entry_keys == [x[0] for x in section.menu_entry_tokens]
+
+    section.number_menu_entries = True
+    assert section.number_menu_entries
+    assert section.menu_entry_keys == ['add', 'del', 'mod']
+    assert section.menu_entry_keys == [x[0] for x in section.menu_entry_tokens]
