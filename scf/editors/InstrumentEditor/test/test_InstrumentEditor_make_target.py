1b28552eae9361f4cd105ae6f46d1f9cbcc8d41d
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2011-12-31 01:13:33 +0000

    SCF. Instrumentation.

--- scf/editors/InstrumentEditor/test/test_InstrumentEditor_make_target.py
+++ scf/editors/InstrumentEditor/test/test_InstrumentEditor_make_target.py
@@ -5,5 +5,5 @@ from abjad import *
 def test_InstrumentEditor_make_target_01():
 
     editor = baca.scf.editors.InstrumentEditor()
-    editor.run(user_input='9 q')
-    assert editor.target == instrumenttools.ContrabassFlute()
+    editor.run(user_input='cel q')
+    assert editor.target == instrumenttools.Cello()
