b20f97775a3fbcbd79167557cf0304d9e31a9bab
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-03 00:50:01 +0000

    SCF. Menuing tests.

--- scf/menuing/Menu/test/test_Menu_run.py
+++ scf/menuing/Menu/test/test_Menu_run.py
@@ -99,7 +99,110 @@ def test_Menu_run_05():
       '']
 
 
-#    section_2 = menu.make_new_section()
-#    section_2.menu_entry_tokens.append(('add', 'add something'))
-#    section_2.menu_entry_tokens.append(('del', 'delete something'))
-#    section_2.menu_entry_tokens.append(('mod', 'modify something'))
+def test_Menu_run_06():
+    '''Tuple tokens with default settings.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section_1 = menu.make_new_section(is_keyed=True, is_hidden=False, is_numbered=False, is_ranged=False)
+    section_1.section_title = 'section'
+    section_1 = menu.make_new_section()
+    section_1.menu_entry_tokens.append(('add', 'add something'))
+    section_1.menu_entry_tokens.append(('del', 'delete something'))
+    section_1.menu_entry_tokens.append(('mod', 'modify something'))
+    menu.run(user_input='q')
+    assert menu.transcript[-2] == \
+    ['Location',
+      '',
+      '     Section',
+      '',
+      '     add something (add)',
+      '     delete something (del)',
+      '     modify something (mod)',
+      '']
+
+
+def test_Menu_run_07():
+    '''Tuple tokens with keys turned off.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section_1 = menu.make_new_section(is_keyed=False, is_hidden=False, is_numbered=False, is_ranged=False)
+    section_1.section_title = 'section'
+    section_1.menu_entry_tokens.append(('add', 'add something'))
+    section_1.menu_entry_tokens.append(('del', 'delete something'))
+    section_1.menu_entry_tokens.append(('mod', 'modify something'))
+    menu.run(user_input='q')
+    assert menu.transcript[-2] == \
+    ['Location',
+      '',
+      '     Section',
+      '',
+      '     add something',
+      '     delete something',
+      '     modify something',
+      '']
+
+
+def test_Menu_run_08():
+    '''Hidding suppresses output.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section_1 = menu.make_new_section(is_keyed=True, is_hidden=True, is_numbered=False, is_ranged=False)
+    section_1.section_title = 'section'
+    section_1.menu_entry_tokens.append(('add', 'add something'))
+    section_1.menu_entry_tokens.append(('del', 'delete something'))
+    section_1.menu_entry_tokens.append(('mod', 'modify something'))
+    menu.run(user_input='q')
+    assert menu.transcript[-2] == \
+    ['Location', '']
+
+
+def test_Menu_run_09():
+    '''Tuple tokens with numbering turned on.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section_1 = menu.make_new_section(is_keyed=True, is_hidden=False, is_numbered=True, is_ranged=False)
+    section_1.section_title = 'section'
+    section_1.menu_entry_tokens.append(('add', 'add something'))
+    section_1.menu_entry_tokens.append(('del', 'delete something'))
+    section_1.menu_entry_tokens.append(('mod', 'modify something'))
+    menu.run(user_input='q')
+    assert menu.transcript[-2] == \
+    ['Location',
+      '',
+      '     Section',
+      '',
+      '     1: add something (add)',
+      '     2: delete something (del)',
+      '     3: modify something (mod)',
+      '']
+
+
+def test_Menu_run_10():
+    '''Turning ranges on without numbering does nothing.
+    '''
+
+    menu = baca.scf.menuing.Menu()
+    menu.append_breadcrumb('location')
+    section_1 = menu.make_new_section(is_keyed=True, is_hidden=False, is_numbered=False, is_ranged=True)
+    section_1.section_title = 'section'
+    section_1.menu_entry_tokens.append(('add', 'add something'))
+    section_1.menu_entry_tokens.append(('del', 'delete something'))
+    section_1.menu_entry_tokens.append(('mod', 'modify something'))
+    menu.run(user_input='q')
+    assert menu.transcript[-2] == \
+    ['Location',
+      '',
+      '     Section',
+      '',
+      '     add something (add)',
+      '     delete something (del)',
+      '     modify something (mod)',
+      '']
