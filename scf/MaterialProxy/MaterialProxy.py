aa9177f05c8ea979136f1c7c8956ea1a70f5acff
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-15 19:29:08 +0000

    SCF. Materials management. Deprecating interactive material proxy.

--- scf/MaterialProxy/MaterialProxy.py
+++ scf/MaterialProxy/MaterialProxy.py
@@ -314,11 +314,8 @@ class MaterialProxy(PackageProxy):
     # TODO: write test
     @property
     def user_input_wrapper(self):
-        #if self.is_interactive:
         if self.has_user_input_module:
-            #if self.material_definition_module_importable_name is not None:
             if True:
-                #command = 'from {} import user_input'.format(self.material_definition_module_importable_name)
                 command = 'from {} import user_input'.format(self.user_input_module_importable_name)
                 exec(command)
                 return user_input
@@ -992,3 +989,101 @@ class MaterialProxy(PackageProxy):
         line = 'stub score builder written to disk.'
         if prompt_proceed:
             self.proceed(lines=[line])
+    
+    ### OLD INTERACTIVE MATERIAL PROXY PUBLIC METHODS ###
+
+    def clear_user_input_wrapper(self, user_input_wrapper):
+        for key in user_input_wrapper:
+            user_input_wrapper[key] = None
+        
+    def edit_item(self, key, value):
+        prompt = key.replace('_', ' ')
+        default = repr(value)
+        response = self.handle_raw_input_with_default('{}> '.format(prompt), default=default)
+        command = 'from abjad import *'
+        exec(command)
+        new_value = eval(response)
+        return new_value
+
+    def initialize_user_input_wrapper(self):
+        user_input_wrapper = copy.deepcopy(self.user_input_template)
+        for key in user_input_wrapper:
+            user_input_wrapper[key] = None
+        return user_input_wrapper
+
+    def make_lilypond_file_from_user_input_wrapper(self, user_input_wrapper):
+        material = self.make(*user_input_wrapper.values)
+        lilypond_file = self.make_lilypond_file_from_output_material(material)
+        return lilypond_file
+
+    def overwrite_user_input_wrapper_with_demo_user_input_values(self, user_input_wrapper):
+        for key in self.user_input_template:
+            user_input_wrapper[key] = self.user_input_template[key]    
+
+    def read_user_input_values_from_disk(self):
+        import baca
+        score_wrangler = baca.scf.ScoreWrangler()
+        material_proxy = score_wrangler.select_interactive_material_proxy(klasses=(type(self),))
+        self.user_input_wrapper = copy.deepcopy(material_proxy.user_input_wrapper)
+    
+    def old_run_interactive(self):
+        self._original_score = self.score
+        self._original_material_underscored_name = self.material_underscored_name
+        self._original_user_input_wrapper = copy.deepcopy(self.user_input_wrapper)
+        while True:
+            menu, section = self.make_new_menu(where=self.where(), is_numbered=True)
+            section.tokens = self.user_input_wrapper.editable_lines
+            if self.user_input_wrapper.is_complete:
+                section.append(('p', 'show pdf of given input'))
+                section.append(('m', 'write material to disk'))
+            if self.has_material_underscored_name:
+                section.append(('n', 'rename material'))
+            else:
+                section.append(('n', 'name material'))
+            section.append(('nc', 'clear name'))
+            section.append(('d', 'show demo input values'))
+            section.append(('o', 'overwrite with demo input values'))
+            section.append(('i', 'read values from disk'))
+            section.append(('c', 'clear values'))
+            if self.purview is not None:
+                section.append(('l', 'change location'))
+            else:
+                section.append(('l', 'set location'))
+            result = menu.run()
+            if result == 'c':
+                self.clear_user_input_wrapper(self.user_input_wrapper)
+            elif result == 'd':
+                self.show_demo_user_input_values()
+            elif result == 'i':
+                self.read_user_input_values_from_disk()
+            elif result == 'l':
+                self.set_purview_interactively()
+            elif result == 'n':
+                self.name_material()
+            elif result == 'nc':
+                self.unname_material()
+            elif result == 'o':
+                self.overwrite_user_input_wrapper_with_demo_user_input_values(self.user_input_wrapper)
+            elif result == 'p':
+                lilypond_file = self.make_lilypond_file_from_user_input_wrapper(self.user_input_wrapper)
+                lilypond_file.file_initial_user_includes.append(self.stylesheet)
+                lilypond_file.header_block.title = markuptools.Markup(self.generic_output_name.capitalize())
+                lilypond_file.header_block.subtitle = markuptools.Markup('(unsaved)')
+                iotools.show(lilypond_file)
+            elif result == 'src':
+                self.edit_source_file()
+            elif mathtools.is_integer_equivalent_expr(result):
+                number = int(result)    
+                index = number - 1
+                result, value = self.user_input_wrapper.list_items[index]
+                new_value = self.edit_item(result)
+                self.user_input_wrapper[result] = new_value
+
+    def show_demo_user_input_values(self):
+        menu, section = self.make_new_menu(where=self.where(), is_numbered=True)
+        items = []
+        for i, (key, value) in enumerate(self.user_input_template.iteritems()):
+            item = '{}: {!r}'.format(key.replace('_', ' '), value)
+            items.append(item)
+        section.tokens = items
+        menu.run()
