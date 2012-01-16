e2c99b74734b02997137e955c51252376e467040
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-16 23:12:34 +0000

    SCF. Package management. Package wranglers.

--- scf/NewPackageWrangler/NewPackageWrangler.py
+++ scf/NewPackageWrangler/NewPackageWrangler.py
@@ -18,11 +18,23 @@ class NewPackageWrangler(SCFObject):
         self._toplevel_global_package_importable_name = toplevel_global_package_importable_name
         self._toplevel_score_package_importable_name_body = toplevel_score_package_importable_name_body
 
+    ### OVERLOADS ###
+
+    def __repr__(self):
+        return '{}({!r})'.format(
+            self.class_name, self.toplevel_global_package_importable_name.split('.')[-1])
+
     ### READ-ONLY PUBLIC ATTRIBUTES ###
 
     @property
     def has_toplevel_packages(self):
         for toplevel_package_importable_name in self.toplevel_package_importable_names:
+            return True
+        return False
+
+    @property
+    def has_wrangled_packages(self):
+        for wrangler_package_importable_name in self.wrangled_package_importable_names:
             return True
         return False
 
@@ -34,10 +46,6 @@ class NewPackageWrangler(SCFObject):
             if x[0].isalpha():
                 result.append(x)
         return result
-
-    @property
-    def toplevel_global_package_directory_name(self):
-        return self.package_importable_name_to_directory_name(self.toplevel_global_package_importable_name)
 
     @property
     def toplevel_global_package_importable_name(self):
@@ -63,4 +71,30 @@ class NewPackageWrangler(SCFObject):
             result.append(toplevel_score_package_importable_name)
         return result
 
-    ### PUBLIC METHODS ###
+    @property
+    def wrangled_global_package_importable_names(self):
+        result = []
+        global_package_directory_name = self.package_importable_name_to_directory_name(
+            self.toplevel_global_package_importable_name)
+        for name in os.listdir(global_package_directory_name):
+            if name[0].isalpha():
+                result.append('{}.{}'.format(self.toplevel_global_package_importable_name, name))
+        return result
+
+    @property
+    def wrangled_package_importable_names(self):
+        result = [] 
+        result.extend(self.wrangled_global_package_importable_names)
+        result.extend(self.wrangled_score_package_importable_names)
+        return result
+
+    @property
+    def wrangled_score_package_importable_names(self):
+        result = []
+        for toplevel_score_package_importable_name in self.toplevel_score_package_importable_names:
+            toplevel_score_package_directory_name = self.package_importable_name_to_directory_name(
+                toplevel_score_package_importable_name)
+            for name in os.listdir(toplevel_score_package_directory_name):
+                if name[0].isalpha():
+                    result.append('{}.{}'.format(toplevel_score_package_importable_name, name))
+        return result
