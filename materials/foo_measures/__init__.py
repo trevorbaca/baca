0b4b4ff38fc52728900b67df21c14b1a56eb57be
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-26 19:14:36 +0000

    SCF. Updated materials initializers.

--- materials/foo_measures/__init__.py
+++
@@ -1,5 +0,0 @@
-from collections import OrderedDict
-
-
-tags = OrderedDict([
-    ('editor', 'SargassoMeasureMaterialPackageMaker')])
--- materials/red_numbers/__init__.py
+++ materials/red_numbers/__init__.py
@@ -1,5 +1,6 @@
+from baca.scf.helpers import safe_import
 from collections import OrderedDict
-from output_material import red_numbers
+safe_import(globals(), 'output_material', 'red_numbers')
 
 
 tags = OrderedDict([
