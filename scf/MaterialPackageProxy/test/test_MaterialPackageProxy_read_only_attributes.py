5b7f303b1eb4bb3e90b8f535d4de4a93a9795863
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-27 19:50:50 +0000

    SCF. Materials management. Material package proxy tests.

--- scf/MaterialPackageProxy/test/test_MaterialPackageProxy_read_only_attributes.py
+++ scf/MaterialPackageProxy/test/test_MaterialPackageProxy_read_only_attributes.py
@@ -1,4 +1,5 @@
 from abjad.tools import measuretools
+from abjad.tools import notetools
 import baca
 import py
 
@@ -139,68 +140,68 @@ def test_MaterialPackageProxy_read_only_attributes_02():
 def test_MaterialPackageProxy_read_only_attributes_03():
     '''Handmade material.
     '''
-    py.test.skip()
 
     mpp = baca.scf.MaterialPackageProxy('baca.materials.red_notes')
-    assert     mpp.breadcrumb == 'red sargasso'
-    assert not mpp.has_illustration_builder_module
+    assert     mpp.breadcrumb == 'red notes'
+    assert     mpp.has_illustration_builder_module
     assert     mpp.has_illustration_ly
     assert     mpp.has_illustration_pdf
-    assert not mpp.has_material_definition
-    assert not mpp.has_material_definition_module
-    assert     mpp.has_material_package_maker
+    assert     mpp.has_material_definition
+    assert     mpp.has_material_definition_module
+    assert not mpp.has_material_package_maker
     assert     mpp.has_output_material
     assert     mpp.has_output_material_module
-    assert     mpp.has_user_input_module
-    assert     mpp.has_user_input_wrapper        
-    assert not mpp.illustration is not None
-    assert     mpp.illustration_builder_module_file_name is None
-    assert     mpp.illustration_builder_module_importable_name is None
-    assert     mpp.illustration_builder_module_proxy is None
+    assert not mpp.has_user_input_module
+    assert not mpp.has_user_input_wrapper        
+    assert     mpp.illustration is not None
+    assert     mpp.illustration_builder_module_file_name == \
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes/illustration_builder.py'
+    assert     mpp.illustration_builder_module_importable_name == \
+        'baca.materials.red_notes.illustration_builder'
+    assert     mpp.illustration_builder_module_proxy is not None
     assert     mpp.illustration_ly_file_name == \
-        '/Users/trevorbaca/Documents/other/baca/materials/red_sargasso/illustration.ly'
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes/illustration.ly'
     assert     mpp.illustration_ly_file_proxy is not None
     assert     mpp.illustration_pdf_file_name == \
-        '/Users/trevorbaca/Documents/other/baca/materials/red_sargasso/illustration.pdf'
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes/illustration.pdf'
     assert     mpp.illustration_pdf_file_proxy is not None
     # TODO:
     #assert not mpp.is_changed   
     assert not mpp.is_data_only
-    assert not mpp.is_handmade
-    assert     mpp.is_makermade
-    assert     mpp.material_definition_module_file_name is None
-    assert     mpp.material_definition_module_importable_name is None
-    assert     mpp.material_definition_module_proxy is None
+    assert     mpp.is_handmade
+    assert not mpp.is_makermade
+    assert     mpp.material_definition_module_file_name == \
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes/material_definition.py'
+    assert     mpp.material_definition_module_importable_name == \
+        'baca.materials.red_notes.material_definition'
+    assert     mpp.material_definition_module_proxy is not None
     assert     mpp.material_package_directory == \
-        '/Users/trevorbaca/Documents/other/baca/materials/red_sargasso'
-    assert     mpp.material_package_maker is baca.scf.materialpackagemakers.SargassoMeasureMaterialPackageMaker
-    assert     mpp.material_package_maker_class_name == 'SargassoMeasureMaterialPackageMaker'
-    assert     mpp.material_package_short_name == 'red_sargasso'
-    assert     mpp.material_spaced_name == 'red sargasso'
-    assert     mpp.material_underscored_name == 'red_sargasso'
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes'
+    assert     mpp.material_package_maker is None
+    assert     mpp.material_package_maker_class_name is None
+    assert     mpp.material_package_short_name == 'red_notes'
+    assert     mpp.material_spaced_name == 'red notes'
+    assert     mpp.material_underscored_name == 'red_notes'
     assert     mpp.materials_directory_name == \
         '/Users/trevorbaca/Documents/other/baca/materials'
     assert     mpp.materials_package_importable_name == 'baca.materials'
-    # TODO:
-    #assert     measuretools.all_are_measures(mpp.output_material)
-    assert     mpp.output_material_module_body_lines is None
+    assert     notetools.all_are_notes(mpp.output_material)
+    assert     mpp.output_material_module_body_lines is not None
     assert     mpp.output_material_module_file_name == \
-        '/Users/trevorbaca/Documents/other/baca/materials/red_sargasso/output_material.py'
+        '/Users/trevorbaca/Documents/other/baca/materials/red_notes/output_material.py'
     assert     mpp.output_material_module_importable_name == \
-        'baca.materials.red_sargasso.output_material'
+        'baca.materials.red_notes.output_material'
     assert      mpp.output_material_module_proxy is not None
     assert      mpp.should_have_illustration
-    assert not  mpp.should_have_illustration_builder_module
+    assert      mpp.should_have_illustration_builder_module
     assert      mpp.should_have_illustration_ly
     assert      mpp.should_have_illustration_pdf
-    assert not  mpp.should_have_material_definition_module
+    assert      mpp.should_have_material_definition_module
     assert      mpp.should_have_output_material_module
-    assert      mpp.should_have_user_input_module
+    assert not  mpp.should_have_user_input_module
     # TODO:
-    #assert      mpp.source_stylesheet_file_name is None
-    #assert      mpp.source_stylesheet_file_proxy is None
-    assert      mpp.user_input_module_file_name == \
-        '/Users/trevorbaca/Documents/other/baca/materials/red_sargasso/user_input.py'
-    assert      mpp.user_input_module_importable_name == \
-        'baca.materials.red_sargasso.user_input'
-    assert      mpp.user_input_module_proxy is not None
+    #assert      mpp.source_stylesheet_file_name == 'something'
+    #assert      mpp.source_stylesheet_file_proxy is not None
+    assert      mpp.user_input_module_file_name is None
+    assert      mpp.user_input_module_importable_name is None
+    assert      mpp.user_input_module_proxy is None
