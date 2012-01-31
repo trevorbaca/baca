467eefea2b5dfeb228a812b22473460191f06cfc
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-01-31 02:06:24 +0000

    SCF. Assets. Canned testdata material definition.

--- scf/assets/stub_material_definition.py
+++
@@ -1,5 +0,0 @@
-from abjad import *
-
-
-data = None
-output_material_module_import_statements = ['from abjad import *', '']
--- scf/assets/stub_score_builder.py
+++
@@ -1,6 +0,0 @@
-from abjad import *
-from output_material import data
-
-
-score, treble_staff, bass_staff = scoretools.make_piano_score_from_leaves()
-lilypond_file = lilypondfiletools.make_basic_lilypond_file(score)
