fa6af05181b172bd028ecb906d91b378087955c1
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-11 23:34:04 +0000

    SCF. Tests. Sargasso measure maker.

--- scf/MaterialPackageMaker/MaterialPackageMaker.py
+++ scf/MaterialPackageMaker/MaterialPackageMaker.py
@@ -54,7 +54,7 @@ class MaterialPackageMaker(MaterialPackageProxy):
             self.user_input_module_proxy.write_to_disk(self.user_input_wrapper_in_memory)
             self.proceed('user input wrapper cleared and written to disk.', prompt=prompt)
 
-    def edit_user_input_at_number(self, number):
+    def edit_user_input_wrapper_at_number(self, number, include_newline=True):
         if self.user_input_wrapper_in_memory is None:
             return
         if len(self.user_input_wrapper_in_memory) < number:
@@ -78,6 +78,7 @@ class MaterialPackageMaker(MaterialPackageProxy):
         getter.tests.append(test)
         getter.execs[-1].append('from abjad import *')
         getter.execs[-1].append(exec_string)
+        getter.include_newlines = include_newline
         new_value = getter.run()
         if self.backtrack():
             return
@@ -123,7 +124,7 @@ class MaterialPackageMaker(MaterialPackageProxy):
         return output_material
 
     def populate_user_input_wrapper(self, prompt=True):
-        total_elements = len(self.self.user_input_wrapper_in_memory)
+        total_elements = len(self.user_input_wrapper_in_memory)
         getter = self.make_new_getter(where=self.where())
         getter.append_integer_in_range('start at element number', 1, total_elements, default=1)
         self.push_backtrack()
@@ -134,7 +135,7 @@ class MaterialPackageMaker(MaterialPackageProxy):
         current_element_index = current_element_number - 1
         while True:
             self.push_backtrack()
-            self.edit_user_input_at_number(current_element_number)
+            self.edit_user_input_wrapper_at_number(current_element_number, include_newline=False)
             self.pop_backtrack()
             if self.backtrack():
                 return
--- scf/materialpackagemakers/SargassoMeasureMaterialPackageMaker/test/test_SargassoMeasureMaterialPackageMaker.py
+++ scf/materialpackagemakers/SargassoMeasureMaterialPackageMaker/test/test_SargassoMeasureMaterialPackageMaker.py
@@ -11,6 +11,34 @@ def test_SargassoMeasureMaterialPackageMaker_01():
     try:
         studio.run(user_input=
             'materials maker sargasso testsargasso default '
+            'q'
+            )
+        mpp = baca.scf.materialpackagemakers.SargassoMeasureMaterialPackageMaker(
+            'baca.materials.testsargasso')
+        assert mpp.directory_contents == ['__init__.py', 'user_input.py']
+        user_input_wrapper = UserInputWrapper([
+            ('measure_denominator', None),
+            ('measure_numerator_talea', None),
+            ('measure_division_denominator', None),
+            ('measure_division_talea', None),
+            ('total_duration', None),
+            ('measures_are_scaled', None),
+            ('measures_are_split', None),
+            ('measures_are_shuffled', None)])
+        assert mpp.user_input_wrapper_in_memory == user_input_wrapper
+    finally:
+        studio.run(user_input='m testsargasso del remove default q')
+        assert not studio.package_exists('baca.materials.testsargasso')
+
+
+def test_SargassoMeasureMaterialPackageMaker_02():
+
+    studio = baca.scf.Studio()
+    assert not studio.package_exists('baca.materials.testsargasso')
+
+    try:
+        studio.run(user_input=
+            'materials maker sargasso testsargasso default '
             'testsargasso uil '
             'q'
             )
@@ -26,6 +54,89 @@ def test_SargassoMeasureMaterialPackageMaker_01():
             ('measures_are_scaled', True),
             ('measures_are_split', True),
             ('measures_are_shuffled', True)])
+        assert mpp.user_input_wrapper_in_memory == user_input_wrapper
+    finally:
+        studio.run(user_input='m testsargasso del remove default q')
+        assert not studio.package_exists('baca.materials.testsargasso')
+
+
+def test_SargassoMeasureMaterialPackageMaker_03():
+
+    studio = baca.scf.Studio()
+    assert not studio.package_exists('baca.materials.testsargasso')
+
+    try:
+        studio.run(user_input=
+            'materials maker sargasso testsargasso default '
+            'testsargasso uil uic '
+            'q'
+            )
+        mpp = baca.scf.materialpackagemakers.SargassoMeasureMaterialPackageMaker(
+            'baca.materials.testsargasso')
+        assert mpp.directory_contents == ['__init__.py', 'user_input.py']
+        user_input_wrapper = UserInputWrapper([
+            ('measure_denominator', None),
+            ('measure_numerator_talea', None),
+            ('measure_division_denominator', None),
+            ('measure_division_talea', None),
+            ('total_duration', None),
+            ('measures_are_scaled', None),
+            ('measures_are_split', None),
+            ('measures_are_shuffled', None)])
+        assert mpp.user_input_wrapper_in_memory == user_input_wrapper
+    finally:
+        studio.run(user_input='m testsargasso del remove default q')
+        assert not studio.package_exists('baca.materials.testsargasso')
+
+
+def test_SargassoMeasureMaterialPackageMaker_04():
+    studio = baca.scf.Studio()
+    assert not studio.package_exists('baca.materials.testsargasso')
+    try:
+        studio.run(user_input=
+            'materials maker sargasso testsargasso default '
+            'testsargasso 3 16 '
+            'q'
+            )
+        mpp = baca.scf.materialpackagemakers.SargassoMeasureMaterialPackageMaker(
+            'baca.materials.testsargasso')
+        assert mpp.directory_contents == ['__init__.py', 'user_input.py']
+        user_input_wrapper = UserInputWrapper([
+            ('measure_denominator', None),
+            ('measure_numerator_talea', None),
+            ('measure_division_denominator', 16),
+            ('measure_division_talea', None),
+            ('total_duration', None),
+            ('measures_are_scaled', None),
+            ('measures_are_split', None),
+            ('measures_are_shuffled', None)])
+        assert mpp.user_input_wrapper_in_memory == user_input_wrapper
+    finally:
+        studio.run(user_input='m testsargasso del remove default q')
+        assert not studio.package_exists('baca.materials.testsargasso')
+
+
+def test_SargassoMeasureMaterialPackageMaker_05():
+    studio = baca.scf.Studio()
+    assert not studio.package_exists('baca.materials.testsargasso')
+    try:
+        studio.run(user_input=
+            'materials maker sargasso testsargasso default '
+            'testsargasso 3 16 '
+            'q'
+            )
+        mpp = baca.scf.materialpackagemakers.SargassoMeasureMaterialPackageMaker(
+            'baca.materials.testsargasso')
+        assert mpp.directory_contents == ['__init__.py', 'user_input.py']
+        user_input_wrapper = UserInputWrapper([
+            ('measure_denominator', None),
+            ('measure_numerator_talea', None),
+            ('measure_division_denominator', 16),
+            ('measure_division_talea', None),
+            ('total_duration', None),
+            ('measures_are_scaled', None),
+            ('measures_are_split', None),
+            ('measures_are_shuffled', None)])
         assert mpp.user_input_wrapper_in_memory == user_input_wrapper
     finally:
         studio.run(user_input='m testsargasso del remove default q')
