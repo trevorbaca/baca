16f1d1c21e288b76517d621cc95381e59e1fdaf5
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-16 19:45:44 +0000

    SCF. Menuing. List editor.

--- scf/editors/ListEditor/ListEditor.py
+++ scf/editors/ListEditor/ListEditor.py
@@ -1,18 +1,86 @@
+from abjad.tools import sequencetools
 from baca.scf.editors.InteractiveEditor import InteractiveEditor
 
 
-def ListEditor(InteractiveEditor):
+class ListEditor(InteractiveEditor):
 
     ### READ-ONLY ATTRIBUTES ###
 
     target_item_class = None
-
     target_item_editor_class = None
-
     target_item_identifier = 'element'
-     
     target_items_identifier = 'elements'
 
     ### PUBLIC METHODS ###
 
-    
+    def add_target_item_interactively(self):
+        getter = self.make_getter(where=self.where())
+        self.target_item_getter_configuration_method(getter, self.target_item_identifier)
+        self.push_backtrack()
+        target_item_initialization_token = getter.run()
+        self.pop_backtrack()
+        if self.backtrack():
+            return
+        target_item = self.target_item_class(target_item_initialization_token)
+        self.target_items.append(target_item)
+
+    def edit_target_item_interactively(self, target_item_number):
+        target_item = self.get_target_itme_from_target_item_number(target_item_number)
+        if target_item is not None:
+            target_item_editor = self.target_item_editor_class(session=self.session, target=target_item)
+            target_item_editor.run()
+
+    def get_target_item_from_target_item_number(self, target_item_number):
+        try:
+            return self.target_items[int(target_item_number) - 1]
+        except:
+            pass
+
+    def handle_main_menu_result(self, result):
+        if not isinstance(result, str):
+            raise TypeError('result must be string.')
+        if result == 'add':
+            self.add_target_item_interactively()
+        elif result == 'del':
+            self.remove_target_items_interactively()
+        elif result == 'mv':
+            self.move_target_item_interactively()
+        else:
+            self.edit_target_item_interactively(result)
+
+    def make_main_menu(self):
+        menu, section = self.make_menu(where=self.where(), is_numbered=True)
+        section.tokens = self.summary_lines
+        section.return_value_attribute = 'number'
+        section = menu.make_section(is_keyed=False)
+        section.append(('add', 'add {}'.format(self.target_item_identifier)))
+        if 0 < len(self.target_items):
+            section.append(('del', 'delete {}'.format(self.target_items_identifier)))
+        if 1 < len(self.target_items):
+            section.append(('mv', 'move {}'.format(self.target_items_identifier)))
+        return menu
+
+    def move_target_item_interactively(self):
+        getter = self.make_getter(where=self.where())
+        getter.append_integer_in_range('old number', 1, len(self.target_items))
+        getter.append_integer_in_range('new number', 1, len(self.target_items))
+        result = getter.run()
+        if self.backtrack():
+            return
+        old_number, new_number = result
+        old_index, new_index = old_number - 1, new_number - 1
+        target_item = self.target_items[old_index]
+        self.target_items.remove(target_item)
+        self.target_items.insert(new_index, target_item)
+
+    def remove_target_items_interactively(self):
+        getter = self.make_getter(where=self.where())
+        getter.append_argument_range(self.target_items_identifier, self.summary_lines)
+        argument_range = getter.run()
+        if self.backtrack():
+            return
+        indices = [argument_number - 1 for argument_number in argument_range]
+        indices = list(reversed(sorted(set(indices))))
+        target_items = self.target_items[:]
+        target_items = sequencetools.remove_sequence_elements_at_indices(target_items, indices)
+        self.target_items[:] = target_items
