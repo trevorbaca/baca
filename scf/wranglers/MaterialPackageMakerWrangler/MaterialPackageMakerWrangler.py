9a3a5d03b686a1ae7bb317c9d726392d57057426
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-20 21:22:22 +0000

    SCF. Material package maker wrangler.

--- scf/wranglers/MaterialPackageMakerWrangler/MaterialPackageMakerWrangler.py
+++ scf/wranglers/MaterialPackageMakerWrangler/MaterialPackageMakerWrangler.py
@@ -8,7 +8,11 @@ import os
 class MaterialPackageMakerWrangler(PackageWrangler):
 
     def __init__(self, session=None):
-        PackageWrangler.__init__(self, self.makers_package_importable_name, session=session)
+        PackageWrangler.__init__(self, 
+            score_external_wrangler_target_package_importable_name= \
+                self.makers_package_importable_name, 
+            score_internal_wrangler_target_package_importable_name_suffix=None,
+            session=session)
 
     ### READ-ONLY PUBLIC ATTRIBUTES ###
 
@@ -17,9 +21,9 @@ class MaterialPackageMakerWrangler(PackageWrangler):
         return 'material proxies'
 
     @property
-    def material_proxy_spaced_class_names(self):
+    def wrangled_proxy_lowercase_spaced_class_names(self):
         result = []
-        for name in self.list_wrangled_package_short_names(head=self.home_package_importable_name):
+        for name in self.list_wrangled_package_short_names():
             spaced_class_name = iotools.uppercamelcase_to_space_delimited_lowercase(name)
             result.append(spaced_class_name)
         result.remove('material package maker')
@@ -27,35 +31,36 @@ class MaterialPackageMakerWrangler(PackageWrangler):
 
     ### PUBLIC METHODS ###
 
-    def get_wrangled_package_proxy(self, material_package_importable_name):
-        import baca
-        material_proxy = baca.scf.proxies.MaterialPackageProxy(material_package_importable_name, session=self.session)
-        material_package_maker_class_name = material_proxy.material_package_maker_class_name
+    def get_wrangled_package_proxy(self, package_importable_name):
+        from baca.scf.proxies.MaterialPackageProxy import MaterialPackageProxy
+        material_package_proxy = MaterialPackageProxy(package_importable_name, session=self.session)
+        material_package_maker_class_name = material_package_proxy.material_package_maker_class_name
         if material_package_maker_class_name is not None:
             material_proxy_class = None
             command = 'from baca.scf.makers import {} as material_proxy_class'
             command = command.format(material_package_maker_class_name)
             exec(command)
-            material_proxy = material_proxy_class(material_package_importable_name, session=self.session)
-        return material_proxy
+            material_package_proxy = material_proxy_class(package_importable_name, session=self.session)
+        return material_package_proxy
             
     def handle_main_menu_result(self, result):
         if result == 'new':
-            self.make_material_proxy_interactively()
+            self.make_wrangled_package_interactively()
         else:
             raise ValueError
 
     def make_main_menu(self):
         menu, section = self.make_menu(where=self.where(), is_numbered=True)
-        section.tokens = self.material_proxy_spaced_class_names
+        section.tokens = self.wrangled_proxy_lowercase_spaced_class_names
         section = menu.make_section()
-        section.append(('new', 'make material_proxy'))
+        section.append(('new', 'new material package maker'))
         return menu
 
     # TODO: implement MaterialPackageProxyClassFile object to model and customize these settings
     def make_material_proxy_class_file(self, material_proxy_name, generic_output_name):
         class_file_name = os.path.join(
-            self.score_external_wrangler_target_package_importable_name, material_proxy_name, material_proxy_name + '.py')
+            self.score_external_wrangler_target_package_importable_name, 
+            material_proxy_name, material_proxy_name + '.py')
         class_file = file(class_file_name, 'w')
         lines = []
         lines.append('from baca.music.foo import foo')
@@ -114,25 +119,20 @@ class MaterialPackageMakerWrangler(PackageWrangler):
         initializer.write("_import_structured_package(__path__[0], globals(), 'baca')\n")
         initializer.close() 
 
-    def make_material_proxy_interactively(self):
+    def make_wrangled_package_interactively(self):
         getter = self.make_getter(where=self.where())
-        getter.append_string('material proxy name')
-        material_proxy_name = getter.run()
+        getter.append_material_package_maker_class_name('material proxy name')
+        getter.append_space_delimited_lowercase_string('generic output product')
+        result = getter.run()
         if self.backtrack():
             return
-        assert iotools.is_uppercamelcase_string(material_proxy_name)
-        assert material_proxy_name.endswith('Maker')
-        getter = self.make_getter(where=self.where())
-        getter.append_string('generic output product')
-        generic_output_product = getter.run()
-        if self.backtrack():
-            return
-        material_proxy_directory = os.path.join(self.score_external_wrangler_target_package_importable_name, material_proxy_name)
+        material_package_maker_class_name, generic_output_product_name = result
+        material_proxy_directory = os.path.join(
+            self.score_external_wrangler_target_package_importable_name, material_package_maker_class_name)
         os.mkdir(material_proxy_directory)
-        self.make_material_proxy_initializer(material_proxy_name)
-        self.make_material_proxy_class_file(material_proxy_name, generic_output_product)
-        self.make_material_proxy_stylesheet(material_proxy_name)
-
+        self.make_material_proxy_initializer(material_package_maker_class_name)
+        self.make_material_proxy_class_file(material_package_maker_class_name, generic_output_product_name)
+        self.make_material_proxy_stylesheet(material_package_maker_class_name)
 
     # TODO: change to boilerplate file stored somewhere
     def make_material_proxy_stylesheet(self, material_proxy_name):
@@ -174,7 +174,7 @@ class MaterialPackageMakerWrangler(PackageWrangler):
     def select_material_package_proxy_class_name_interactively(self, clear=True, cache=False):
         self.cache_breadcrumbs(cache=cache)
         menu, section = self.make_menu(where=self.where(), is_numbered=True)
-        section.tokens = self.material_proxy_spaced_class_names
+        section.tokens = self.wrangled_proxy_lowercase_spaced_class_names
         while True:
             self.push_breadcrumb('select material proxy:')
             result = menu.run(clear=clear)
