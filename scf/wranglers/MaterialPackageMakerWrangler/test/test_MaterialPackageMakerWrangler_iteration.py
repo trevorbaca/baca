d90d1add054c8ef218d0b5d22df00c5320b4bee2
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-20 23:25:32 +0000

    SCF. Tests. Material package maker wrangler.

---
+++ scf/wranglers/MaterialPackageMakerWrangler/test/test_MaterialPackageMakerWrangler_iteration.py
@@ -0,0 +1,42 @@
+import baca
+
+studio = baca.scf.studio.Studio()
+wrangler = studio.material_package_maker_wrangler
+
+
+def test_MaterialPackageMakerWrangler_iteration_01():
+
+    assert wrangler.list_score_internal_wrangled_package_importable_names() == []
+
+
+def test_MaterialPackageMakerWrangler_iteration_02():
+
+    assert wrangler.list_score_internal_wrangler_target_package_importable_names() == []
+
+
+def test_MaterialPackageMakerWrangler_iteration_03():
+
+    assert 'PitchRangeInventoryMaterialPackageMaker' in \
+        wrangler.list_visible_wrangled_package_short_names()
+
+
+def test_MaterialPackageMakerWrangler_iteration_04():
+
+    assert 'baca.scf.makers.PitchRangeInventoryMaterialPackageMaker' in \
+        wrangler.list_wrangled_package_importable_names()
+    
+
+def test_MaterialPackageMakerWrangler_iteration_05():
+
+    assert ('PitchRangeInventoryMaterialPackageMaker', 
+        'pitch range inventory material package maker') in wrangler.list_wrangled_package_menuing_pairs()
+
+
+def test_MaterialPackageMakerWrangler_iteration_06():
+
+    assert 'PitchRangeInventoryMaterialPackageMaker' in wrangler.list_wrangled_package_short_names()
+
+    
+def test_MaterialPackageMakerWrangler_iteration_07():
+
+    assert 'PitchRangeInventoryMaterialPackageMaker' in wrangler.list_wrangled_package_spaced_names()
