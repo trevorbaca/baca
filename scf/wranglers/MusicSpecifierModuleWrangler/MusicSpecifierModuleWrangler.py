d0a5cb8bccf1f725af14134bb17d9e2e293ac916
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-20 23:59:56 +0000

    SCF. Music-specifier module wrangler.

--- scf/wranglers/MusicSpecifierModuleWrangler/MusicSpecifierModuleWrangler.py
+++ scf/wranglers/MusicSpecifierModuleWrangler/MusicSpecifierModuleWrangler.py
@@ -3,7 +3,6 @@ from baca.scf.wranglers.PackageWrangler import PackageWrangler
 import os
 
 
-# TODO: write tests
 class MusicSpecifierModuleWrangler(PackageWrangler):
 
     def __init__(self, session=None):
@@ -20,63 +19,43 @@ class MusicSpecifierModuleWrangler(PackageWrangler):
     def breadcrumb(self):
         return 'music specifiers'
 
-    @property
-    def specifier_spaced_names(self):
-        self.list_wrangled_package_spaced_names
-
-    @property
-    def specifiers_directory_name(self):
-        return self.package_importable_name_to_directory_name(self.specifiers_package_importable_name)
-
-    @property
-    def specifiers_package_importable_name(self):
-        if self.session.is_in_score:
-            score_package_short_name = self.session.current_score_package_short_name
-            return self.dot_join([score_package_short_name, self.score_internal_wrangler_target_package_importable_name_suffix])
-        else:
-            return self.score_external_wrangler_target_package_importable_name
-            
     ### PUBLIC METHODS ###
 
-    def conditionally_make_specifiers_package(self):
-        if self.session.is_in_score:
-            if not self.package_exists(self.specifiers_package_importable_name):
-                os.mkdir(self.specifiers_directory_name)
-            if not os.path.exists(self.score_external_wrangler_target_initializer_file_name):
-                file_pointer = open(self.score_external_wrangler_target_initializer_file_name, 'w')
-                file_pointer.write('')
-                file_pointer.close()
+    def get_wrangled_package_proxy(self, package_importable_name):
+        return MusicSpecifierModuleProxy(package_importable_name, session=self.session)
 
     def handle_main_menu_result(self, result):
         if result == 'new':
-            self.make_specifier_interactively()
+            self.make_wrangled_package_interactively()
+        elif result == 'missing':
+            self.conditionally_make_wrangler_target_packages(is_interactive=True)
+        elif result == 'profile':
+            self.profile_visible_wrangled_package_structures()
         else:
-            specifier_module_name = self.dot_join([self.specifiers_package_importable_name, result])
-            specifier_proxy = MusicSpecifierModuleProxy(specifier_module_importable_name, session=self.session)
-            specifier_proxy.run()
+            package_proxy = self.get_wrangled_package_proxy(result)
+            package_proxy.run()
 
-    def make_main_menu(self):
+    def make_main_menu(self, head=None):
         menu, section = self.make_menu(where=self.where(), is_parenthetically_numbered=True)
-        self.debug(self.specifier_spaced_names)
-        section.tokens = self.specifier_spaced_names
+        section.tokens = self.list_wrangled_package_menuing_pairs(head=head)
         section = menu.make_section()
         section.append(('new', 'new music specifier'))
+        hidden_section = menu.make_section(is_hidden=True)
+        hidden_section.append(('missing', 'create missing packages'))
+        hidden_section.append(('profile', 'profile packages'))
         return menu
 
-    def make_specifier_interactively(self):
+    def make_wrangled_package_interactively(self):
         getter = self.make_getter()
         getter.append_space_delimited_lowercase_string('music specifier name')
         specifier_name = getter.run()
         if self.backtrack():
             return
-        specifier_module_short_name = specifier_name.replace(' ', '_')
-        specifier_module_importable_name = self.dot_join([
-            self.specifiers_package_importable_name, specifier_module_short_name])
-        specifier_module_proxy = MusicSpecifierModuleProxy(specifier_module_importable_name)
-        #specifier_module_proxy.run()
+        package_short_name = specifier_name.replace(' ', '_')
+        self.make_wrangled_package(package_short_name)
 
     def run(self, user_input=None, clear=True, cache=False):
-        self.conditionally_make_specifiers_package()
+        self.conditionally_make_wrangler_target_packages()
         self.assign_user_input(user_input=user_input)
         self.cache_breadcrumbs(cache=cache)
         while True:
@@ -96,10 +75,10 @@ class MusicSpecifierModuleWrangler(PackageWrangler):
         self.restore_breadcrumbs(cache=cache)
 
     # TODO: write test
-    def select_specifier_spaced_name_interactively(self, clear=True, cache=False):
+    def select_specifier_spaced_name_interactively(self, cache=False, clear=True, head=None):
         self.cache_breadcrumbs(cache=cache)
         menu, section = self.make_menu(where=self.where(), is_parenthetically_numbered=True)
-        section.tokens = self.specifier_spaced_names
+        section.tokens = self.list_wrangled_package_menuing_pairs(head=head)
         while True:
             self.push_breadcrumb('select music specifier')
             result = menu.run(clear=clear)
