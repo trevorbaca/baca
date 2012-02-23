cf8b44f73bc5a952be0d58a86c59fed3ded92113
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-23 16:54:00 +0000

    SCF. Music specifier editor.

--- scf/editors/MusicSpecifierEditor/test/test_MusicSpecifierEditor_run.py
+++ scf/editors/MusicSpecifierEditor/test/test_MusicSpecifierEditor_run.py
@@ -2,41 +2,16 @@ import baca
 
 
 def test_MusicSpecifierEditor_run_01():
-    '''With target.
-    '''
 
-    pcs_1 = baca.scf.specifiers.PerformerContributionSpecifier()
-    pcs_1.articulation_specifier = 'foo'
-    pcs_1.clef_specifier = 'bar'
-    pcs_1.directive_specifier = ['apple', 'banana', 'cherry']
-
-    pcs_2 = baca.scf.specifiers.PerformerContributionSpecifier()
-    pcs_2.articulation_specifier = 'blee'
-    pcs_2.clef_specifier = 'blah'
-    pcs_2.directive_specifier = ['durian']
-
-    ms = baca.scf.specifiers.MusicSpecifier()
-    ms.music_specifier_name = 'blue music'
-    ms.tempo = 90
-    ms.performer_contribution_specifiers.extend([pcs_1, pcs_2])
+    editor = baca.scf.editors.MusicSpecifierEditor()
+    editor.run(user_input='1 blue~music q')
 
     r'''
     MusicSpecifier(
         music_specifier_name='blue music',
         performer_contribution_specifiers=PerformerContributionSpecifierList(
-            PerformerContributionSpecifier(
-                articulation_specifier='foo',
-                clef_specifier='bar',
-                directive_specifier=['apple', 'banana', 'cherry'],
-                ),
-            PerformerContributionSpecifier(
-                articulation_specifier='blee',
-                clef_specifier='blah',
-                directive_specifier=['durian'],
-                ),
             ),
-        tempo=90,
         )
     '''
 
-    editor = baca.scf.editors.MusicSpecifierEditor(target=ms)
+    assert editor.target.format == "MusicSpecifier(\n\tmusic_specifier_name='blue music',\n\tperformer_contribution_specifiers=PerformerContributionSpecifierList(\n\t\t),\n\t)"
