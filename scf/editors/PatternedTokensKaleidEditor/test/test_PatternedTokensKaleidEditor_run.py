9cb904b9c1fa3d4fa19a318f9b44cc5573ff3256
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-02-29 19:53:10 +0000

    SCF. Tests. Patterned-tokens kaleid editor.

---
+++ scf/editors/PatternedTokensKaleidEditor/test/test_PatternedTokensKaleidEditor_run.py
@@ -0,0 +1,15 @@
+from rhythm import kaleids
+import scf
+
+
+def test_PatternedTokensKaleidEditor_run_01():
+
+    editor = scf.editors.PatternedTokensKaleidEditor()
+    editor.run(user_input='2 16 [2, 3] [6] [-1, 2, -3, 4] q')
+
+    kaleid = kaleids.PatternedTokens([-1, 2, -3, 4], 16,
+        prolation_addenda=[2, 3],
+        secondary_divisions=[6],
+        )
+
+    assert editor.target == kaleid
