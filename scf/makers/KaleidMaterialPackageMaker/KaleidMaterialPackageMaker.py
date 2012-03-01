0218e81bdede5f142d29e1bbfe729dc0f8889112
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-01 20:14:22 +0000

    SCF. Kaleid package maker. Output material now stored with tools package qualifiers.

--- scf/makers/KaleidMaterialPackageMaker/KaleidMaterialPackageMaker.py
+++ scf/makers/KaleidMaterialPackageMaker/KaleidMaterialPackageMaker.py
@@ -17,13 +17,14 @@ class KaleidMaterialPackageMaker(MaterialPackageMaker):
     output_material_checker = staticmethod(lambda x: isinstance(x, _RhytmicKaleid))
     output_material_editor = staticmethod(get_kaleid_editor)
     output_material_creation_wizard = KaleidWizard
-    output_material_module_import_statements = ['from kaleids import *']
+    output_material_module_import_statements = ['import kaleids']
 
     ### PUBLIC METHODS ###
 
     def make_output_material_module_body_lines(self, output_material):
         lines = []
-        lines.append('{} = {}('.format(self.material_underscored_name, output_material._class_name))
+        lines.append('{} = {}('.format(
+            self.material_underscored_name, output_material._class_name_with_tools_package))
         for input_parameter in output_material._formatted_input_parameters[:-1]:
             lines.append('\t{},'.format(input_parameter))
         line = output_material._formatted_input_parameters[-1]
