f501e27b6a082d100aa0657b3a5a9b35599e76f3
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-04 00:41:24 +0000

    SCF. Signal-affixed note-filled tokens kaleid editor.

---
+++ scf/editors/SignalAffixedNoteFilledTokensKaleidEditor/SignalAffixedNoteFilledTokensKaleidEditor.py
@@ -0,0 +1,18 @@
+from scf.editors.KaleidEditor import KaleidEditor
+from scf.editors.TargetManifest import TargetManifest
+from scf import getters
+import kaleids
+
+
+class SignalAffixedNoteFilledTokensKaleidEditor(KaleidEditor):
+
+    ### CLASS ATTRIBUTES ###
+
+    target_class = kaleids.SignalAffixedNoteFilledTokens
+    target_manifest = TargetManifest(kaleids.SignalAffixedNoteFilledTokens,
+        ('prefix_signal', None, 'ps', getters.get_nonzero_integers, True),
+        ('prefix_lengths', None, 'pl', getters.get_nonnegative_integers, True),
+        ('suffix_signal', None, 'ss', getters.get_nonzero_integers, True),
+        ('suffix_lengths', None, 'sl', getters.get_nonnegative_integers, True),
+        ('denominator', None, 'de', getters.get_positive_integer_power_of_two, True),
+        )
