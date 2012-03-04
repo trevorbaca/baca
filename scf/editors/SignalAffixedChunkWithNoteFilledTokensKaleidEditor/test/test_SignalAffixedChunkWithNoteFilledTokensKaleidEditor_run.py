d11b97577453a16d68f8ab4f1c9f825bffb1c181
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-04 00:52:29 +0000

    SCF. Signal-affixed chunk with note-filled tokens kaleid editor.

---
+++ scf/editors/SignalAffixedChunkWithNoteFilledTokensKaleidEditor/test/test_SignalAffixedChunkWithNoteFilledTokensKaleidEditor_run.py
@@ -0,0 +1,12 @@
+import kaleids
+import scf
+
+
+def test_SignalAffixedChunkWithNoteFilledTokensKaleidEditor_run_01():
+
+    editor = scf.editors.SignalAffixedChunkWithNoteFilledTokensKaleidEditor()
+    editor.run(user_input='1 [-8] [2] [-3] [4] 32 q', is_autoadvancing=True)
+
+    kaleid = kaleids.SignalAffixedChunkWithNoteFilledTokens([-8], [2], [-3], [4], 32)
+
+    assert editor.target == kaleid
