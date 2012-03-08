159e1619933d4df0711dec5875505d07e5e78aef
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-08 23:34:32 +0000

    SCF. Tests. Octave transposition mapping editor.

--- scf/editors/OctaveTranspositionMappingEditor/test/test_OctaveTranspositionMappingEditor_run.py
+++ scf/editors/OctaveTranspositionMappingEditor/test/test_OctaveTranspositionMappingEditor_run.py
@@ -5,7 +5,23 @@ import scf
 def test_OctaveTranspositionMappingEditor_run_01():
 
     editor = scf.editors.OctaveTranspositionMappingEditor()
-    editor.run(user_input='add add source [A0, F#4] target 22 done add source (F#4, C8] target 26 done done done')
+    editor.run(user_input='add source [A0, F#4] target 22 done add source (F#4, C8] target 26 done done done')
 
     mapping = pitchtools.OctaveTranspositionMapping([('[A0, F#4]', 22), ('(F#4, C8]', 26)])
+    assert editor.target == mapping
+
+
+def test_OctaveTranspositionMappingEditor_run_02():
+    '''Named mapping.
+    '''
+
+    editor = scf.editors.OctaveTranspositionMappingEditor()
+    editor.run(user_input='name piccolo~second~octave '
+        'add source [A0, F#4] target 22 done '
+        'add source (F#4, C8] target 26 done done done ')
+
+    mapping = pitchtools.OctaveTranspositionMapping(
+            [('[A0, F#4]', 22), ('(F#4, C8]', 26)], 
+            inventory_name='piccolo second octave')
+
     assert editor.target == mapping
