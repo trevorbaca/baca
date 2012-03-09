85a90348d985371cf67090df95cbf0f8a48f6c7e
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-09 01:05:12 +0000

    SCF. Tests. Octave transposition mapping inventory.

--- scf/editors/OctaveTranspositionMappingInventoryEditor/test/test_OctaveTranspositionMappingInventoryEditor_run.py
+++ scf/editors/OctaveTranspositionMappingInventoryEditor/test/test_OctaveTranspositionMappingInventoryEditor_run.py
@@ -3,6 +3,8 @@ import scf
 
 
 def test_OctaveTranspositionMappingInventoryEditor_run_01():
+    '''Empty inventories.
+    '''
 
     editor = scf.editors.OctaveTranspositionMappingInventoryEditor()
     editor.run(user_input='done')
@@ -22,7 +24,31 @@ def test_OctaveTranspositionMappingInventoryEditor_run_01():
 
 
 def test_OctaveTranspositionMappingInventoryEditor_run_02():
+    '''Empty named inventory.
+    '''
 
     editor = scf.editors.OctaveTranspositionMappingInventoryEditor()
     editor.run(user_input='name foo done')
     assert editor.target == pitchtools.OctaveTranspositionMappingInventory(inventory_name='foo')
+
+
+def test_OctaveTranspositionMappingInventoryEditor_run_03():
+    '''Named inventory with named mapping.
+    '''
+
+    editor = scf.editors.OctaveTranspositionMappingInventoryEditor()
+    editor.run(user_input=
+        'name mapping~inventory '
+        'add name piccolo~strict~first~octave '
+        'add source [A0, C8] target 14 '
+        'done done done '
+        )
+
+    inventory = pitchtools.OctaveTranspositionMappingInventory(
+        [pitchtools.OctaveTranspositionMapping(
+            [('[A0, C8]', 14)], 
+            inventory_name='piccolo strict first octave')], 
+        inventory_name='mapping inventory'
+        )
+
+    assert editor.target == inventory
