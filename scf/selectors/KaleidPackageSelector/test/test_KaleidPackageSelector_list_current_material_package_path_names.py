102e6e5d2b306efcabbb42728b171b2aafe5fa50
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-13 17:28:39 +0000

    SCF. Streamlined material package selector.

--- scf/selectors/KaleidPackageSelector/test/test_KaleidPackageSelector_list_current_material_package_path_names.py
+++ scf/selectors/KaleidPackageSelector/test/test_KaleidPackageSelector_list_current_material_package_path_names.py
@@ -2,11 +2,11 @@ import os
 import scf
 
 
-def test_KaleidPackageSelector_list_material_package_path_names_in_current_score_01():
+def test_KaleidPackageSelector_list_current_material_package_path_names_01():
 
     selector = scf.selectors.KaleidPackageSelector()
     selector.session._current_score_package_short_name = 'betoerung'
     speckled_kaleid_path_name = os.path.join(
         os.environ.get('SCORES'), 'betoerung', 'mus', 'materials', 'speckled_kaleid')
 
-    assert speckled_kaleid_path_name in selector.list_material_package_path_names_in_current_score()
+    assert speckled_kaleid_path_name in selector.list_current_material_package_path_names()
--- scf/selectors/MaterialPackageSelector/MaterialPackageSelector.py
+++ scf/selectors/MaterialPackageSelector/MaterialPackageSelector.py
@@ -11,38 +11,18 @@ class MaterialPackageSelector(Selector):
 
     ### PUBLIC METHODS ###
 
-    def list_material_package_path_names_in_current_score(self):
+    def list_current_material_package_path_names(self):
         result = []
-        if self.session.is_in_score:
-            current_materials_package_importable_name = self.dot_join([
-                self.session.current_score_package_short_name, 'mus', 'materials'])
-            current_materials_package_path_name = self.package_importable_name_to_path_name(
-                current_materials_package_importable_name)
-            for package_path_name in self.list_public_package_path_names_in_subtree(
-                current_materials_package_path_name):
-                if self.get_tag_from_path_name(package_path_name, 'generic_output_name') == \
-                    self.generic_output_name:
-                    result.append(package_path_name)
+        for package_path_name in self.list_public_package_path_names_in_subtree(
+            self.session.current_materials_package_path_name):
+            if self.get_tag_from_path_name(package_path_name, 'generic_output_name') == \
+                self.generic_output_name:
+                result.append(package_path_name)
         return result
-
-    # TODO: impelement me
-#    def list_current_material_package_path_names(self):
-#        result = []
-#        if self.session.is_in_score:
-#            current_materials_package_importable_name = self.dot_join([
-#                self.session.current_score_package_short_name, 'mus', 'materials'])
-#            current_materials_package_path_name = self.package_importable_name_to_path_name(
-#                current_materials_package_importable_name)
-#            for package_path_name in self.list_public_package_path_names_in_subtree(
-#                current_materials_package_path_name):
-#                if self.get_tag_from_path_name(package_path_name, 'generic_output_name') == \
-#                    self.generic_output_name:
-#                    result.append(package_path_name)
-#        return result
 
     def list_items(self):
         result = []
-        for path_name in self.list_material_package_path_names_in_current_score():
+        for path_name in self.list_current_material_package_path_names():
             package_importable_name = self.path_name_to_package_importable_name(path_name)
             result.append(package_importable_name)
         return result
