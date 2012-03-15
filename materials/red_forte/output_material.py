82a03f0719400e3ef30091f5db159bf2a8bd4a8c
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-15 21:11:23 +0000

    renamed green forte to red forte.

--- materials/red_forte/output_material.py
+++ materials/red_forte/output_material.py
@@ -2,7 +2,7 @@ from abjad.tools import durationtools
 import handlers
 
 
-green_forte = handlers.dynamics.ReiteratedDynamicHandler(
+red_forte = handlers.dynamics.ReiteratedDynamicHandler(
 	dynamic_name='f',
 	minimum_prolated_duration=durationtools.Duration(
 		1,
