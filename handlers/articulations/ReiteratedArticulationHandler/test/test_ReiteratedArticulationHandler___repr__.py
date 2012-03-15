2316666046419d34399cff880029a786234f7c42
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-15 18:19:52 +0000

    SCF. Tests. Articulation handler.

--- handlers/articulations/ReiteratedArticulationHandler/test/test_ReiteratedArticulationHandler___repr__.py
+++ handlers/articulations/ReiteratedArticulationHandler/test/test_ReiteratedArticulationHandler___repr__.py
@@ -10,3 +10,5 @@ def test_ReiteratedArticulationHandler___repr___01():
         maximum_prolated_duration = Duration(1, 8))
 
     assert repr(handler) == "ReiteratedArticulationHandler(articulation_list=['.', '^'], minimum_prolated_duration=Fraction(1, 16), maximum_prolated_duration=Fraction(1, 8))"
+
+    assert handler._tools_package_qualified_indented_repr == "handlers.articulations.ReiteratedArticulationHandler(\n\tarticulation_list=['.', '^'],\n\tminimum_prolated_duration=Fraction(1, 16),\n\tmaximum_prolated_duration=Fraction(1, 8)\n\t)"
