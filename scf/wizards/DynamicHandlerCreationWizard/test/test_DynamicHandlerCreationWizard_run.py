29519cd99fd392b4504911e7d61617d06cd439a7
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-15 19:47:06 +0000

    SCF. Handler makers all now autostart.

--- scf/wizards/DynamicHandlerCreationWizard/test/test_DynamicHandlerCreationWizard_run.py
+++ scf/wizards/DynamicHandlerCreationWizard/test/test_DynamicHandlerCreationWizard_run.py
@@ -6,7 +6,7 @@ import scf
 def test_DynamicHandlerCreationWizard_run_01():
 
     wizard = scf.wizards.DynamicHandlerCreationWizard()
-    wizard.run(user_input='reit 1 f (1, 16) done')
+    wizard.run(user_input='reit f (1, 16) done')
 
     handler = handlers.dynamics.ReiteratedDynamicHandler(
         dynamic_name='f',
