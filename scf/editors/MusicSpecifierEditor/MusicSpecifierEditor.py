045ad36565bd886fbed44e9e9d69c712f51e5c75
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-03-18 16:41:23 +0000

    SCF. Added object inventory editor.

--- scf/editors/MusicSpecifierEditor/MusicSpecifierEditor.py
+++ scf/editors/MusicSpecifierEditor/MusicSpecifierEditor.py
@@ -1,11 +1,11 @@
 from scf import getters
 from scf import specifiers
-from scf.editors.ListEditor import ListEditor
+from scf.editors.ObjectInventoryEditor import ObjectInventoryEditor
 from scf.editors.MusicContributionSpecifierEditor import MusicContributionSpecifierEditor
 from scf.editors.TargetManifest import TargetManifest
 
 
-class MusicSpecifierEditor(ListEditor):
+class MusicSpecifierEditor(ObjectInventoryEditor):
 
     ### CLASS ATTRIBUTES ###
 
@@ -17,11 +17,3 @@ class MusicSpecifierEditor(ListEditor):
         ('inventory_name', 'name', 'nm', getters.get_string, False),
         target_attribute_name='inventory_name',
         )
-
-    ### PUBLIC READ-ONLY PROPERTIES ###
-
-    # TODO: abstract up to ObjectInventoryEditor
-    @property
-    def target_name(self):
-        if self.target is not None:
-            return self.target.inventory_name
