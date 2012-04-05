5ba470c73b0460b13742df5d76919cb730771ca2
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-05 20:05:07 +0000

    Moving time token makers to Abjad.

--- scf/editors/NoteFilledTimeTokenMakerEditor/NoteFilledTimeTokenMakerEditor.py
+++ scf/editors/NoteFilledTimeTokenMakerEditor/NoteFilledTimeTokenMakerEditor.py
@@ -1,12 +1,12 @@
+from abjad.tools import timetokentools
 from scf import getters
 from scf.editors.KaleidEditor import KaleidEditor
 from scf.editors.TargetManifest import TargetManifest
-import handlers
 
 
 class NoteFilledTimeTokenMakerEditor(KaleidEditor):
 
     ### CLASS ATTRIBUTES ###
 
-    target_manifest = TargetManifest(handlers.kaleids.NoteFilledTimeTokenMaker,
+    target_manifest = TargetManifest(timetokentools.NoteFilledTimeTokenMaker,
         )
