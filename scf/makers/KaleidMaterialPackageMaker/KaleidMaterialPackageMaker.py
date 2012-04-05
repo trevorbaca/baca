6a872a51d1788cea70e613e7095c82f60ba5fb55
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-05 19:23:15 +0000

    Moving time token makers to Abjad.

--- scf/makers/KaleidMaterialPackageMaker/KaleidMaterialPackageMaker.py
+++ scf/makers/KaleidMaterialPackageMaker/KaleidMaterialPackageMaker.py
@@ -2,7 +2,7 @@ from make_illustration_from_output_material import make_illustration_from_output
 from scf.editors.get_kaleid_editor import get_kaleid_editor
 from scf.makers.MaterialPackageMaker import MaterialPackageMaker
 from scf.wizards.KaleidCreationWizard import KaleidCreationWizard
-from handlers.kaleids.TimeTokenMaker import TimeTokenMaker
+from abjad.tools.timetokentools.TimeTokenMaker import TimeTokenMaker
 
 
 class KaleidMaterialPackageMaker(MaterialPackageMaker):
