8c2fa78a9ce2a8e0afd0381ce5d0436ef3650961
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-05 20:46:38 +0000

    Moving time-token makers to Abjad.

--- handlers/kaleids/OutputBurnishedSignalFilledTimeTokenMaker/__init__.py
+++
@@ -1,1 +0,0 @@
-from OutputBurnishedSignalFilledTimeTokenMaker import OutputBurnishedSignalFilledTimeTokenMaker
--- scf/editors/SignalFilledTimeTokenMakerEditor/SignalFilledTimeTokenMakerEditor.py
+++ scf/editors/SignalFilledTimeTokenMakerEditor/SignalFilledTimeTokenMakerEditor.py
@@ -1,14 +1,14 @@
+from abjad.tools import timetokentools
 from scf.editors.KaleidEditor import KaleidEditor
 from scf.editors.TargetManifest import TargetManifest
 from scf import getters
-import handlers
 
 
 class SignalFilledTimeTokenMakerEditor(KaleidEditor):
 
     ### CLASS ATTRIBUTES ###
 
-    target_manifest = TargetManifest(handlers.kaleids.SignalFilledTimeTokenMaker,
+    target_manifest = TargetManifest(timetokentools.SignalFilledTimeTokenMaker,
         ('pattern', None, 'pa', getters.get_nonzero_integers, True),
         ('denominator', None, 'de', getters.get_positive_integer_power_of_two, True),
         ('prolation_addenda', None, 'ad', getters.get_integers, False),
