56032bbfa5ae0bfdf118594d0152b4d25667717a
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-06 00:15:58 +0000

    Moving time-token makers to Abjad.

--- scf/selectors/PackageContentSelector/test/test_PackageContentSelector_run.py
+++ scf/selectors/PackageContentSelector/test/test_PackageContentSelector_run.py
@@ -1,10 +1,10 @@
-import handlers
+from abjad.tools import timetokentools
 import scf
 
 
 def test_PackageContentSelector_run_01():
 
     selector = scf.selectors.PackageContentSelector()
-    selector.asset_container_package_importable_names = ['handlers.kaleids']
+    selector.asset_container_package_importable_names = ['abjad.tools.timetokentools']
 
-    assert selector.run(user_input='notefilled') == handlers.kaleids.NoteFilledTimeTokenMaker
+    assert selector.run(user_input='notefilled') == timetokentools.NoteFilledTimeTokenMaker
