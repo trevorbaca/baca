48e2f998d7e43c31ff26c2a7e6e80f4250445861
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-06 00:13:26 +0000

    Moving time-token makers to Abjad.

--- scf/selectors/TimeTokenMakerClassNameSelector/TimeTokenMakerClassNameSelector.py
+++ scf/selectors/TimeTokenMakerClassNameSelector/TimeTokenMakerClassNameSelector.py
@@ -6,6 +6,6 @@ class TimeTokenMakerClassNameSelector(HandlerClassNameSelector):
 
     ### CLASS ATTRIBUTES ###
     
-    asset_container_package_importable_names = ['handlers.kaleids']
-    asset_container_path_names = [os.path.join(os.environ.get('HANDLERS'), 'kaleids')]
-    target_human_readable_name = 'kaleid class name'
+    asset_container_package_importable_names = ['abjad.tools.timetokentools']
+    asset_container_path_names = [os.path.join(os.environ.get('ABJAD'), 'tools', 'timetokentools')]
+    target_human_readable_name = 'time-token maker class name'
