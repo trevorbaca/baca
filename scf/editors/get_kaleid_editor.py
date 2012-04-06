4b0ee13d67910e8e05aeae795520a92995f4f097
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-04-06 00:33:10 +0000

    Moving time-token makers to Abjad.

--- scf/editors/get_kaleid_editor.py
+++
@@ -1,6 +0,0 @@
-def get_kaleid_editor(target=None, session=None):
-    from scf.wizards.TimeTokenMakerCreationWizard import TimeTokenMakerCreationWizard
-    if target:
-        wizard = TimeTokenMakerCreationWizard()
-        kaleid_editor = wizard.get_handler_editor(target._class_name, target=target)
-        return kaleid_editor
--- scf/makers/TimeTokenMakerMaterialPackageMaker/TimeTokenMakerMaterialPackageMaker.py
+++ scf/makers/TimeTokenMakerMaterialPackageMaker/TimeTokenMakerMaterialPackageMaker.py
@@ -1,17 +1,17 @@
 from make_illustration_from_output_material import make_illustration_from_output_material
-from scf.editors.get_kaleid_editor import get_kaleid_editor
+from scf.editors.get_time_token_maker_editor import get_time_token_maker_editor
 from scf.makers.MaterialPackageMaker import MaterialPackageMaker
 from scf.wizards.TimeTokenMakerCreationWizard import TimeTokenMakerCreationWizard
 from abjad.tools.timetokentools.TimeTokenMaker import TimeTokenMaker
 
 
-class KaleidMaterialPackageMaker(MaterialPackageMaker):
+class TimeTokenMakerMaterialPackageMaker(MaterialPackageMaker):
 
     ### CLASS ATTRIBUTES ###
 
     generic_output_name = 'kaleid'
     illustration_maker = staticmethod(make_illustration_from_output_material)
-    output_material_checker = staticmethod(lambda x: isinstance(x, _RhytmicKaleid))
-    output_material_editor = staticmethod(get_kaleid_editor)
+    output_material_checker = staticmethod(lambda x: isinstance(x, TimeTokenMaker))
+    output_material_editor = staticmethod(get_time_token_maker_editor)
     output_material_maker = TimeTokenMakerCreationWizard
-    output_material_module_import_statements = ['import handlers']
+    output_material_module_import_statements = ['from abjad.tools import timetokentools']
