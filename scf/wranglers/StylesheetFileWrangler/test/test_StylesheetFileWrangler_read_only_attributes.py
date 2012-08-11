866e6dc62ae8eb8912e058d997bc4da212bf1d35
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-08-11 19:23:20 +0000

    Changed 'other/baca' to 'baca' globally.

--- scf/wranglers/StylesheetFileWrangler/test/test_StylesheetFileWrangler_read_only_attributes.py
+++ scf/wranglers/StylesheetFileWrangler/test/test_StylesheetFileWrangler_read_only_attributes.py
@@ -6,5 +6,5 @@ def test_StylesheetFileWrangler_read_only_attributes_01():
     studio = scf.studio.Studio()
     wrangler = studio.stylesheet_file_wrangler
 
-    assert '/Users/trevorbaca/Documents/other/baca/scf/stylesheets/clean_letter_14.ly' in \
+    assert '/Users/trevorbaca/Documents/baca/scf/stylesheets/clean_letter_14.ly' in \
         wrangler.list_score_external_asset_path_names()
