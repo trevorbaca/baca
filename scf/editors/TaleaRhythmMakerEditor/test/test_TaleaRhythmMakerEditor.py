dfdc9debb20db0f77023ae9c1ca946b7e4ebe48c
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2012-11-15 22:27:58 +0000

    Removing 'Filled' from all rhythm-maker class names.

--- scf/editors/TaleaRhythmMakerEditor/test/test_TaleaRhythmMakerEditor.py
+++ scf/editors/TaleaRhythmMakerEditor/test/test_TaleaRhythmMakerEditor.py
@@ -2,7 +2,7 @@ from abjad.tools import rhythmmakertools
 import scf
 
 
-def test_TaleaRhythmMakerEditor_run_01():
+def test_TaleaRhythmMakerEditor_01():
 
     editor = scf.editors.TaleaRhythmMakerEditor()
     editor.run(user_input='2 16 [2, 3] [6] [-1, 2, -3, 4] q', is_autoadvancing=True)
