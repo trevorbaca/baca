e4d7233510608a1532c2b3092a1b97c6055074fa
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2013-08-19 22:51:19 +0000

    Integrated NamedPitch.

--- pitch/Constellation/test/test_baca_pitch_Constellation_pitch_range.py
+++ pitch/Constellation/test/test_baca_pitch_Constellation_pitch_range.py
@@ -5,5 +5,5 @@ import baca
 def test_baca_pitch_Constellation_pitch_range_01():
 
     assert baca.pitch.CC[0].pitch_range == pitchtools.PitchRange(
-        (pitchtools.NamedChromaticPitch('a,,,'), 'inclusive'),
-        (pitchtools.NamedChromaticPitch("c'''''"), 'inclusive'))
+        (pitchtools.NamedPitch('a,,,'), 'inclusive'),
+        (pitchtools.NamedPitch("c'''''"), 'inclusive'))
