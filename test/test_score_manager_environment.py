3348fa80c6ea623a38c742acbc67de9043171761
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2014-04-07 14:30:49 +0000

    Updated score manager test.

--- test/test_score_manager_environment.py
+++ test/test_score_manager_environment.py
@@ -4,11 +4,7 @@ import scoremanager
 def test_score_manager_environment_01():
 
     score_manager = scoremanager.core.ScoreManager()
-    score_manager._run('q', display_active_scores=True)
+    input_ = 'ssv q'
+    score_manager._run(pending_user_input=input_)
 
-    lines = score_manager._transcript.entries[0].lines
-    for line in lines:
-        if 'Sekka (2007)' in line:
-            break
-    else:
-        raise Exception('Sekka not found.')
+    assert 'Sekka (2007)' in score_manager._transcript.contents
