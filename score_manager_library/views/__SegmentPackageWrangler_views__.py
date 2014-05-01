c735bd6c41e5cd21ecca2416bb579970ce1288bf
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2014-05-01 22:14:51 +0000

    Views.

---
+++ score_manager_library/views/__SegmentPackageWrangler_views__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from scoremanager import iotools
+
+
+view_inventory=datastructuretools.TypedOrderedDict(
+    [
+        (
+            'flamingo',
+            iotools.View(
+                [
+                    'segment 03 (Red Example Score)',
+                    'segment 02 (Red Example Score)',
+                    'segment 01 (Red Example Score)',
+                    ]
+                ),
+            ),
+        ]
+    )
--- score_manager_library/views/__metadata__.py
+++ score_manager_library/views/__metadata__.py
@@ -4,5 +4,10 @@ import collections
 
 metadata = collections.OrderedDict([
     ('BuildFileWrangler_view_name', None),
+    ('DistributionFileWrangler_view_name', None),
+    ('MakerModuleWrangler_view_name', None),
+    ('MaterialPackageWrangler_view_name', None),
+    ('ScorePackageWrangler_view_name', '_test'),
+    ('SegmentPackageWrangler_view_name', None),
     ('StylesheetWrangler_view_name', None),
     ])
