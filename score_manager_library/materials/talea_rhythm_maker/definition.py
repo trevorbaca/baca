b3dade4e5abcec16690301bdded301b6dede3036
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2014-06-30 18:09:38 +0000

    Adjust talea rhythm-maker.

--- score_manager_library/materials/talea_rhythm_maker/definition.py
+++ score_manager_library/materials/talea_rhythm_maker/definition.py
@@ -2,13 +2,28 @@
 from abjad import *
 
 
-test_talea_rythm_maker = rhythmmakertools.TaleaRhythmMaker(
+talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker(
     talea=rhythmmakertools.Talea(
-        counts=(1, 2, 3, 4),
-        denominator=16,
+        counts=(2, 2, 2, 4, 1),
+        denominator=32,
         ),
+    split_divisions_by_counts=(2, 2, 2),
+    extra_counts_per_division=(0, 0, 1, 9),
     beam_specifier=rhythmmakertools.BeamSpecifier(
         beam_each_division=True,
         beam_divisions_together=True,
+        ),
+    burnish_specifier=rhythmmakertools.BurnishSpecifier(
+        burnish_divisions=False,
+        burnish_output=False,
+        lefts=(),
+        ),
+    duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+        decrease_durations_monotonically=True,
+        forbidden_written_duration=durationtools.Duration(1, 2),
+        ),
+    tie_specifier=rhythmmakertools.TieSpecifier(
+        tie_across_divisions=True,
+        tie_split_notes=True,
         ),
     )
--- score_manager_library/materials/talea_rhythm_maker/output.py
+++ score_manager_library/materials/talea_rhythm_maker/output.py
@@ -2,9 +2,26 @@
 from abjad import *
 
 
-test_talea_rythm_maker = rhythmmakertools.TaleaRhythmMaker(
+talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker(
     talea=rhythmmakertools.Talea(
-        counts=(1, 2, 3, 4),
-        denominator=16,
+        counts=(2, 2, -3),
+        denominator=8,
+        ),
+    split_divisions_by_counts=(2, 1),
+    beam_specifier=rhythmmakertools.BeamSpecifier(
+        beam_each_division=True,
+        beam_divisions_together=True,
+        ),
+    burnish_specifier=rhythmmakertools.BurnishSpecifier(
+        burnish_divisions=False,
+        burnish_output=False,
+        ),
+    duration_spelling_specifier=rhythmmakertools.DurationSpellingSpecifier(
+        decrease_durations_monotonically=True,
+        forbidden_written_duration=durationtools.Duration(1, 2),
+        ),
+    tie_specifier=rhythmmakertools.TieSpecifier(
+        tie_across_divisions=True,
+        tie_split_notes=True,
         ),
     )
