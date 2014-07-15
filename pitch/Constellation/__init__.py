468df001452f3b3104e355af39ccb93dead31dc6
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2014-07-15 18:04:49 +0000

    Removed package_root_name keyword from ImportManager calls.

--- pitch/Constellation/__init__.py
+++ pitch/Constellation/__init__.py
@@ -1,8 +1,8 @@
 #from Constellation import Constellation
 from abjad.tools import systemtools
 
+
 systemtools.ImportManager.import_structured_package(
     __path__[0],
     globals(),
-    package_root_name='baca',
     )
--- pitch/ConstellationCircuit/__init__.py
+++ pitch/ConstellationCircuit/__init__.py
@@ -1,8 +1,8 @@
 #from ConstellationCircuit import ConstellationCircuit
 from abjad.tools import systemtools
 
+
 systemtools.ImportManager.import_structured_package(
     __path__[0],
     globals(),
-    package_root_name='baca',
     )
