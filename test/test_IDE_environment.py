277c81a5120289c77750f205ee68d382edd62ed5
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2014-09-29 01:23:05 +0000

    Added StringSoloScoreTemplate.

--- test/test_IDE_environment.py
+++ test/test_IDE_environment.py
@@ -11,4 +11,4 @@ def test_IDE_environment_01():
     contents = score_manager._transcript.contents
 
     assert '5: Chrysanthemums (1995)' in contents
-    assert '20: Zeit (1998)' in contents
+    assert '21: Zeit (1998)' in contents
