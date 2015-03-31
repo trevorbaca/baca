1482fa3a115100832bc71c07adaa715eaca0b41c
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-03-31 21:55:55 +0000

    Added tied notes rhythm-maker to library.

---
+++ library/materials/tied_notes/definition.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+tied_notes = rhythmmakertools.NoteRhythmMaker(
+    tie_specifier=rhythmmakertools.TieSpecifier(
+        tie_across_divisions=True,
+        ),
+    )
