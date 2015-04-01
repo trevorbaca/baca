736a4aa86f1870687a3703424ef7caacc7aeb7d1
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-01 18:31:37 +0000

    Changed tied notes rhythm-maker to use Messiaen-style ties.

--- library/materials/tied_notes/definition.py
+++ library/materials/tied_notes/definition.py
@@ -5,5 +5,6 @@ from abjad import *
 tied_notes = rhythmmakertools.NoteRhythmMaker(
     tie_specifier=rhythmmakertools.TieSpecifier(
         tie_across_divisions=True,
+        use_messiaen_style=True,
         ),
     )
