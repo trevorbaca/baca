0ca7d45335322e7c0322d432319a76eaee88f293
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-02 15:10:24 +0000

    Added quarter divisions material package.

---
+++ library/materials/quarter_divisions/definition.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from experimental import *
+
+
+quarter_divisions = makertools.DivisionMaker(
+    cyclic=True,
+    pattern=[(1, 4)],
+    pattern_rotation_index=0,
+    remainder=Right,
+    )
---
+++ library/materials/quarter_divisions/output.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import durationtools
+from experimental.tools import makertools
+
+
+quarter_divisions = makertools.DivisionMaker(
+    pattern=(
+        durationtools.Division(1, 4),
+        ),
+    )
