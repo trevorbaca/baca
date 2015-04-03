e79e5b60f820b951399815ff97bac36cb15c7237
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-03 17:10:33 +0000

    Updated quarter note beat groups.

--- library/materials/quarter_note_beat_groups/definition.py
+++ library/materials/quarter_note_beat_groups/definition.py
@@ -7,11 +7,12 @@ quarter_note_beat_groups = makertools.BeatGroupDivisionMaker(
     beat_grouper=rhythmmakertools.BeatGrouper(
         counts=[2],
         fuse_remainder=True,
-        remainder_direction=Right,
+        remainder_direction=Left,
         ),
     beat_maker=rhythmmakertools.DurationBeatMaker(
         compound_beat_duration=Duration(3, 8),
+        fuse_remainder=True,
+        remainder_direction=Right,
         simple_beat_duration=Duration(1, 4),
         ),
-    decrease_durations_monotonically=False,
     )
--- library/materials/quarter_note_beat_groups/output.py
+++ library/materials/quarter_note_beat_groups/output.py
@@ -9,11 +9,12 @@ quarter_note_beat_groups = makertools.BeatGroupDivisionMaker(
     beat_grouper=rhythmmakertools.BeatGrouper(
         counts=[2],
         fuse_remainder=True,
-        remainder_direction=Right,
+        remainder_direction=Left,
         ),
     beat_maker=rhythmmakertools.DurationBeatMaker(
         compound_beat_duration=durationtools.Duration(3, 8),
+        fuse_remainder=True,
+        remainder_direction=Right,
         simple_beat_duration=durationtools.Duration(1, 4),
         ),
-    decrease_durations_monotonically=False,
     )
