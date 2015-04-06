350e77170575c11ab73ae9a65c81714e9e7c4419
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-06 04:22:58 +0000

    Removed eight-note accelerandi.

--- library/materials/sixteenth_note_accelerandi/definition.py
+++ library/materials/sixteenth_note_accelerandi/definition.py
@@ -3,7 +3,13 @@ from abjad import *
 
 
 sixteenth_note_accelerandi = rhythmmakertools.AccelerandoRhythmMaker(
+    beam_specifier=rhythmmakertools.BeamSpecifier(
+        use_feather_beams=True,
+        ),
     start_duration=Duration(1, 16),
     stop_duration=Duration(1, 32),
+    tuplet_spelling_specifier=rhythmmakertools.TupletSpellingSpecifier(
+        use_note_duration_bracket=True,
+        ),
     written_duration=Duration(1, 16),
     )
--- library/materials/sixteenth_note_accelerandi/output.py
+++ library/materials/sixteenth_note_accelerandi/output.py
@@ -4,7 +4,18 @@ from abjad.tools import rhythmmakertools
 
 
 sixteenth_note_accelerandi = rhythmmakertools.AccelerandoRhythmMaker(
+    beam_specifier=rhythmmakertools.BeamSpecifier(
+        beam_each_division=True,
+        beam_divisions_together=False,
+        use_feather_beams=True,
+        ),
     start_duration=durationtools.Duration(1, 16),
     stop_duration=durationtools.Duration(1, 32),
+    tuplet_spelling_specifier=rhythmmakertools.TupletSpellingSpecifier(
+        avoid_dots=False,
+        is_diminution=True,
+        simplify_tuplets=False,
+        use_note_duration_bracket=True,
+        ),
     written_duration=durationtools.Duration(1, 16),
     )
