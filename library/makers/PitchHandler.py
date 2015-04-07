a8a914f6844f1178678fd68f1f87ea61a42fac87
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-07 02:10:21 +0000

    Added pitch-handler.

---
+++ library/makers/PitchHandler.py
@@ -0,0 +1,97 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+class PitchHandler(abctools.AbjadObject):
+    r'''ensemble pitch-handler.
+
+    ..  container:: example
+
+        ::
+
+            >>> import baca
+            >>> handler = baca.makers.PitchHandler(
+            ...     scope=(['Flute Music Voice', 'Piano Music Voice'], (1, 4)),
+            ...     )
+
+        ::
+
+            >>> print(format(handler))
+            baca.library.makers.PitchHandler(
+                scope=baca.library.makers.CompoundScope(
+                    baca.library.makers.SimpleScope(
+                        context_name='Flute Music Voice',
+                        stages=(1, 4),
+                        ),
+                    baca.library.makers.SimpleScope(
+                        context_name='Piano Music Voice',
+                        stages=(1, 4),
+                        )
+                    ),
+                )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_scope',
+        '_specifiers',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        scope=None,
+        specifiers=None,
+        ):
+        import baca
+        parser = baca.makers.ScopeTokenParser()
+        scope = parser._to_compound_scope(scope)
+        assert isinstance(scope, baca.makers.CompoundScope), repr(scope)
+        self._scope = scope
+        if specifiers is not None:
+            specifiers = tuple(specifiers)
+        self._specifiers = specifiers
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def scope(self):
+        r'''Gets scope of pitch-handler.
+
+        ..  container:: example
+
+            ::
+
+                >>> print(format(handler.scope))
+                baca.library.makers.CompoundScope(
+                    baca.library.makers.SimpleScope(
+                        context_name='Flute Music Voice',
+                        stages=(1, 4),
+                        ),
+                    baca.library.makers.SimpleScope(
+                        context_name='Piano Music Voice',
+                        stages=(1, 4),
+                        )
+                    )
+
+        Set to compound scope.
+        '''
+        return self._scope
+
+    @property
+    def specifiers(self):
+        r'''Gets specifiers of pitch-handler.
+
+        ..  container:: example
+
+            ::
+
+                >>> handler.specifiers is None
+                True
+
+        Set to specifiers or none.
+        '''
+        return self._specifiers
