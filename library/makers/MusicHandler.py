5fd303d6a9e8d343108b5be712f5bb2001f710dd
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-04-07 02:04:17 +0000

    Added DisplacementSpecifier and MusicHandler.

---
+++ library/makers/MusicHandler.py
@@ -0,0 +1,97 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+class MusicHandler(abctools.AbjadObject):
+    r'''Ensemble music-handler.
+
+    ..  container:: example
+
+        ::
+
+            >>> import baca
+            >>> handler = baca.makers.MusicHandler(
+            ...     scope=('Violin Music Voice', (1, 4)),
+            ...     specifiers=[
+            ...         baca.makers.DisplacementSpecifier(
+            ...             displacements=[0, 0, 0, 0, 1, 1, 1, 1],
+            ...             ),
+            ...         ],
+            ...     )
+
+        ::
+
+            >>> print(format(handler))
+            baca.library.makers.MusicHandler(
+                scope=baca.library.makers.SimpleScope(
+                    context_name='Violin Music Voice',
+                    stages=(1, 4),
+                    ),
+                specifiers=[
+                    baca.library.makers.DisplacementSpecifier(
+                        displacements=datastructuretools.CyclicTuple(
+                            [0, 0, 0, 0, 1, 1, 1, 1]
+                            ),
+                        ),
+                    ],
+                )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_scope',
+        '_specifiers',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        scope=None,
+        specifiers=None,
+        ):
+        import baca
+        if isinstance(scope, tuple):
+            scope = baca.makers.SimpleScope(*scope)
+        if scope is not None:
+            assert isinstance(scope, baca.makers.SimpleScope), repr(scope)
+        self._scope = scope
+        assert isinstance(specifiers, list), repr(specifiers)
+        self._specifiers = specifiers
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def scope(self):
+        r'''Gets scope of music-handler.
+
+        ..  container:: example
+
+            ::
+
+                >>> print(format(handler.scope))
+                baca.library.makers.SimpleScope(
+                    context_name='Violin Music Voice',
+                    stages=(1, 4),
+                    )
+
+        Set to scope or none.
+        '''
+        return self._scope
+
+    @property
+    def specifiers(self):
+        r'''Gets specifiers of music-handler.
+
+        ..  container:: example
+
+            ::
+
+                >>> print(format(handler.specifiers))
+                [DisplacementSpecifier(displacements=CyclicTuple([0, 0, 0, 0, 1, 1, 1, 1]))]
+
+        Set to list of specifiers or none.
+        '''
+        return self._specifiers
