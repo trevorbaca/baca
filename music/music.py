86b9ba95bde53c39d8c3f082a69f708cc04641ad
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-05-26 17:38:22 +0000

    Cleaned up music module.

--- music/music.py
+++ music/music.py
@@ -965,37 +965,3 @@ def hpartition_rest_terminated(leaves, cut=(0,), gap=(0,)):
                 if lastLeaf.history['class'] == 'Note':
                     lastChunk.append(l)
     return result
-
-
-def map_elements_to_numbered_sublists(sequence):
-    '''Maps `sequence` elements to numbered sublists.
-
-    ::
-
-        >>> sequence = [1, 2, -3, -4, 5]
-        >>> map_elements_to_numbered_sublists(sequence)
-        [[1], [2, 3], [-4, -5, -6], [-7, -8, -9, -10], [11, 12, 13, 14, 15]]
-
-    ::
-
-        >>> sequence = [1, 0, -3, -4, 5]
-        >>> map_elements_to_numbered_sublists(sequence)
-        [[1], [], [-2, -3, -4], [-5, -6, -7, -8], [9, 10, 11, 12, 13]]
-
-    Note that numbering starts at ``1``.
-
-    Returns newly constructed list of lists.
-    '''
-    if not isinstance(sequence, list):
-        raise TypeError
-    if not all(isinstance(x, (int, long)) for x in sequence):
-        raise ValueError
-    result = []
-    current = 1
-    for length in sequence:
-        abs_length = abs(length)
-        part = range(current, current + abs_length)
-        part = [mathtools.sign(length) * x for x in part]
-        result.append(part)
-        current += abs_length
-    return result
--- music/test/test_music_make_measures.py
+++ music/test/test_music_make_measures.py
@@ -1,12 +1,12 @@
 from abjad import *
-from baca import music
+import baca
 
 
 def test_music_make_measures_01():
     t = Voice([Note(n, (1, 8)) for n in range(8)])
     assert len(t) == 8
     leaves_before = t.select_leaves()
-    music.makeMeasures(t, [(n, 8) for n in (2, 2, 2, 2)])
+    baca.music.make_measures(t, [(n, 8) for n in (2, 2, 2, 2)])
     assert len(t) == 4
     leaves_after = t.select_leaves()
     assert leaves_before == leaves_after
@@ -19,7 +19,7 @@ def test_music_make_measures_02():
     t = Voice([Note(n, (1, 8)) for n in range(8)])
     assert len(t) == 8
     leaves_before = t.select_leaves()
-    music.makeMeasures(t, [(n, 8) for n in (2, 3, 3)])
+    baca.music.make_measures(t, [(n, 8) for n in (2, 3, 3)])
     assert len(t) == 3
     leaves_after = t.select_leaves()
     assert leaves_before == leaves_after
