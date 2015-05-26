525f515a9ca18f83a30dbe9e92180e3c4546782c
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-05-26 20:38:38 +0000

    Cleaned up utilities.

--- utilities/insert_and_transpose_nested_subruns_in_pitch_class_number_list.py
+++ utilities/insert_and_transpose_nested_subruns_in_pitch_class_number_list.py
@@ -1,22 +1,29 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import datastructuretools
+from abjad.tools import scoretools
 from abjad.tools import sequencetools
+from abjad.tools import pitchtools
 
 
 def insert_and_transpose_nested_subruns_in_pitch_class_number_list(
     notes,
     subrun_tokens,
     ):
-    '''Insert and transpose nested subruns in `pitch_class_number_list`
+    '''Inserts and transposes nested subruns in `pitch_class_number_list`
     according to `subrun_tokens`.
 
     ::
 
         >>> import baca
+
+    ::
+
         >>> notes = [Note(p, (1, 4)) for p in [0, 2, 7, 9, 5, 11, 4]]
         >>> subrun_tokens = [(0, [2, 4]), (4, [3, 1])]
         >>> baca.utilities.insert_and_transpose_nested_subruns_in_pitch_class_number_list(
         ...     notes, subrun_tokens)
+
+    ::
 
         >>> t = []
         >>> for x in notes:
@@ -25,24 +32,25 @@ def insert_and_transpose_nested_subruns_in_pitch_class_number_list(
         ...   except AttributeError:
         ...        t.append([y.written_pitch.pitch_number for y in x])
 
+    ::
+
         >>> t
         [0, [5, 7], 2, [4, 0, 6, 11], 7, 9, 5, [10, 6, 8], 11, [7], 4]
 
-    Set `subrun_tokens` to a list of zero or more ``(index, length_list)`` pairs.
+    Set `subrun_tokens` to a list of zero or more ``(index, length_list)`` 
+    pairs.
 
-    For each ``(index, length_list)`` pair in *subrun_tokens*
-    the function will read *index* mod ``len(notes)`` and insert
-    a subrun of length ``length_list[0]`` immediately after ``notes[index]``,
-    a subrun of length ``length_list[1]`` immediately after ``notes[index+1]``,
-    and, in general, a subrun of ``length_list[i]`` immediately after
-    ``notes[index+i]``, for ``i < length(length_list)``.
+    For each ``(index, length_list)`` pair in *subrun_tokens* the function will
+    read *index* mod ``len(notes)`` and insert a subrun of length
+    ``length_list[0]`` immediately after ``notes[index]``, a subrun of length
+    ``length_list[1]`` immediately after ``notes[index+1]``, and, in general, a
+    subrun of ``length_list[i]`` immediately after ``notes[index+i]``, for ``i
+    < length(length_list)``.
 
-    New subruns are wrapped with lists.
-    These wrapper lists are designed
-    to allow inspection of the structural changes to *notes*
-    immediately after the function returns.
-    For this reason most calls to this function will be followed
-    by ``notes = sequencetools.flatten_sequence(notes)``:
+    New subruns are wrapped with lists. These wrapper lists are designed to
+    allow inspection of the structural changes to `notes` immediately after the
+    function returns. For this reason most calls to this function will be
+    followed by ``notes = sequencetools.flatten_sequence(notes)``:
 
     ::
 
@@ -60,26 +68,18 @@ def insert_and_transpose_nested_subruns_in_pitch_class_number_list(
         [Note("g'4")]
         Note("e'4")
 
-    This function is designed to work on a built-in Python list
-    of notes. This function is **not** designed to work on Abjad
-    voices, staves or other containers because the function currently
-    implements no spanner-handling.
-    That is, this function is designed to be used during
-    precomposition when other, similar abstract pitch transforms
-    may be common.
+    This function is designed to work on a built-in Python list of notes. This
+    function is **not** designed to work on Abjad voices, staves or other
+    containers because the function currently implements no spanner-handling.
+    That is, this function is designed to be used during precomposition.
 
     Returns list of integers and / or floats.
     '''
-    from abjad.tools import scoretools
-    from abjad.tools import pitchtools
-
     assert isinstance(notes, list)
     assert all(isinstance(x, scoretools.Note) for x in notes)
     assert isinstance(subrun_tokens, list)
-
     len_notes = len(notes)
     instructions = []
-
     for subrun_token in subrun_tokens:
         pairs = _make_index_length_pairs(subrun_token)
         for anchor_index, subrun_length in pairs:
@@ -96,14 +96,11 @@ def insert_and_transpose_nested_subruns_in_pitch_class_number_list(
                 anchor_pitch, anchor_written_duration, subrun_intervals)
             instruction = (anchor_index, new_notes)
             instructions.append(instruction)
-
     for anchor_index, new_notes in reversed(sorted(instructions)):
         notes.insert(anchor_index + 1, new_notes)
 
 
 def _get_intervals_in_subrun(subrun_source):
-    from abjad.tools import pitchtools
-
     subrun_source = list(subrun_source)
     result = [0]
     for first, second in sequencetools.iterate_sequence_nwise(
@@ -130,8 +127,6 @@ def _make_index_length_pairs(subrun_token):
 
 
 def _make_new_notes(anchor_pitch, anchor_written_duration, subrun_intervals):
-    from abjad.tools import pitchtools
-    from abjad.tools import scoretools
     new_notes = []
     for subrun_interval in subrun_intervals:
         new_pc = (pitchtools.NumberedPitch(anchor_pitch).pitch_number +
