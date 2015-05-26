56e481e789cc1787a28b3533d3483901760792db
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-05-26 19:24:05 +0000

    Updated utilities.

---
+++ utilities/make_breaks_voice.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+import math
+from abjad import *
+
+
+def make_breaks_voice(signatures, durations, pages, verticals, staves=None):
+    if staves != None:
+        staves = ' '.join([str(x) for x in staves])
+    result = []
+    total = 0
+    for p, page in enumerate(pages):
+        for l, line in enumerate(page):
+            for measure in range(line):
+                d = durations[total]
+                s = scoretools.Skip((1))
+                #inspect_(s).get_duration().multiplier = d
+                attach(d, s)
+                tabs = ''.join(
+                    #['\t'] * int(math.ceil((10 - len(s.body)) / 3.0))
+                    ['\t'] * int(math.ceil((10 - len(str(s))) / 3.0))
+                    )
+                result.append(s)
+                result[-1].directives.before.append(signatures[total] + '\t')
+                result[-1].directives.right = None
+                result[-1].directives.right.append('%s\\noBreak\t\t' % tabs)
+                result[-1].comments.before.append('measure %s' % (total + 1))
+                if l == 0 and measure == 0:
+                    string = '\n%% page %s' % (p + 1)
+                    result[-1].directives.before.append(string)
+                total += 1
+            result[-1].right = []
+            tabs = ''.join(['\t'] * int(math.ceil((10 - len(s.body)) / 3.0)))
+            result[-1].directives.right = None
+            result[-1].directives.right.append('%s\\break\t\t' % tabs)
+            if len(result[-(measure + 1)].directives.before) == 0:
+                result[-(measure + 1)].directives.before.append('')
+            result[-(measure + 1)].directives.before.append(
+                '\overrideProperty #"Score.NonMusicalPaperColumn"')
+            if staves == None:
+                string = "#'line-break-system-details #'((Y-offset . {}))"
+                string = string.format(verticals[p][l])
+                result[-(measure + 1)].directives.before.append(string)
+            else:
+                string = "#'line-break-system-details #'((Y-offset . {})"
+                string += " (alignment-offsets . ({})))"
+                string = string.format(verticals[p][l], staves)
+                result[-(measure + 1)].directives.before.append(string)
+        result[-1].directives.right = None
+        tabs = ''.join(['\t'] * int(math.ceil((10 - len(s.body)) / 3.0)))
+        result[-1].directives.right = None
+        string = '{}\\pageBreak\t'.format(tabs)
+        result[-1].directives.right.append(string)
+    result = Voice(result)
+    result.name = 'breaks'
+    return result
