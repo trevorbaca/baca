31788513e86e08f9cc16dc5e36d60aa707c6c84b
trevorbaca <trevorbaca@c9a753d1-e2f3-47c6-9bb3-04bd92e030d7>
2015-07-17 21:04:36 +0000

    Removed materials directory.

--- materials/full_measure_divisions/definition.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-from experimental import *
-
-
-full_measure_divisions = makertools.SplitByDurationsDivisionCallback(durations=None)
--- materials/full_measure_divisions/output.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad.tools import durationtools
-from experimental.tools import makertools
-
-
-full_measure_divisions = makertools.SplitByDurationsDivisionCallback(
-    compound_meter_multiplier=durationtools.Multiplier(1, 1),
-    )
