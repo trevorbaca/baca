3e377b6c7329fc5509297d17b5465ad63d2cf3c9
Trevor Bača <trevor.baca@gmail.com>
2017-10-04 14:43:17 -0500

    Cleaned up StemTremoloCommand.

--- baca/tools/StemTremoloCommand.py
+++ baca/tools/StemTremoloCommand.py
@@ -117,7 +117,6 @@ class StemTremoloCommand(abjad.AbjadObject):
     __documentation_section__ = 'Commands'
 
     __slots__ = (
-        '_pattern',
         '_selector',
         '_tremolo_flags',
         )
@@ -126,13 +125,9 @@ class StemTremoloCommand(abjad.AbjadObject):
 
     def __init__(
         self,
-        pattern=None,
         selector=None,
         tremolo_flags=32,
         ):
-        if pattern is not None:
-            assert isinstance(pattern, abjad.Pattern), repr(pattern)
-        self._pattern = pattern
         if selector is not None:
             assert isinstance(selector, abjad.Selector)
         self._selector = selector
@@ -149,246 +144,17 @@ class StemTremoloCommand(abjad.AbjadObject):
         '''
         if argument is None:
             return
-        pattern = self.pattern
-        if pattern is None:
-            pattern = abjad.index_all()
-        selector = self.selector or baca.select_plt_heads()
-        selections = selector(argument)
-        selections = baca.MusicMaker._normalize_selections(selections)
+        if self.selector is not None:
+            argument = self.selector(argument)
+        selections = baca.MusicMaker._normalize_selections(argument)
         for selection in selections:
-            items = pattern.get_matching_items(selection)
-            for item in items:
+            for pl in abjad.iterate(selection).by_leaf(pitched=True):
                 stem_tremolo = abjad.StemTremolo(
                     tremolo_flags=self.tremolo_flags
                     )
-                abjad.attach(stem_tremolo, item)
+                abjad.attach(stem_tremolo, pl)
 
     ### PUBLIC PROPERTIES ###
-
-    @property
-    def pattern(self):
-        r'''Gets pattern.
-
-        ..  container:: example
-
-            Selects first and last logical ties:
-
-            ::
-
-                >>> segment_maker = baca.SegmentMaker(
-                ...     score_template=baca.ViolinSoloScoreTemplate(),
-                ...     time_signatures=[(4, 8), (3, 8), (4, 8), (3, 8)],
-                ...     )
-
-            ::
-
-                >>> pattern = abjad.index_first() | abjad.index_last()
-                >>> specifiers = segment_maker.append_commands(
-                ...     'vn',
-                ...     baca.select_stages(1),
-                ...     baca.pitches('E4 F4'),
-                ...     baca.even_runs(),
-                ...     baca.StemTremoloCommand(
-                ...         pattern=pattern,
-                ...         ),
-                ...     )
-
-            ::
-
-                >>> result = segment_maker(is_doc_example=True)
-                >>> lilypond_file, metadata = result
-                >>> show(lilypond_file) # doctest: +SKIP
-
-            ..  docs::
-
-                >>> f(lilypond_file[abjad.Score])
-                \context Score = "Score" <<
-                    \tag violin
-                    \context GlobalContext = "Global Context" <<
-                        \context GlobalRests = "Global Rests" {
-                            {
-                                \time 4/8
-                                R1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                R1 * 3/8
-                            }
-                            {
-                                \time 4/8
-                                R1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                R1 * 3/8
-                            }
-                        }
-                        \context GlobalSkips = "Global Skips" {
-                            {
-                                \time 4/8
-                                s1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                s1 * 3/8
-                            }
-                            {
-                                \time 4/8
-                                s1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                s1 * 3/8
-                            }
-                        }
-                    >>
-                    \context MusicContext = "Music Context" <<
-                        \tag violin
-                        \context ViolinMusicStaff = "Violin Music Staff" {
-                            \context ViolinMusicVoice = "Violin Music Voice" {
-                                {
-                                    \set ViolinMusicStaff.instrumentName = \markup { Violin }
-                                    \set ViolinMusicStaff.shortInstrumentName = \markup { Vn. }
-                                    \clef "treble"
-                                    e'8 :32 [
-                                    f'8
-                                    e'8
-                                    f'8 ]
-                                }
-                                {
-                                    e'8 [
-                                    f'8
-                                    e'8 ]
-                                }
-                                {
-                                    f'8 [
-                                    e'8
-                                    f'8
-                                    e'8 ]
-                                }
-                                {
-                                    f'8 [
-                                    e'8
-                                    f'8 :32 ]
-                                    \bar "|"
-                                }
-                            }
-                        }
-                    >>
-                >>
-
-        ..  container:: example
-
-            Selects every other logical tie:
-
-            ::
-
-                >>> segment_maker = baca.SegmentMaker(
-                ...     score_template=baca.ViolinSoloScoreTemplate(),
-                ...     time_signatures=[(4, 8), (3, 8), (4, 8), (3, 8)],
-                ...     )
-
-            ::
-
-                >>> specifiers = segment_maker.append_commands(
-                ...     'vn',
-                ...     baca.select_stages(1),
-                ...     baca.pitches('E4 F4'),
-                ...     baca.even_runs(),
-                ...     baca.StemTremoloCommand(
-                ...         pattern=abjad.index_every([1], period=2),
-                ...         ),
-                ...     )
-
-            ::
-
-                >>> result = segment_maker(is_doc_example=True)
-                >>> lilypond_file, metadata = result
-                >>> show(lilypond_file) # doctest: +SKIP
-
-            ..  docs::
-
-                >>> f(lilypond_file[abjad.Score])
-                \context Score = "Score" <<
-                    \tag violin
-                    \context GlobalContext = "Global Context" <<
-                        \context GlobalRests = "Global Rests" {
-                            {
-                                \time 4/8
-                                R1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                R1 * 3/8
-                            }
-                            {
-                                \time 4/8
-                                R1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                R1 * 3/8
-                            }
-                        }
-                        \context GlobalSkips = "Global Skips" {
-                            {
-                                \time 4/8
-                                s1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                s1 * 3/8
-                            }
-                            {
-                                \time 4/8
-                                s1 * 1/2
-                            }
-                            {
-                                \time 3/8
-                                s1 * 3/8
-                            }
-                        }
-                    >>
-                    \context MusicContext = "Music Context" <<
-                        \tag violin
-                        \context ViolinMusicStaff = "Violin Music Staff" {
-                            \context ViolinMusicVoice = "Violin Music Voice" {
-                                {
-                                    \set ViolinMusicStaff.instrumentName = \markup { Violin }
-                                    \set ViolinMusicStaff.shortInstrumentName = \markup { Vn. }
-                                    \clef "treble"
-                                    e'8 [
-                                    f'8 :32
-                                    e'8
-                                    f'8 :32 ]
-                                }
-                                {
-                                    e'8 [
-                                    f'8 :32
-                                    e'8 ]
-                                }
-                                {
-                                    f'8 :32 [
-                                    e'8
-                                    f'8 :32
-                                    e'8 ]
-                                }
-                                {
-                                    f'8 :32 [
-                                    e'8
-                                    f'8 :32 ]
-                                    \bar "|"
-                                }
-                            }
-                        }
-                    >>
-                >>
-
-        Set to pattern or none.
-
-        Returns pattern or none.
-        '''
-        return self._pattern
 
     @property
     def selector(self):
