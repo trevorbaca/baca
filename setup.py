6945b312054169095bd30dfec21513b10f20d14c
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 12:42:24 -0500

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -2,14 +2,34 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    'sphinx',
+    'sphinx-rtd-theme',
+    'uqbar>=0.2.13',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad[development]',),
-        name='baca',
-        packages=('baca',),
+        install_requires=install_requires,
+        keywords=keywords,
+        license='MIT',
+        name='Bača Composition API',
+        packages=['baca'],
+        platforms='Any',
         url='https://github.com/trevorbaca/baca',
-        version='0.1',
         zip_safe=False,
         )
