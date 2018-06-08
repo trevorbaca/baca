b9da7765728ded9bae1bbae4c2819ac8784d5090
Trevor Bača <trevor.baca@gmail.com>
2018-06-08 15:57:57 -0500

    Integrated rmaker changes.

--- baca/HorizontalSpacingSpecifier.py
+++ baca/HorizontalSpacingSpecifier.py
@@ -623,7 +623,7 @@ class HorizontalSpacingSpecifier(abjad.AbjadObject):
         ...                 written_duration=abjad.Duration(1, 16),
         ...                 ),
         ...             tuplet_specifier=rmakers.TupletSpecifier(
-        ...                 use_note_duration_bracket=True,
+        ...                 duration_bracket=True,
         ...                 ),
         ...             ),
         ...         ),
