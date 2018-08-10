fd95089dfd8d3e50b1a9951f7a231dce97d51fdb
Trevor Bača <trevor.baca@gmail.com>
2018-08-10 12:59:33 -0500

    Wrote baca.scope() examples.

--- baca/scoping.py
+++ baca/scoping.py
@@ -1179,6 +1179,121 @@ def scope(
     ) -> Suite:
     """
     Sets ``command.match`` to ``match`` for each command in ``commands``.
+
+    ..  container:: example
+
+        >>> maker = baca.SegmentMaker(
+        ...     score_template=baca.SingleStaffScoreTemplate(),
+        ...     spacing=baca.minimum_duration((1, 16)),
+        ...     time_signatures=[(4, 8), (3, 8), (4, 8), (3, 8)],
+        ...     )
+
+        >>> maker(
+        ...     [('MusicVoice', (1, 2)), ('MusicVoice', (3, 4))],
+        ...     baca.make_even_divisions(),
+        ...     baca.scope(
+        ...         0,
+        ...         baca.pitches('E4 F4'),
+        ...         ),
+        ...     baca.scope(
+        ...         1,
+        ...         baca.pitches('F#4 G#4'),
+        ...         ),
+        ...     )
+
+        >>> lilypond_file = maker.run(environment='docs')
+        >>> abjad.show(lilypond_file, strict=89) # doctest: +SKIP
+
+        ..  docs::
+
+            >>> abjad.f(lilypond_file[abjad.Score], strict=89)
+            \context Score = "Score"                                                                 %! SingleStaffScoreTemplate
+            <<                                                                                       %! SingleStaffScoreTemplate
+                \context GlobalContext = "GlobalContext"                                             %! _make_global_context
+                <<                                                                                   %! _make_global_context
+                    \context GlobalSkips = "GlobalSkips"                                             %! _make_global_context
+                    {                                                                                %! _make_global_context
+            <BLANKLINE>
+                        % [GlobalSkips measure 1]                                                    %! _comment_measure_numbers
+                        \baca_new_spacing_section #1 #16                                             %! HorizontalSpacingSpecifier(1):SPACING
+                        \time 4/8                                                                    %! EXPLICIT_TIME_SIGNATURE:_set_status_tag:_make_global_skips(2)
+                        \baca_time_signature_color #'blue                                            %! EXPLICIT_TIME_SIGNATURE_COLOR:_attach_color_literal(2)
+                        s1 * 1/2                                                                     %! _make_global_skips(1)
+            <BLANKLINE>
+                        % [GlobalSkips measure 2]                                                    %! _comment_measure_numbers
+                        \baca_new_spacing_section #1 #16                                             %! HorizontalSpacingSpecifier(1):SPACING
+                        \time 3/8                                                                    %! EXPLICIT_TIME_SIGNATURE:_set_status_tag:_make_global_skips(2)
+                        \baca_time_signature_color #'blue                                            %! EXPLICIT_TIME_SIGNATURE_COLOR:_attach_color_literal(2)
+                        s1 * 3/8                                                                     %! _make_global_skips(1)
+            <BLANKLINE>
+                        % [GlobalSkips measure 3]                                                    %! _comment_measure_numbers
+                        \baca_new_spacing_section #1 #16                                             %! HorizontalSpacingSpecifier(1):SPACING
+                        \time 4/8                                                                    %! EXPLICIT_TIME_SIGNATURE:_set_status_tag:_make_global_skips(2)
+                        \baca_time_signature_color #'blue                                            %! EXPLICIT_TIME_SIGNATURE_COLOR:_attach_color_literal(2)
+                        s1 * 1/2                                                                     %! _make_global_skips(1)
+            <BLANKLINE>
+                        % [GlobalSkips measure 4]                                                    %! _comment_measure_numbers
+                        \baca_new_spacing_section #1 #16                                             %! HorizontalSpacingSpecifier(1):SPACING
+                        \time 3/8                                                                    %! EXPLICIT_TIME_SIGNATURE:_set_status_tag:_make_global_skips(2)
+                        \baca_time_signature_color #'blue                                            %! EXPLICIT_TIME_SIGNATURE_COLOR:_attach_color_literal(2)
+                        s1 * 3/8                                                                     %! _make_global_skips(1)
+                        \baca_bar_line_visible                                                       %! _attach_final_bar_line
+                        \bar "|"                                                                     %! _attach_final_bar_line
+            <BLANKLINE>
+                    }                                                                                %! _make_global_context
+                >>                                                                                   %! _make_global_context
+                \context MusicContext = "MusicContext"                                               %! SingleStaffScoreTemplate
+                <<                                                                                   %! SingleStaffScoreTemplate
+                    \context Staff = "MusicStaff"                                                    %! SingleStaffScoreTemplate
+                    {                                                                                %! SingleStaffScoreTemplate
+                        \context Voice = "MusicVoice"                                                %! SingleStaffScoreTemplate
+                        {                                                                            %! SingleStaffScoreTemplate
+            <BLANKLINE>
+                            % [MusicVoice measure 1]                                                 %! _comment_measure_numbers
+                            e'8                                                                      %! baca_make_even_divisions
+                            [                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            f'8                                                                      %! baca_make_even_divisions
+            <BLANKLINE>
+                            e'8                                                                      %! baca_make_even_divisions
+            <BLANKLINE>
+                            f'8                                                                      %! baca_make_even_divisions
+                            ]                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            % [MusicVoice measure 2]                                                 %! _comment_measure_numbers
+                            e'8                                                                      %! baca_make_even_divisions
+                            [                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            f'8                                                                      %! baca_make_even_divisions
+            <BLANKLINE>
+                            e'8                                                                      %! baca_make_even_divisions
+                            ]                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            % [MusicVoice measure 3]                                                 %! _comment_measure_numbers
+                            fs'!8                                                                    %! baca_make_even_divisions
+                            [                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            gs'!8                                                                    %! baca_make_even_divisions
+            <BLANKLINE>
+                            fs'!8                                                                    %! baca_make_even_divisions
+            <BLANKLINE>
+                            gs'!8                                                                    %! baca_make_even_divisions
+                            ]                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            % [MusicVoice measure 4]                                                 %! _comment_measure_numbers
+                            fs'!8                                                                    %! baca_make_even_divisions
+                            [                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                            gs'!8                                                                    %! baca_make_even_divisions
+            <BLANKLINE>
+                            fs'!8                                                                    %! baca_make_even_divisions
+                            ]                                                                        %! baca_make_even_divisions
+            <BLANKLINE>
+                        }                                                                            %! SingleStaffScoreTemplate
+                    }                                                                                %! SingleStaffScoreTemplate
+                >>                                                                                   %! SingleStaffScoreTemplate
+            >>                                                                                       %! SingleStaffScoreTemplate
+
     """
     if match is not None:
         assert isinstance(match, (int, tuple, list)), repr(match)
