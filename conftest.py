c6490ecad75d87cd1bb9bdf4047414f4b797b853
Trevor Bača <trevor.baca@gmail.com>
2019-07-18 16:54:54 -0400

    Cleaned up conftest.py file.

--- conftest.py
+++ conftest.py
@@ -5,18 +5,10 @@ from abjadext import rmakers
 
 
 @pytest.fixture(autouse=True)
-def add_baca(doctest_namespace):
-    doctest_namespace["baca"] = baca
-
-
-@pytest.fixture(autouse=True)
 def add_libraries(doctest_namespace):
     doctest_namespace["abjad"] = abjad
+    doctest_namespace["baca"] = baca
     doctest_namespace["f"] = abjad.f
+    doctest_namespace["rmakers"] = rmakers
     doctest_namespace["Infinity"] = abjad.mathtools.Infinity()
     doctest_namespace["NegativeInfinity"] = abjad.mathtools.NegativeInfinity()
-
-
-@pytest.fixture(autouse=True)
-def add_rmakers(doctest_namespace):
-    doctest_namespace["rmakers"] = rmakers
