8061e0f03911e4308bd8392e381ec7a019330f38
Trevor Bača <trevor.baca@gmail.com>
2019-07-24 18:39:27 -0400

    Cleaned up baca.MusicAccumulator call signature.

--- baca/mmaker.py
+++ baca/mmaker.py
@@ -5651,28 +5651,7 @@ class MusicAccumulator(object):
     ### SPECIAL METHODS ###
 
     def __call__(
-        self,
-        voice_name,
-        collections,
-        music_maker,
-        *specifiers,
-        allow_repeats=None,
-        color_unregistered_pitches=None,
-        counts=None,
-        denominator=None,
-        exhaustive=None,
-        extend_beam=None,
-        figure_name=None,
-        hide_time_signature=None,
-        imbrication_map=None,
-        is_foreshadow=None,
-        is_incomplete=None,
-        is_recollection=None,
-        talea_denominator=None,
-        thread=None,
-        time_treatments=None,
-        tuplet_denominator=None,
-        tuplet_force_fraction=None,
+        self, voice_name, collections, music_maker, *specifiers, **keywords
     ):
         r"""
         Calls music-accumulator on ``music_contribution``.
@@ -5710,25 +5689,6 @@ class MusicAccumulator(object):
         Returns none.
         """
         assert isinstance(music_maker, MusicMaker), repr(music_maker)
-        keywords = {
-            "allow_repeats": allow_repeats,
-            "color_unregistered_pitches": color_unregistered_pitches,
-            "counts": counts,
-            "denominator": denominator,
-            "exhaustive": exhaustive,
-            "extend_beam": extend_beam,
-            "figure_name": figure_name,
-            "hide_time_signature": hide_time_signature,
-            "imbrication_map": imbrication_map,
-            "is_foreshadow": is_foreshadow,
-            "is_incomplete": is_incomplete,
-            "is_recollection": is_recollection,
-            "talea_denominator": talea_denominator,
-            "thread": thread,
-            "time_treatments": time_treatments,
-            "tuplet_denominator": tuplet_denominator,
-            "tuplet_force_fraction": tuplet_force_fraction,
-        }
         keywords["figure_index"] = self._figure_index
         voice_name = self.score_template.voice_abbreviations.get(
             voice_name, voice_name
