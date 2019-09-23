b064b2a3ea7ed67d6b548fb1453c1f8fa812516a
Trevor Bača <trevor.baca@gmail.com>
2019-09-23 12:42:12 -0400

    Cleaned up typehints.

--- baca/commands.py
+++ baca/commands.py
@@ -1369,8 +1369,8 @@ def flat_glissando(
                 pitch, stop_pitch, mock=mock, selector=selector
             )
         else:
-            assert isinstance(pitch, abjad.NamedPitch)
-            assert isinstance(stop_pitch, abjad.NamedPitch)
+            assert isinstance(pitch, (str, abjad.NamedPitch))
+            assert isinstance(stop_pitch, (str, abjad.NamedPitch))
             interpolation_command = pitchcommands.interpolate_pitches(
                 pitch, stop_pitch, mock=mock, selector=selector
             )
