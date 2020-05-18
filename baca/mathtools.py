31242a015c36f03bae5bcf2a601627e8ecee8e5b
Trevor Bača <trevor.baca@gmail.com>
2020-05-18 14:46:04 -0400

    Added baca.partition_nested_into_inward_pointing_parts().

--- baca/mathtools.py
+++ baca/mathtools.py
@@ -422,6 +422,70 @@ def partition_integer_into_halves(n, bigger=abjad.Left, even="allowed"):
         return (smaller_half, bigger_half)
 
 
+def partition_nested_into_inward_pointing_parts(list_, target="negative"):
+    """
+    Partitions integers in ``list_`` into inward-pointing parts.
+
+    ..  container:: example
+
+        >>> list_ = [[1, 1, 5]]
+        >>> baca.partition_nested_into_inward_pointing_parts(list_)
+        [[1, 1, 5]]
+
+        >>> list_ = [[1, 1, -5]]
+        >>> baca.partition_nested_into_inward_pointing_parts(list_)
+        [[1, 1, 1, -4]]
+
+        >>> list_ = [[1], [5], [5, 1], [1, 5], [5, 5], [1, 5, 1]]
+        >>> baca.partition_nested_into_inward_pointing_parts(
+        ...    list_, target='positive')
+        [[1], [4, 1], [4, 1, 1], [1, 1, 4], [4, 1, 1, 4], [1, 4, 1, 1]]
+
+        >>> list_ = [[1, 1, -5]]
+        >>> baca.partition_nested_into_inward_pointing_parts(
+        ...    list_, target='positive')
+        [[1, 1, -5]]
+
+    """
+    result = []
+    if target == "negative":
+        for element in list_:
+            # -5 at beginning
+            if element[0] == -5:
+                result.append([-4, 1] + element[1:])
+            # -5 at end
+            elif element[-1] == -5:
+                result.append(element[:-1] + [1, -4])
+            # -5 in middle
+            elif -5 in element:
+                new = []
+                for x in element:
+                    if x != -5:
+                        new.append(x)
+                    else:
+                        new.append(-4)
+                        new.append(1)
+            # no -5
+            else:
+                result.append(element)
+    if target == "positive":
+        for sublist in list_:
+            new = sublist
+            # 5 at beginning
+            if new[0] == 5:
+                new = [4, 1] + new[1:]
+            # 5 at end
+            if new[-1] == 5:
+                new = new[:-1] + [1, 4]
+            # 5 in middle
+            if 5 in new:
+                new = [(4, 1) if element == 5 else element for element in new]
+                new = Sequence(new).flatten()
+                new = list(new)
+            result.append(new)
+    return result
+
+
 def repeat_subruns_to_length(notes, pairs, history=False):
     """
     Repeats ``notes`` according to ``pairs``.
