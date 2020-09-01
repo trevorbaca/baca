20ecac15b9ca5bd93f6b85a5ef72b50a2d46cbcb
Trevor Bača <trevor.baca@gmail.com>
2020-09-01 15:45:09 -0400

    Cleaned up find_score_directory.py script.

--- scr/find_score_directory.py
+++ scr/find_score_directory.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 
-
 if __name__ == "__main__":
     directory = sys.argv[1]
+    subdirectory = sys.argv[2]
     path = ide.Path(os.path.abspath(directory))
-    string = str(path.distribution)
+    subpath = getattr(path, subdirectory)
+    string = str(subpath)
     print(string)
