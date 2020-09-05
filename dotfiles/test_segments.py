766a96975490209d23c471b3afe851030dc8343e
Trevor Bača <trevor.baca@gmail.com>
2020-09-05 09:18:52 -0400

    Cleaned up test_segments.py file.

--- dotfiles/test_segments.py
+++ dotfiles/test_segments.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-import sys
 
 import ide
 import pytest
@@ -18,13 +17,6 @@ assert isinstance(segments, baca.Path), repr(segments)
 directories = segments.list_paths()
 
 
-# @pytest.mark.parametrize("directory", directories)
-# def test_segments_01(directory):
-#    exit_code = abjad_ide.check_definition_py(directory)
-#    if exit_code != 0:
-#        sys.exit(exit_code)
-
-
 @pytest.mark.parametrize("directory", directories)
-def test_segments_02(directory):
+def test_segments(directory):
     abjad_ide._test_segment_illustration(directory)
