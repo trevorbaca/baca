c3f2b557c655700331df5a3e9fc58e6966d1b426
Trevor Bača <trevor.baca@gmail.com>
2021-01-21 16:29:16 -0500

    Taught test_segments.py about $GITHUB_WORKSPACE.

--- dotfiles/test_segments.py
+++ dotfiles/test_segments.py
@@ -7,7 +7,7 @@ import pytest
 import baca  # isort:skip
 
 abjad_ide = ide.AbjadIDE()
-travis_build_dir = os.getenv("TRAVIS_BUILD_DIR")
+travis_build_dir = os.getenv("GITHUB_WORKSPACE")
 assert isinstance(travis_build_dir, str), repr(travis_build_dir)
 wrapper = pathlib.Path(travis_build_dir)
 assert isinstance(wrapper, pathlib.Path), repr(wrapper)
