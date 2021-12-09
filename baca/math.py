b40c3ac388dfcb8243f5b8493947ba0269ba5106
Trevor Bača <trevor.baca@gmail.com>
2021-12-09 09:58:15 +0100

    Cleaned up baca.partition_integer_into_halves().

--- baca/math.py
+++ baca/math.py
@@ -340,8 +340,7 @@ def overwrite_elements(sequence, pairs):
     return result
 
 
-# TODO: replace string-valued even='allowed' with constant-valued keyword
-def partition_integer_into_halves(n, bigger=abjad.Left, even="allowed"):
+def partition_integer_into_halves(n, bigger=abjad.Left, allow_even=True):
     """
     Partitions ``n`` into halves.
 
@@ -360,25 +359,17 @@ def partition_integer_into_halves(n, bigger=abjad.Left, even="allowed"):
 
     ..  container:: example
 
-        Likewise when ``n`` is even and ``even = 'disallowed'``:
+        Likewise when ``n`` is even and ``allow_even`` is false:
 
-        >>> baca.partition_integer_into_halves(
-        ...     8,
-        ...     bigger=abjad.Left,
-        ...     even="disallowed",
-        ... )
+        >>> baca.partition_integer_into_halves(8, bigger=abjad.Left, allow_even=False)
         (5, 3)
 
-        >>> baca.partition_integer_into_halves(
-        ...     8,
-        ...     bigger=abjad.Right,
-        ...     even="disallowed",
-        ... )
+        >>> baca.partition_integer_into_halves(8, bigger=abjad.Right, allow_even=False)
         (3, 5)
 
     ..  container:: example
 
-        But when ``n`` is even and ``even = 'allowed'`` then ``left == right`` and
+        But when ``n`` is even and ``allow_even`` is true then ``left == right`` and
         ``bigger`` is ignored:
 
         >>> baca.partition_integer_into_halves(8)
@@ -397,19 +388,19 @@ def partition_integer_into_halves(n, bigger=abjad.Left, even="allowed"):
         >>> baca.partition_integer_into_halves(0)
         (0, 0)
 
-    When ``n`` is ``0`` and ``even = 'disallowed'`` raises partition error.
-
-    Returns pair of positive integers.
+    Raises excepton when ``n`` is ``0`` and ``allow_even`` is false.
     """
     assert isinstance(n, int), repr(n)
     assert 0 <= n, repr(n)
+    assert bigger in (abjad.Left, abjad.Right), repr(bigger)
+    assert allow_even in (True, False), repr(allow_even)
     if n == 0:
-        if even == "disallowed":
+        if not allow_even:
             raise Exception(f"even number disallowed: {n!r}.")
         return (0, 0)
     smaller_half = int(math.floor(n / 2))
     bigger_half = n - smaller_half
-    if (smaller_half == bigger_half) and (even != "allowed"):
+    if smaller_half == bigger_half and not allow_even:
         smaller_half -= 1
         bigger_half += 1
     if bigger == abjad.Left:
