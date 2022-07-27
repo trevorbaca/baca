adbfe880fe7626a21bb997839e462c67ee88d2db
Trevor Bača <trevor.baca@gmail.com>
2022-07-27 16:48:00 -0400

    Cleaned up interpret.py.

--- baca/interpret.py
+++ baca/interpret.py
@@ -2585,6 +2585,9 @@ class DynamicScope:
     def leaves(self):
         return abjad.select.leaves(self.argument, exclude=_enums.HIDDEN)
 
+    def mmrests(self):
+        return _select.mmrests(self.argument, exclude=_enums.HIDDEN)
+
     def phead(self, n):
         return _select.phead(self.argument, n, exclude=_enums.HIDDEN)
 
