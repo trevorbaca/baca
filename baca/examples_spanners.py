5c69bd0dd13ac7dba9f71bdf71cfd09f105c146e
Trevor Bača <trevor.baca@gmail.com>
2023-10-14 14:36:37 -0400

    Cleaned up examples.

--- baca/examples_spanners.py
+++ baca/examples_spanners.py
@@ -5,23 +5,26 @@ Examples: spanners.
 
     Beams everything and sets beam direction down:
 
-    >>> score = baca.docs.make_empty_score(1)
-    >>> time_signatures = baca.section.wrap([(4, 8), (3, 8), (4, 8), (3, 8)])
-    >>> baca.section.set_up_score(score, time_signatures(), docs=True)
-    >>> baca.SpacingSpecifier(fallback_duration=(1, 12))(score)
-    >>> music = baca.make_even_divisions(time_signatures())
-    >>> score["Music"].extend(music)
-    >>> voice = score["Music"]
-    >>> _ = baca.pitch(voice, "C4")
-    >>> _ = baca.beam(voice, direction=abjad.DOWN)
-    >>> baca.docs.remove_deactivated_wrappers(score)
-    >>> lilypond_file = baca.lilypond.file(score, includes=["baca.ily"])
+    >>> def make_lilypond_file():
+    ...     score = baca.docs.make_empty_score(1)
+    ...     time_signatures = baca.section.wrap([(4, 8), (3, 8), (4, 8), (3, 8)])
+    ...     baca.section.set_up_score(score, time_signatures(), docs=True)
+    ...     baca.SpacingSpecifier(fallback_duration=(1, 12))(score)
+    ...     music = baca.make_even_divisions(time_signatures())
+    ...     score["Music"].extend(music)
+    ...     voice = score["Music"]
+    ...     baca.pitch(voice, "C4")
+    ...     baca.beam(voice, direction=abjad.DOWN)
+    ...     baca.docs.remove_deactivated_wrappers(score)
+    ...     lilypond_file = baca.lilypond.file(score, includes=["baca.ily"])
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -68,15 +71,18 @@ Examples: spanners.
 
     Attaches ottava indicators to trimmed leaves:
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.ottava(baca.select.tleaves(container))
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.ottava(baca.select.tleaves(container))
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -101,15 +107,18 @@ Examples: spanners.
 
     Attaches ottava bassa indicators to trimmed leaves:
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.ottava_bassa(baca.select.tleaves(container))
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.ottava_bassa(baca.select.tleaves(container))
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -134,15 +143,18 @@ Examples: spanners.
 
     Attaches slur to trimmed leaves:
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.slur(baca.select.tleaves(container))
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.slur(baca.select.tleaves(container))
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -167,16 +179,19 @@ Examples: spanners.
 
     Pedals leaves:
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.sustain_pedal(container, context="Staff")
-    >>> _ = baca.override.sustain_pedal_staff_padding(container, 6)
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.sustain_pedal(container, context="Staff")
+    ...     baca.override.sustain_pedal_staff_padding(container, 6)
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -203,15 +218,18 @@ Examples: spanners.
 
     Attaches trill spanner to trimmed leaves (leaked to the right):
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.trill_spanner(baca.select.tleaves(container, rleak=True))
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.trill_spanner(baca.select.tleaves(container, rleak=True))
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
@@ -236,19 +254,22 @@ Examples: spanners.
 
     Tweaks trill spanner:
 
-    >>> container = abjad.Container("r8 d'4 e' r8")
-    >>> lilypond_file = abjad.illustrators.components([container])
-    >>> _ = baca.trill_spanner(
-    ...     baca.select.tleaves(container, rleak=True),
-    ...     abjad.Tweak(r"- \tweak color #red"),
-    ...     alteration="M2",
-    ... )
+    >>> def make_lilypond_file():
+    ...     container = abjad.Container("r8 d'4 e' r8")
+    ...     lilypond_file = abjad.illustrators.components([container])
+    ...     baca.trill_spanner(
+    ...         baca.select.tleaves(container, rleak=True),
+    ...         abjad.Tweak(r"- \tweak color #red"),
+    ...         alteration="M2",
+    ...     )
+    ...     return lilypond_file
+
+    >>> lilypond_file = make_lilypond_file()
     >>> abjad.show(lilypond_file) # doctest: +SKIP
 
     ..  docs::
 
-        >>> score = lilypond_file["Score"]
-        >>> string = abjad.lilypond(score)
+        >>> string = abjad.lilypond(lilypond_file["Score"])
         >>> print(string)
         \context Score = "Score"
         {
