4ee6b454e224c36795f8e4fb8a18bf0af37ae660
Trevor Bača <trevor.baca@gmail.com>
2024-02-04 16:38:17 -0500

    Cleaned up baca.spanners.covered().

--- baca/spanners.py
+++ baca/spanners.py
@@ -212,10 +212,8 @@ def covered(
     left_broken_text: str = r"\baca-left-broken-covered-markup",
     right_broken: bool = False,
     staff_padding: int | float | None = None,
-    with_next_leaf: bool = False,
 ) -> list[abjad.Wrapper]:
-    if with_next_leaf is True:
-        argument = _select.rleak(argument)
+    argument = _select.rleak(argument)
     specifiers = _piecewise._prepare_text_spanner_arguments(
         items,
         boxed=False,
