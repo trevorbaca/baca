6921aed72413f83c1537f7761ebb902a93016579
Trevor Bača <trevor.baca@gmail.com>
2024-02-04 17:48:01 -0500

    Cleaned up baca.spanners.damp().

--- baca/spanners.py
+++ baca/spanners.py
@@ -251,10 +251,8 @@ def damp(
     left_broken: bool = False,
     right_broken: bool = False,
     staff_padding: int | float | None = None,
-    with_next_leaf: bool = False,
 ) -> list[abjad.Wrapper]:
-    if with_next_leaf is True:
-        argument = _with_next_nonobgc_leaf(argument)
+    argument = _with_next_nonobgc_leaf(argument)
     specifiers = _piecewise._prepare_text_spanner_arguments(
         r"\baca-damp-markup =|",
         boxed=False,
