97699b02e9898f6e38f1efef18756d746ee71bbd
Trevor Bača <trevor.baca@gmail.com>
2024-02-11 16:07:10 -0500

    Cleaned up baca.mspanners.text().

--- baca/mspanners.py
+++ baca/mspanners.py
@@ -210,3 +210,65 @@ def vibrato(
         )
     _tags.wrappers(wrappers, _helpers.function_name(_frame()))
     return wrappers
+
+
+def text(
+    argument,
+    descriptor: str,
+    *tweaks: _typings.IndexedTweak,
+    bookend: bool = True,
+    direction: int | None = None,
+    do_not_start_spanner_on_final_piece: bool = False,
+    leak_spanner_stop: bool = False,
+    left_broken: bool = False,
+    left_broken_text: str | None = None,
+    lilypond_id: int | str | None = None,
+    pieces: list[list[abjad.Leaf]] | None = None,
+    right_broken: bool = False,
+    staff_padding: int | float | None = None,
+) -> list[abjad.Wrapper]:
+    if not argument:
+        assert pieces, repr(pieces)
+        the_argument = pieces
+    if not pieces:
+        assert argument, repr(argument)
+        the_argument = argument
+    assert isinstance(descriptor, str), repr(descriptor)
+    specifiers = _piecewise.parse_text_spanner_descriptor(
+        descriptor,
+        direction=direction,
+        left_broken_text=left_broken_text,
+        lilypond_id=lilypond_id,
+    )
+    if len(specifiers) == 1:
+        specifier = specifiers[0]
+        wrappers = []
+        wrapper = _spanners._attach_spanner_start(
+            the_argument,
+            specifier.spanner_start,
+            *tweaks,
+            left_broken=left_broken,
+            staff_padding=staff_padding,
+        )
+        wrappers.append(wrapper)
+        wrapper = _spanners._attach_spanner_stop(
+            the_argument,
+            specifier.spanner_stop,
+            right_broken=right_broken,
+        )
+        wrappers.append(wrapper)
+    else:
+        wrappers = _piecewise._iterate_pieces(
+            argument,
+            *tweaks,
+            bookend=bookend,
+            do_not_start_spanner_on_final_piece=do_not_start_spanner_on_final_piece,
+            leak_spanner_stop=leak_spanner_stop,
+            left_broken=left_broken,
+            pieces=pieces,
+            right_broken=right_broken,
+            specifiers=specifiers,
+            staff_padding=staff_padding,
+        )
+    _tags.wrappers(wrappers, _helpers.function_name(_frame()))
+    return wrappers
