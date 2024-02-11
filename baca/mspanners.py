e4fb65550eef94e553e375664cf584491ef11bd1
Trevor Bača <trevor.baca@gmail.com>
2024-02-11 18:22:25 -0500

    Cleaned up baca.mspanners.text().

--- baca/mspanners.py
+++ baca/mspanners.py
@@ -223,16 +223,9 @@ def text(
     left_broken: bool = False,
     left_broken_text: str | None = None,
     lilypond_id: int | str | None = None,
-    pieces: list[list[abjad.Leaf]] | None = None,
     right_broken: bool = False,
     staff_padding: int | float | None = None,
 ) -> list[abjad.Wrapper]:
-    if not argument:
-        assert pieces, repr(pieces)
-        the_argument = pieces
-    if not pieces:
-        assert argument, repr(argument)
-        the_argument = argument
     assert isinstance(descriptor, str), repr(descriptor)
     specifiers = _piecewise.parse_text_spanner_descriptor(
         descriptor,
@@ -244,7 +237,7 @@ def text(
         specifier = specifiers[0]
         wrappers = []
         wrapper = _spanners._attach_spanner_start(
-            the_argument,
+            argument,
             specifier.spanner_start,
             *tweaks,
             left_broken=left_broken,
@@ -252,20 +245,20 @@ def text(
         )
         wrappers.append(wrapper)
         wrapper = _spanners._attach_spanner_stop(
-            the_argument,
+            argument,
             specifier.spanner_stop,
             right_broken=right_broken,
         )
         wrappers.append(wrapper)
     else:
         wrappers = _piecewise._iterate_pieces(
-            argument,
+            (),
             *tweaks,
             bookend=bookend,
             do_not_start_spanner_on_final_piece=do_not_start_spanner_on_final_piece,
             leak_spanner_stop=leak_spanner_stop,
             left_broken=left_broken,
-            pieces=pieces,
+            pieces=argument,
             right_broken=right_broken,
             specifiers=specifiers,
             staff_padding=staff_padding,
