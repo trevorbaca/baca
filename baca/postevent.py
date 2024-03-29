5c081c20e564a97b9aaf9f4f0ed6040ec1f8db04
Trevor Bača <trevor.baca@gmail.com>
2024-03-29 15:51:21 -0400

    Added postevent.py.

--- baca/postevent.py
+++ baca/postevent.py
@@ -1,11 +1,41 @@
 import abjad
 
 
+def bound_details_right_padding(n):
+    tweak = abjad.Tweak(rf"- \tweak bound-details.right.padding {n}")
+    return tweak
+
+
+def direction_down():
+    tweak = abjad.Tweak(r"- \tweak direction #down")
+    return tweak
+
+
+def padding(n):
+    tweak = abjad.Tweak(rf"- \tweak padding {n}")
+    return tweak
+
+
+def parent_alignment_x(n):
+    tweak = abjad.Tweak(rf"- \tweak parent-alignment-X {n}")
+    return tweak
+
+
+def self_alignment_x(n):
+    tweak = abjad.Tweak(rf"- \tweak self-alignment-X {n}")
+    return tweak
+
+
+def staff_padding(n):
+    tweak = abjad.Tweak(rf"- \tweak staff-padding {n}")
+    return tweak
+
+
 def to_bar_line_false(*, index=None):
     tweak = abjad.Tweak(r"- \tweak to-barline ##t")
     if index is not None:
         assert isinstance(index, int), repr(index)
-        tweak = tweak(tweak, index)
+        tweak = (tweak, index)
     return tweak
 
 
@@ -13,5 +43,5 @@ def to_bar_line_true(*, index=None):
     tweak = abjad.Tweak(r"- \tweak to-barline ##t")
     if index is not None:
         assert isinstance(index, int), repr(index)
-        tweak = tweak(tweak, index)
+        tweak = (tweak, index)
     return tweak
