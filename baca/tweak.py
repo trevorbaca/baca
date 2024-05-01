458b520c9a9406bb34b705c01468a33490435a91
Trevor Bača <trevor.baca@gmail.com>
2024-05-01 16:28:13 -0400

    Cleaned up tweak.py.

--- baca/tweak.py
+++ baca/tweak.py
@@ -16,93 +16,80 @@ def _handle_tweak(string, *, event=False, grob=None, i=None, tag=None, target=No
         return tweak
 
 
-def bound_details_left_broken_text(string, *, grob=None, i=None):
-    if grob is None:
-        tweak = abjad.Tweak(rf"- \tweak bound-details.left-broken.text {string}", i=i)
-    else:
-        tweak = abjad.Tweak(
-            rf"- \tweak {grob}.bound-details.left-broken.text {string}", i=i
-        )
-    return tweak
+def bound_details_left_broken_text(
+    string, *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = f"bound-details.left-broken.text {string}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def bound_details_left_padding(n, *, grob=None, i=None):
-    if grob is None:
-        tweak = abjad.Tweak(rf"- \tweak bound-details.left.padding {n}", i=i)
-    else:
-        tweak = abjad.Tweak(rf"- \tweak {grob}.bound-details.left.padding {n}", i=i)
-    return tweak
+def bound_details_left_padding(
+    n, *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = f"bound-details.left.padding {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def bound_details_left_text(string, *, grob=None, i=None):
-    if grob is None:
-        tweak = abjad.Tweak(rf"- \tweak bound-details.left.text {string}", i=i)
-    else:
-        tweak = abjad.Tweak(rf"- \tweak {grob}.bound-details.left.text {string}", i=i)
-    return tweak
+def bound_details_left_text(
+    string, *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = f"bound-details.left.text {string}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def bound_details_right_end_on_accidental_false(*, i=None):
-    tweak = abjad.Tweak(r"- \tweak bound-details.right.end-on-accidental ##f", i=i)
-    return tweak
+def bound_details_right_end_on_accidental_false(
+    *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = "bound-details.right.end-on-accidental ##f"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def bound_details_right_padding(n, *, grob=None, i=None):
-    if grob is None:
-        tweak = abjad.Tweak(rf"- \tweak bound-details.right.padding {n}", i=i)
-    else:
-        tweak = abjad.Tweak(rf"- \tweak {grob}.bound-details.right.padding {n}", i=i)
-    return tweak
+def bound_details_right_padding(
+    n, *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = f"bound-details.right.padding {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def bound_details_right_y(n):
-    tweak = abjad.Tweak(rf"- \tweak bound-details.right.Y {n}")
-    return tweak
+def bound_details_right_y(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"bound-details.right.Y {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def color(string):
-    tweak = abjad.Tweak(rf"- \tweak color {string}")
-    return tweak
+def color(string, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"color {string}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def direction_down():
-    tweak = abjad.Tweak(r"- \tweak direction #down")
-    return tweak
+def direction_down(*, event=False, grob=None, i=None, tag=None, target=None):
+    string = "direction #down"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def extra_offset(pair, *, event=False, tag=None):
+def extra_offset(pair, *, event=False, grob=None, i=None, tag=None, target=None):
     x, y = pair
-    string = rf"- \tweak extra-offset #'({x} . {y})"
-    if event is True:
-        string = string.removeprefix("- ")
-    tweak = abjad.Tweak(string, tag=tag)
-    return tweak
+    string = f"extra-offset #'({x} . {y})"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def font_size(n, *, i=None):
-    tweak = abjad.Tweak(rf"- \tweak font-size {n}", i=i)
-    return tweak
+def font_size(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"font-size {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def padding(n, *, event=False, grob=None):
-    if grob is None:
-        string = rf"- \tweak padding {n}"
-    else:
-        string = rf"- \tweak {grob}.padding {n}"
-    if event is True:
-        string = string.removeprefix("- ")
-    tweak = abjad.Tweak(string)
-    return tweak
+def padding(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"padding {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def parent_alignment_x(n):
-    tweak = abjad.Tweak(rf"- \tweak parent-alignment-X {n}")
-    return tweak
+def parent_alignment_x(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"parent-alignment-X {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def self_alignment_x(n):
-    tweak = abjad.Tweak(rf"- \tweak self-alignment-X {n}")
-    return tweak
+def self_alignment_x(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"self-alignment-X {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
 def shorten_pair(pair, *, event=False, grob=None, i=None, tag=None, target=None):
@@ -126,29 +113,26 @@ def style_trill(*, event=False, grob=None, i=None, tag=None, target=None):
     return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def to_bar_line_false(*, i=None):
-    tweak = abjad.Tweak(r"- \tweak to-barline ##f", i=i)
-    return tweak
+def to_bar_line_false(*, event=False, grob=None, i=None, tag=None, target=None):
+    string = "to-barline ##f"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def to_bar_line_true(*, i=None):
-    tweak = abjad.Tweak(r"- \tweak to-barline ##t", i=i)
-    return tweak
+def to_bar_line_true(*, event=False, grob=None, i=None, tag=None, target=None):
+    string = "to-barline ##t"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def x_extent_false(*, event=False, tag=None):
-    string = r"- \tweak X-extent ##f"
-    if event is True:
-        string = string.removeprefix("- ")
-    tweak = abjad.Tweak(string, tag=tag)
-    return tweak
+def x_extent_false(*, event=False, grob=None, i=None, tag=None, target=None):
+    string = "X-extent ##f"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def x_extent_zero(*, tag=None):
-    tweak = abjad.Tweak(r"- \tweak X-extent #'(0 . 0)", tag=tag)
-    return tweak
+def x_extent_zero(*, event=False, grob=None, i=None, tag=None, target=None):
+    string = "X-extent #'(0 . 0)"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
-def x_offset(n):
-    tweak = abjad.Tweak(rf"- \tweak X-offset {n}")
-    return tweak
+def x_offset(n, *, event=False, grob=None, i=None, tag=None, target=None):
+    string = f"X-offset {n}"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
