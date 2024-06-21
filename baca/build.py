42dff7379891f722590324706b8e8aad7584c699
Trevor Bača <trevor.baca@gmail.com>
2024-06-21 17:51:03 +0200

    Cleaned up _externalize().

--- baca/build.py
+++ baca/build.py
@@ -169,15 +169,163 @@ def _display_lilypond_log_errors(lilypond_log_file_path):
             print_always(line)
 
 
+def _externalize(
+    path: pathlib.Path,
+    *,
+    file_name: str | None = None,
+    in_place: bool = False,
+) -> pathlib.Path | None:
+    assert isinstance(path, pathlib.Path), repr(path)
+    if in_place is True:
+        assert file_name is None, repr(file_name)
+        path_ily = None
+    elif file_name is None:
+        path_ily = path.with_suffix(".ily")
+    else:
+        path_ily = path.with_name(file_name)
+    if path_ily is not None:
+        assert path_ily.suffix == ".ily", repr(path_ily)
+    preamble_lines: list[str] = []
+    score_lines: list[str] = []
+    stack: dict[str, list[str]] = {}
+    finished_variables = {}
+    found_score = False
+    with open(path) as pointer:
+        readlines = pointer.readlines()
+    for line in readlines:
+        if (
+            line.startswith(r"\score")
+            or line.startswith(r"\context Score")
+            or line.startswith("{")
+        ):
+            found_score = True
+        if not found_score:
+            preamble_lines.append(line)
+        elif " %*% " in line:
+            words = line.split()
+            index = words.index("%*%")
+            name = words[index + 1]
+            # first line in expression:
+            if name not in stack:
+                stack[name] = []
+                stack[name].append(line)
+            # last line in expression
+            else:
+                stack[name].append(line)
+                finished_variables[name] = stack[name]
+                del stack[name]
+                count = len(line) - len(line.lstrip())
+                indent = count * " "
+                dereference_string = indent + rf"{{ \{name} }}"
+                first_line = finished_variables[name][0]
+                dereference = [dereference_string]
+                dereference = [_ + "\n" for _ in dereference]
+                if bool(stack):
+                    items = list(stack.items())
+                    items[-1][-1].extend(dereference)
+                else:
+                    score_lines.extend(dereference)
+        elif bool(stack):
+            items = list(stack.items())
+            items[-1][-1].append(line)
+        else:
+            score_lines.append(line)
+    if in_place is False:
+        assert path_ily is not None
+        if path_ily.parent == path.parent:
+            include_name = path_ily.name
+        else:
+            include_name = str(path_ily)
+        include_line = f'\\include "{include_name}"'
+        include_lines = [include_line]
+        include_lines = [_ + "\n" for _ in include_lines]
+        last_include = 0
+        for i, line in enumerate(preamble_lines):
+            if line.startswith(r"\include"):
+                last_include = i
+        preamble_lines[last_include + 1 : last_include + 1] = include_lines
+    if preamble_lines[-2] == "\n":
+        del preamble_lines[-2]
+    lines = []
+    lines.extend(preamble_lines)
+    lines.extend(score_lines)
+    lines_ = []
+    for line in lines:
+        lines_.append(line)
+    text = "".join(lines_)
+    path.write_text(text)
+    lines = []
+    if in_place is False:
+        string = abjad.Configuration().get_lilypond_version_string()
+        string = rf'\version "{string}"'
+        lines.append(string + "\n")
+        lines.append("\n")
+    items = list(finished_variables.items())
+    total = len(items)
+    for i, item in enumerate(items):
+        name, variable_lines = item
+        first_line = variable_lines[0]
+        count = len(first_line) - len(first_line.lstrip())
+        first_line = first_line[count:]
+        first_line = f"{name} = {first_line}"
+        words = first_line.split()
+        index = words.index("%*%")
+        first_line = " ".join(words[:index])
+        first_lines = [first_line]
+        first_lines = [_ + "\n" for _ in first_lines]
+        lines.extend(first_lines)
+        for variable_line in variable_lines[1:]:
+            assert variable_line[:count].isspace(), repr(line)
+            variable_line = variable_line[count:]
+            if variable_line == "":
+                variable_line = "\n"
+            assert variable_line.endswith("\n"), repr(variable_line)
+            lines.append(variable_line)
+        not_topmost_index = None
+        for j, line in enumerate(reversed(lines)):
+            if line.strip() == f"%! {baca.tags.NOT_TOPMOST.string}":
+                not_topmost_index = j
+                break
+            if line.isspace():
+                break
+        if not_topmost_index is not None:
+            assert 0 < not_topmost_index
+            index = -(not_topmost_index + 1)
+            del lines[index]
+        last_line = lines[-1]
+        assert last_line.startswith("} ") or last_line.startswith(">> ")
+        words = last_line.split()
+        index = words.index("%*%")
+        last_line = " ".join(words[:index])
+        last_lines = [last_line]
+        last_lines = [_ + "\n" for _ in last_lines]
+        lines[-1:] = last_lines
+        if i < total - 1:
+            lines.append("\n")
+            lines.append("\n")
+    if in_place is False:
+        assert path_ily is not None
+        text = "".join(lines)
+        path_ily.write_text(text)
+        return path_ily
+    else:
+        assert "context Score" in score_lines[0]
+        score_lines[0] = "page-layout-score = " + score_lines[0]
+        lines_ = preamble_lines + lines + ["\n\n"] + score_lines
+        text = "".join(lines_)
+        path.write_text(text)
+        return None
+
+
 def _externalize_layout_ily_path(layout_ily_path):
     print_file_handling(f"Externalizing {baca.path.trim(layout_ily_path)} ...")
-    baca.path.externalize(layout_ily_path, do_not_tag=True, in_place=True)
+    _externalize(layout_ily_path, in_place=True)
 
 
 def _externalize_music_ly_path(music_ly):
     print_file_handling(f"Externalizing {baca.path.trim(music_ly)} ...")
     assert "sections" in music_ly.parts, repr(music_ly)
-    music_ily = baca.path.externalize(music_ly)
+    music_ily = _externalize(music_ly)
     for file in (music_ly, music_ily):
         text = file.read_text()
         messages = []
