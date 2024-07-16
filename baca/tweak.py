c83b135d3f9e0cb90327366156ef27f8e056617b
Trevor Bača <trevor.baca@gmail.com>
2024-07-16 13:18:32 +0200

    Added baca.tweak.endpoint_alignments_left_center().

--- baca/tweak.py
+++ baca/tweak.py
@@ -73,6 +73,20 @@ def direction_down(*, event=False, grob=None, i=None, tag=None, target=None):
     return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
 
 
+def endpoint_alignments_left_center(
+    *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = "endpoint-alignments #'(-1 . 0)"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
+
+
+def endpoint_alignments_left_left(
+    *, event=False, grob=None, i=None, tag=None, target=None
+):
+    string = "endpoint-alignments #'(-1 . -1)"
+    return _handle_tweak(string, event=event, grob=grob, i=i, tag=tag, target=target)
+
+
 def extra_offset(pair, *, event=False, grob=None, i=None, tag=None, target=None):
     x, y = pair
     string = f"extra-offset #'({x} . {y})"
