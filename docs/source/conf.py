4cd16077ece8b388e3b38c66c52fa09309b3e605
Trevor Bača <trevor.baca@gmail.com>
2025-07-30 16:07:46 -0400

    Bump Abjad from 3.27 to 3.28

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ html_theme_options = {
     "navigation_depth": 1,
     "sticky_navigation": False,
     "style_external_links": True,
-    "style_nav_header_background": "#FFAA55",
+    "style_nav_header_background": "#22CC66",
 }
 
 intersphinx_mapping = {
@@ -73,4 +73,4 @@ uqbar_book_strict = False
 uqbar_book_use_black = True
 uqbar_book_use_cache = True
 
-version = "3.27"
+version = "3.28"
